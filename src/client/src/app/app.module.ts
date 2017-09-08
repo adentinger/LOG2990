16d0b316671596e9dbb234f7072cc9bf4977c84b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-08 14:53:32 -0400

    Moved "crossword-game/" to the "app/" dir.

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { CubeComponent } from './cube/cube.component';
 import {RenderService} from './cube/render.service';
 import {BasicService} from './basic.service';
 
-import { CrosswordGameModule } from '../crossword-game/crossword-game.module';
+import { CrosswordGameModule } from './crossword-game/crossword-game.module';
 
 @NgModule({
   declarations: [
