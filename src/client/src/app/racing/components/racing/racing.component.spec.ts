ecd9e59761754baba3f0935dd0f0dbf8f0b4f5ef
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-08 15:44:08 -0400

    Generated Racing component.

---
+++ src/client/src/app/racing/components/racing/racing.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RacingComponent } from './racing.component';
+
+describe('RacingComponent', () => {
+  let component: RacingComponent;
+  let fixture: ComponentFixture<RacingComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RacingComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RacingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/client/src/app/racing/racing.module.ts
+++ src/client/src/app/racing/racing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RacingComponent } from './components/racing/racing.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [RacingComponent]
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        RacingComponent
+    ],
+    declarations: [
+        RacingComponent
+    ]
 })
 export class RacingModule { }
