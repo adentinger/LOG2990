b108f04f9fee08f80654abfb1b647ec7ace3b278
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-08 16:06:28 -0400

    Generated Board component.

--- src/client/src/app/crossword/crossword.module.ts
+++ src/client/src/app/crossword/crossword.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CrosswordComponent } from './components/crossword/crossword.component';
+import { BoardComponent } from './components/crossword/board/board.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations: [
-        CrosswordComponent
+        CrosswordComponent,
+        BoardComponent
     ],
     exports: [
         CrosswordComponent
