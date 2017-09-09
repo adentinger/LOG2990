1dccc650c98301eda3b15acbc843d16dc5ee5342
Adam <adam.martin-cote@noviflow.com>
2017-09-09 18:21:18 -0400

    New Feature: Implement routing for the 3 pages of the apps
    The app is now separed in 3 pages, with the app-header on top
    The user can navigate to the different pages using simple buttons, as decided in last friday's meeting

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,26 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { RenderService } from './cube/render.service';
+import { BasicService } from './basic.service';
+import { Routes, RouterModule } from '@angular/router'
+
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 import { AppComponent } from './app.component';
 import { CubeComponent } from './cube/cube.component';
 
-import { RenderService } from './cube/render.service';
-import { BasicService } from './basic.service';
-
 import { CrosswordModule } from './crossword/crossword.module';
 import { RacingModule } from './racing/racing.module';
+
+import { SelectionScreenComponent } from './selection-screen/selection-screen.component'
+import { CrosswordComponent } from './crossword/components/crossword/crossword.component'
+import { RacingComponent } from './racing/components/racing/racing.component'
+
+const appRoutes: Routes = [
+  { path: '', component: SelectionScreenComponent },
+  { path: 'crossword', component: CrosswordComponent },
+  { path: 'racing', component: RacingComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent
+    CubeComponent,
+    SelectionScreenComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     CrosswordModule,
-    RacingModule
+    RacingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     RenderService,
---
+++ src/client/src/app/selection-screen/selection-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selection-screen',
+  templateUrl: './selection-screen.component.html',
+  styleUrls: ['./selection-screen.component.css']
+})
+export class SelectionScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
