518afe9ef60a5cb804f1ddbe0575670254c5f9ad
pascallacasse <pascal.lacasse@hotmail.fr>
2017-09-10 18:00:01 -0400

    map editor height and length

---
+++ src/client/src/app/admin-screen/map-editor/map.ts
@@ -0,0 +1,7 @@
+import {Point} from './point';
+
+export class Map {
+    public height: number;
+    public lenght: number;
+    public points: Point[];
+}
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { RenderService } from './cube/render.service';
 import { BasicService } from './basic.service';
 
