65488992da27b2fd2f4d42d8c467b5b08ab9bdf7
pascallacasse <pascal.lacasse@hotmail.fr>
2017-09-11 17:05:44 -0400

    pl and a: path class creation and tslint modification

--- src/client/src/app/admin-screen/map-editor/path.ts
+++ src/client/src/app/admin-screen/map-editor/path.ts
@@ -4,5 +4,25 @@ import { Drawable } from './drawable';
 
 
 export class Path implements Drawable {
+    private context: CanvasRenderingContext2D;
     public points: Point[];
+
+    constructor(context: CanvasRenderingContext2D,
+                points: Point[] = []) {
+        this.context = context;
+        this.points = points;
+    }
+
+    public draw(): void {
+        this.context.beginPath();
+        this.points.forEach((point, index) => {
+            point.draw();
+            if (index !== 0) {
+                this.context.lineTo(point.x, point.y);
+            }
+            else {
+                this.context.moveTo(point.x, point.y);
+            }
+        });
+    }
 }
