10b9b5b24b8e6b2eb7b07b4a014d550cbb042ed7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-16 19:02:47 -0400

    Created MapRendererService API.

--- src/client/src/app/admin-screen/map-editor/map-renderer.service.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
 
+import { MapEditorService } from './map-editor.service';
+
 @Injectable()
 export class MapRendererService {
 
-  constructor() { }
+    private canvasContext: CanvasRenderingContext2D;
+
+    constructor(private mapEditor: MapEditorService) { }
+
+    public set context(context: CanvasRenderingContext2D) {
+        if (context !== undefined) {
+            this.context = context;
+        }
+        else {
+            throw new Error('Cannot change context of MapRendererService ' +
+                            'once set.');
+        }
+    }
+
+    public draw(): void {
+    }
 
 }
