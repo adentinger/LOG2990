a36b31f8369ea63e842f80e8973784150f70592e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-16 19:27:59 -0400

    Made MapRendererService test pass.

--- src/client/src/app/admin-screen/map-editor/map-renderer/map-renderer.service.spec.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/map-renderer.service.spec.ts
@@ -1,15 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MapRendererService } from './map-renderer.service';
+import { MapEditorService } from '../map-editor.service';
 
 describe('MapRendererService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MapRendererService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [MapRendererService, MapEditorService]
+        });
     });
-  });
 
-  it('should be created', inject([MapRendererService], (service: MapRendererService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([MapRendererService], (service: MapRendererService) => {
+        expect(service).toBeTruthy();
+    }));
 });
