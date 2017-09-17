6820b4240243f2e48b059ff47c95e6a4f43fb288
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-09-17 01:43:26 -0400

    Implemented ConfigMenuState construction

--- src/client/src/app/crossword/config-menu/config-menu-state.spec.ts
+++ src/client/src/app/crossword/config-menu/config-menu-state.spec.ts
@@ -1,10 +1,17 @@
 import { ConfigMenuState } from './config-menu-state';
-import { ConfigMenuOption } from './config-menu-option';
-import { TestBed } from '@angular/core/testing';
 
 describe('ConfigMenuState', () => {
     it('should create an instance', () => {
-        expect(new ConfigMenuState(0, '', [])).toBeTruthy();
-        expect(new ConfigMenuState(-1, '', [])).toThrowError('bad id');
+        const validPage: any = { id: 0, name: '', options: [{name: '', nextPage: 2}] };
+        expect(ConfigMenuState.fromJson(validPage).length).toEqual(1);
+
+        const validPages: any[] = [{ id: 0, name: '', options: [] }];
+        expect(ConfigMenuState.fromJson(validPages).length).toEqual(1);
+
+        const invalidPage: any = { id: -1, name: '', options: [{ name: '', nextPage: -1 }] };
+        expect(() => ConfigMenuState.fromJson(invalidPage)).toThrow();
+
+        const invalidPages: any[] = [{ id: -1, name: '', options: [] }];
+        expect(() => ConfigMenuState.fromJson(invalidPages)).toThrow();
     });
 });
--- src/client/src/app/crossword/config-menu/config-menu-state.ts
+++ src/client/src/app/crossword/config-menu/config-menu-state.ts
@@ -7,10 +7,53 @@ type ConfigMenuOptions = ConfigMenuOption[] | FetchableOptionList;
 export class ConfigMenuState {
     public readonly options: ConfigMenuOptions = [];
 
+    private static isState(object: any): boolean {
+        if (typeof object['id'] === 'number' &&
+            typeof object['name'] === 'string' &&
+            object['options'] !== undefined) {
+            if (Array.isArray(object['options'])) {
+                for (const option of (object['options'] as any[])) {
+                    if (!('name' in option && typeof option.name === 'string') ||
+                        !('nextPage' in option && typeof option.nextPage === 'number')) {
+                        return false;
+                    }
+                }
+                return true;
+            } else if (('url' in object['options'] &&
+                        typeof object['options'].url === 'string') &&
+                       ('nextPage' in object['options'] &&
+                        typeof object['options'].nextPage === 'number')) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static fromJson(json: any): ConfigMenuState[] {
+        const parsedStates: ConfigMenuState[] = [];
+        if (Array.isArray(json)) {
+            for (const state of (json as any[])) {
+                if (!ConfigMenuState.isState(state)) {
+                    throw new TypeError('Not a ConfigMenuState');
+                } else {
+                    parsedStates.push(new ConfigMenuState(state.id, state.name, state.options));
+                }
+            }
+        } else if (!ConfigMenuState.isState(json)) {
+            throw new TypeError('Not a ConfigMenuState');
+        } else {
+            parsedStates.push(new ConfigMenuState(json.id, json.name, json.options));
+        }
+        return parsedStates;
+    }
+
     constructor(
         public readonly id: PageId,
         public readonly name: string,
         options: ConfigMenuOptions = []) {
+        if (id < 0) {
+            throw new RangeError('Bad id');
+        }
         if (Array.isArray(options)) {
             (this.options as ConfigMenuOption[]).push.apply(this.options, options);
         } else {
