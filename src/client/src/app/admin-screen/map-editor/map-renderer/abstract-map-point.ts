b608296d28d95ae60690cd63cdf0bba2b1557e0d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-17 19:22:39 -0400

    Made AbstractMapPoint use the MapColors class.

--- src/client/src/app/admin-screen/map-editor/map-renderer/abstract-map-point.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/abstract-map-point.ts
@@ -1,44 +1,31 @@
 import { Drawable } from './drawable';
 import { Point } from '../point';
+import { MapColors } from './map-colors';
 
 export const INNER_COLOR_IDX = 0;
 export const RIM_COLOR_IDX   = INNER_COLOR_IDX + 1;
 
-const INNER_RADIUS = 15.0;
-const RIM_RADIUS   = 20.0;
-
 export abstract class AbstractMapPoint extends Point implements Drawable {
 
-    private context: CanvasRenderingContext2D;
-    private isActive = false;
-    private activeColors: string[];
-    private inactiveColors: string[];
+    protected context: CanvasRenderingContext2D;
+    public isActive = false;
+    private activeColors: MapColors;
+    private inactiveColors: MapColors;
 
     constructor(context: CanvasRenderingContext2D,
                 x: number,
                 y: number,
-                activeColors: string[],
-                inactiveColors: string[]) {
+                activeColors: MapColors,
+                inactiveColors: MapColors) {
         super(x, y);
         this.context = context;
         this.activeColors = activeColors;
         this.inactiveColors = inactiveColors;
     }
 
-    public draw(): void {
-        throw new Error('draw() method not provided in subclass of ' +
-                        'AbstractMapPoint.');
-    }
+    public abstract draw(): void;
 
-    protected getInnerColor(): string {
-        return this.currentColors()[INNER_COLOR_IDX];
-    }
-
-    protected getRimColor(): string {
-        return this.currentColors()[RIM_COLOR_IDX];
-    }
-
-    private currentColors(): string[] {
+    protected currentColors(): MapColors {
         return this.isActive ? this.activeColors : this.inactiveColors;
     }
 
