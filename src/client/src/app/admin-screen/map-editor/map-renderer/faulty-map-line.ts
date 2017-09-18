f7a6f302851f5e8953279f2102e6940943f9beb8
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-17 21:42:31 -0400

    Implemented FaultyMapLine and NormalMapLine.

--- src/client/src/app/admin-screen/map-editor/map-renderer/faulty-map-line.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/faulty-map-line.ts
@@ -1,2 +1,15 @@
-export class FaultyMapLine {
+import { AbstractMapLine } from './abstract-map-line';
+import { Point } from '../point';
+import { FaultyMapLineColors } from './faulty-map-line-colors';
+
+const WIDTH = 5;
+
+export class FaultyMapLine extends AbstractMapLine {
+
+    constructor(context: CanvasRenderingContext2D,
+                origin: Point,
+                destination: Point) {
+        super(context, origin, destination, new FaultyMapLineColors(), WIDTH);
+    }
+
 }
