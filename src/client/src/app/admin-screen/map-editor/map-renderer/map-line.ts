574a9a999231a9a6cd281ad553f4f84ce5f7bacd
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-17 20:34:54 -0400

    Began implementing lines.

--- src/client/src/app/admin-screen/map-editor/map-renderer/map-line.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/map-line.ts
@@ -1,9 +1,24 @@
 import { Drawable } from './drawable';
+import { Point } from '../point';
 
 export class MapLine implements Drawable {
 
-    public draw(): void {
+    protected context: CanvasRenderingContext2D;
+    protected origin: Point;
+    protected destination: Point;
 
+    constructor(context: CanvasRenderingContext2D,
+                origin: Point,
+                destination: Point) {
+        this.context = context;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public draw(): void {
+        this.context.moveTo(this.origin.x, this.origin.y);
+        this.context.lineTo(this.destination.x, this.destination.y);
+        this.context.stroke();
     }
 
 }
