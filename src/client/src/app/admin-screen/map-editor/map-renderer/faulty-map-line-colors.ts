d263c458cdb062081e1d3e87cee99d9444de301d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-17 21:37:41 -0400

    Implemented line color classes.

--- src/client/src/app/admin-screen/map-editor/map-renderer/faulty-map-line-colors.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/faulty-map-line-colors.ts
@@ -1,2 +1,11 @@
-export class FaultyMapLineColors {
+import { AbstractMapLineColors } from './abstract-map-line-colors';
+
+const LINE_COLOR = '#800';
+
+export class FaultyMapLineColors extends AbstractMapLineColors {
+
+    constructor() {
+        super(LINE_COLOR);
+    }
+
 }
--- src/client/src/app/admin-screen/map-editor/map-renderer/normal-map-line-colors.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/normal-map-line-colors.ts
@@ -1,2 +1,11 @@
-export class NormalMapLineColors {
+import { AbstractMapLineColors } from './abstract-map-line-colors';
+
+const LINE_COLOR = '#888';
+
+export class NormalMapLineColors extends AbstractMapLineColors {
+
+    constructor() {
+        super(LINE_COLOR);
+    }
+
 }
