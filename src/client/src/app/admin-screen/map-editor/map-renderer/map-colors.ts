97dc50ee3fa50fdeb0e1487742c3dd71d54bdb5f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-18 10:55:50 -0400

    Read along. Nothing to see here.

--- src/client/src/app/admin-screen/map-editor/map-renderer/map-colors.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/map-colors.ts
@@ -1,7 +1,7 @@
 export abstract class MapColors {
 
     public getColorOf(token: string): string {
-        const COLOR = this[token];
+        const COLOR = this[token]; // That's ugly but hey! Code reuse achieved.
         if (COLOR !== null) {
             return COLOR;
         }
