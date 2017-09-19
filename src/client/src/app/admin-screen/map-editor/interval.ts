f788705908cbfc90febc38b086d49936adbba64f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-19 15:13:11 -0400

    Implemented Interval class.

--- src/client/src/app/admin-screen/map-editor/interval.ts
+++ src/client/src/app/admin-screen/map-editor/interval.ts
@@ -1,2 +1,30 @@
 export class Interval {
+
+    public lowerBound: number;
+    public upperBound: number;
+
+    constructor(firstBound: number, secondBound: number) {
+        if (firstBound < secondBound) {
+            this.lowerBound = firstBound;
+            this.upperBound = secondBound;
+        }
+        else {
+            this.lowerBound = secondBound;
+            this.lowerBound = firstBound;
+        }
+    }
+
+    public get lower(): number {
+        return this.lowerBound;
+    }
+
+    public get upper(): number {
+        return this.upperBound;
+    }
+
+    public contains(value: number): boolean {
+        return value >= this.lowerBound
+            && value <= this.upperBound;
+    }
+
 }
