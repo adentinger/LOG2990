fd9a932087e127898ad591deb4609f18b2e0c776
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-18 21:21:50 -0400

    Began implementing Line class.

--- src/client/src/app/admin-screen/map-editor/line.ts
+++ src/client/src/app/admin-screen/map-editor/line.ts
@@ -1,2 +1,19 @@
+import { Point } from './point';
+import { Vector } from './vector';
+
 export class Line {
+
+    public origin: Point;
+    public destination: Point;
+
+    constructor(origin: Point,
+                destination: Point) {
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public get translation(): Vector {
+        return Vector.fromPoints(this.origin, this.destination);
+    }
+
 }
