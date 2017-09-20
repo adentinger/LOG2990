9ffcbef8c6e283d1f3c543fe7fb33484e7e401b8
pascallacasse <pascal.lacasse@hotmail.fr>
2017-09-20 14:19:10 -0400

    finished interval.ts tests

--- src/client/src/app/admin-screen/map-editor/interval.spec.ts
+++ src/client/src/app/admin-screen/map-editor/interval.spec.ts
@@ -33,13 +33,18 @@ describe('Interval', () => {
         expect(INTERVAL.contains(NUMBER_OUT_OF_BOUND)).toBe(false);
     });
 
-    it('should verify is number is in bound', () => {
-        const LOWER_BOUND = 10;
-        const UPPER_BOUND = 100;
-        const NUMBER_IN_BOUND = 50;
-        const NUMBER_OUT_OF_BOUND = 1000;
-        const INTERVAL = new Interval(UPPER_BOUND, LOWER_BOUND);
-        expect(INTERVAL.contains(NUMBER_IN_BOUND)).toBe(true);
-        expect(INTERVAL.contains(NUMBER_OUT_OF_BOUND)).toBe(false);
+    it('should check if two intervals intersect', () => {
+        const LOWER_BOUND_1 = 10;
+        const UPPER_BOUND_1 = 100;
+        const LOWER_BOUND_2 = 50;
+        const UPPER_BOUND_2 = 150;
+        const LOWER_BOUND_3 = 0;
+        const UPPER_BOUND_3 = 9;
+        const INTERVAL_1 = new Interval(UPPER_BOUND_1, LOWER_BOUND_1);
+        const INTERVAL_2 = new Interval(UPPER_BOUND_2, LOWER_BOUND_2);
+        const INTERVAL_3 = new Interval(UPPER_BOUND_3, LOWER_BOUND_3);
+        const FIRST_INTERVAL = new Interval(50, 100);
+        expect(INTERVAL_1.intersect(INTERVAL_2)).toEqual(FIRST_INTERVAL);
+        expect(INTERVAL_1.intersect(INTERVAL_3).isEmpty()).toBe(true);
     });
 });
