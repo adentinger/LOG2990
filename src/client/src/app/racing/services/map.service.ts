2c6b703f7c70b4c1a149012c0334c15abfd3647c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-21 18:28:20 -0400

    Added empty implementation of MapService.

--- src/client/src/app/racing/services/map.service.ts
+++ src/client/src/app/racing/services/map.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { Map } from '../../admin-screen/map-editor/map';
+
 @Injectable()
 export class MapService {
 
+    private readonly MAP_RESOURCE_URL = 'api/maps';
+
     constructor() { }
+
+    public getByName(name: string): Promise<Map> {
+        return Promise.reject('Not yet implemented');
+    }
+
+    public putByName(name: string): Promise<void> {
+        return Promise.reject('Not yet implemented');
+    }
+
+    public postByName(): Promise<void> {
+        return Promise.reject('Not yet implemented');
+    }
 
 }
