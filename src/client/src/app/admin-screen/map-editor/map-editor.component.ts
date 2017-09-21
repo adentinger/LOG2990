adb3b0cf6c79891cd7fff3ecdec0678594ce4a6b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-20 21:33:37 -0400

    Prevented user from creating points if they are over a point.

--- src/client/src/app/admin-screen/map-editor/map-editor.component.ts
+++ src/client/src/app/admin-screen/map-editor/map-editor.component.ts
@@ -18,6 +18,7 @@ export class MapEditorComponent implements OnInit {
 
     public width = 500;
     public height = 500;
+    public isDragging = false;
 
     constructor(private mapEditor: MapEditorService,
                 private mapRenderer: MapRendererService) { }
@@ -31,14 +32,45 @@ export class MapEditorComponent implements OnInit {
         event.preventDefault();
         switch (event.button) {
             case LEFT_MOUSE_BUTTON: {
-                this.addPoint(event.offsetX, event.offsetY);
+                this.leftClick(event);
                 break;
             }
             case RIGHT_MOUSE_BUTTON: {
-                this.removePoint();
+                this.rightClick(event);
                 break;
             }
         }
+    }
+
+    public mouseMoved(event: MouseEvent): void {
+        const COORDINATES = new Point(event.offsetX, event.offsetY);
+        this.mapRenderer.moveCursorTo(COORDINATES);
+        this.mapRenderer.draw();
+        console.log(this.isDragging);
+    }
+
+    public mouseDown(): void {
+        if (this.isHoveringPoint()) {
+            this.isDragging = true;
+        }
+    }
+
+    public mouseUp(): void {
+        this.isDragging = false;
+    }
+
+    private isHoveringPoint(): boolean {
+        return this.mapRenderer.activePoint !== null;
+    }
+
+    private leftClick(event: MouseEvent): void {
+        if (!this.isHoveringPoint()) {
+            this.addPoint(event.offsetX, event.offsetY);
+        }
+    }
+
+    private rightClick(event: MouseEvent): void {
+        this.removePoint();
     }
 
     private addPoint(x: number, y: number): void {
@@ -51,9 +83,4 @@ export class MapEditorComponent implements OnInit {
         this.mapRenderer.draw();
     }
 
-    public mouseMoved(event: MouseEvent): void {
-        const COORDINATES = new Point(event.offsetX, event.offsetY);
-        this.mapRenderer.moveCursorTo(COORDINATES);
-        this.mapRenderer.draw();
-    }
 }
