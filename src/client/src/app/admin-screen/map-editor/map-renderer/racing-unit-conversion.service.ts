09c2ff078cd356372a6c1e83833e4c8dcfc7333e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-09-25 11:12:24 -0400

    Made the RacingUnitConversionService keep its aspect ratio.

--- src/client/src/app/admin-screen/map-editor/map-renderer/racing-unit-conversion.service.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/racing-unit-conversion.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { AbstractRacingUnitConversionService } from '../abstract-racing-unit-conversion.service';
 import { Meters } from '../../../racing/types';
+import { Track } from '../../../racing/track';
 
 @Injectable()
 export class RacingUnitConversionService extends AbstractRacingUnitConversionService {
+
+    private static readonly ASPECT_RATIO = Track.WIDTH_MAX / Track.HEIGHT_MAX;
+
+    private lengthPerMeter = -1;
 
     private width = -1;
     private height = -1;
@@ -33,6 +38,8 @@ export class RacingUnitConversionService extends AbstractRacingUnitConversionSer
     public set windowWidth(width: number) {
         if (this.width > 0) {
             this.width = width;
+            this.height = width / RacingUnitConversionService.ASPECT_RATIO;
+            this.lengthPerMeter = this.width / Track.WIDTH_MAX;
         }
         else {
             throw new Error('Cannot set window width: negative value or zero passed.');
@@ -51,6 +58,8 @@ export class RacingUnitConversionService extends AbstractRacingUnitConversionSer
     public set windowHeight(height: number) {
         if (this.height > 0) {
             this.height = height;
+            this.width = height * RacingUnitConversionService.ASPECT_RATIO;
+            this.lengthPerMeter = this.width / Track.WIDTH_MAX;
         }
         else {
             throw new Error('Cannot set window height: negative value or zero passed.');
