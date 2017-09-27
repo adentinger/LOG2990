12aabda809991b13966b54a445c317ae4faa7562
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-09-27 08:54:30 -0400

    Added implementation of handler for route "/crossword/lexic/words"

--- src/server/app/routes/crossword/lexic.ts
+++ src/server/app/routes/crossword/lexic.ts
@@ -3,7 +3,8 @@ import { Db } from 'mongodb';
 
 import { Route, MiddleWare } from '../middle-ware';
 import { HttpStatus } from '../../http-response-status';
-import { WordConstraint } from './lexic/word-constraint';
+import { WordConstraint, isWordConstraint } from './lexic/word-constraint';
+import { provideDatabase } from '../../app-db';
 
 export const LEXIC_WORDS_COLLECTION = 'crossword-lexic-words';
 
@@ -21,9 +22,32 @@ export class Lexic {
 
 @MiddleWare
 export class LexicMiddleWare {
+    private static readonly LEXIC = new Lexic(provideDatabase());
+
     @Route('get', '/crossword/lexic/words')
     public words(req: express.Request, res: express.Response, next: express.NextFunction): void {
-        res.sendStatus(HttpStatus.NOT_IMPLEMENTED);
+        let constraint: WordConstraint;
+        try {
+            if (('constraint' in req.query) &&
+                (constraint = JSON.parse(req.query.constraint)) &&
+                isWordConstraint(constraint)) {
+                LexicMiddleWare.LEXIC.getWords(constraint).then((words) => {
+                    res.json(words);
+                }).catch((reason: any) => {
+                    console.warn('Warning:', reason);
+                    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+                    res.json(reason);
+                });
+                return;
+            }
+            console.log('Bad query string:', req.query);
+        } catch (e) {
+            console.warn('Warning:', (e as Error).message);
+            res.status(HttpStatus.UNKNOWN_ERROR);
+            res.send(e.toString());
+            return;
+        }
+        res.sendStatus(HttpStatus.BAD_REQUEST);
     }
 
     @Route('get', '/crossword/lexic/definitions')
