5527ddc4e716eb91fd8e8deb5724ae20cfd8eca9
pascallacasse <pascal.lacasse@hotmail.fr>
2017-10-01 18:08:29 -0400

    started regex implementation

--- src/server/app/routes/crossword/lexic/regex-builder.ts
+++ src/server/app/routes/crossword/lexic/regex-builder.ts
@@ -1,8 +1,38 @@
-import { WordConstraint } from './word-constraint';
+import { WordConstraint, isWordConstraint } from './word-constraint';
+import { CharConstraint } from './char-constraint';
 
 export class RegexBuilder {
 
     public buildFromConstraint(constraint: WordConstraint): RegExp {
+
+        let regularExpression: string;
+        regularExpression += '^';
+
+        if (isWordConstraint(constraint)) {
+
+            let positionBuffer = -1;
+            const sortedCharConstraints: CharConstraint[] = constraint.charConstraints.sort((a, b) => a.position - b.position);
+
+            const hasDuplicateEntry = !sortedCharConstraints.every((element) => {
+                if (element.position !== positionBuffer) {
+                    positionBuffer = element.position;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (hasDuplicateEntry) {
+                return null;
+            }
+
+            const first = sortedCharConstraints[0].position;
+
+            if (first !== 0) {
+                regularExpression += '.{' + sortedCharConstraints[0].position + '}';
+            }
+        }
         throw new Error('not implemented');
     }
+
 }
