6b7dd4ffdcb00a1e6d6c915b899910316410dede
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-01 21:36:57 -0400

    Implemented FirstMapLineColors.

--- src/client/src/app/admin-screen/map-editor/map-renderer/first-map-line-colors.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/first-map-line-colors.ts
@@ -1,2 +1,16 @@
-export class FirstMapLineColors {
+import { AbstractMapLineColors } from './abstract-map-line-colors';
+
+const LINE1_COLOR = '#222';
+const LINE2_COLOR = '#ddd';
+const LINE_EXTENTION_COLOR = '#888';
+
+export class FirstMapLineColors extends AbstractMapLineColors {
+
+    private line1: string;
+    private line2: string;
+
+    constructor() {
+        super(LINE_EXTENTION_COLOR);
+    }
+
 }
