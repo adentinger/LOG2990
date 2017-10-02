809c236c989c4b67aa7ebf9fca6eecaaaa246bff
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-01 22:22:35 -0400

    Start of implementation of FirstMapLine.

--- src/client/src/app/admin-screen/map-editor/map-renderer/abstract-map-line.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/abstract-map-line.ts
@@ -5,11 +5,11 @@ import { AbstractMapLineColors } from './abstract-map-line-colors';
 
 export abstract class AbstractMapLine extends Line implements Drawable {
 
-    private context: CanvasRenderingContext2D;
+    protected context: CanvasRenderingContext2D;
     public readonly origin: Point;
     public readonly destination: Point;
-    private colors: AbstractMapLineColors;
-    private width: number;
+    protected colors: AbstractMapLineColors;
+    protected width: number;
 
     constructor(context: CanvasRenderingContext2D,
                 origin: Point,
--- src/client/src/app/admin-screen/map-editor/map-renderer/first-map-line.ts
+++ src/client/src/app/admin-screen/map-editor/map-renderer/first-map-line.ts
@@ -1,2 +1,41 @@
-export class FirstMapLine {
+import { AbstractMapLine } from './abstract-map-line';
+import { Point } from '../point';
+import { FirstMapLineColors } from './first-map-line-colors';
+
+const WIDTH = 5;
+
+const STARTING_GRID_LENGTH = 30.0;
+
+export class FirstMapLine extends AbstractMapLine {
+
+    constructor(context: CanvasRenderingContext2D,
+                origin: Point,
+                destination: Point) {
+        super(context, origin, destination, new FirstMapLineColors(), WIDTH);
+    }
+
+    public draw(): void {
+        this.drawStartingGrid();
+        this.drawLineExtension();
+    }
+
+    private drawStartingGrid(): void {
+        this.context.beginPath();
+        this.context.lineWidth = WIDTH;
+    }
+
+    private drawLineExtension(): void {
+        this.context.beginPath();
+        if (this.translation.norm() > STARTING_GRID_LENGTH) {
+            const EXTENSION_START =
+                this.origin.toVector()
+                    .plus(this.translation.normalized().times(STARTING_GRID_LENGTH));
+            this.context.moveTo(EXTENSION_START.x, EXTENSION_START.y);
+            this.context.lineTo(this.destination.x, this.destination.y);
+            this.context.lineWidth = WIDTH;
+            this.context.strokeStyle = this.colors.getColorOf('line');
+            this.context.stroke();
+        }
+    }
+
 }
