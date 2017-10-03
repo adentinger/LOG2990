7514f234b5000bb96f7bb6822c7735ebc41e7a43
pascallacasse <pascal.lacasse@hotmail.fr>
2017-10-03 16:34:07 -0400

    simplified skybox

--- src/client/src/app/racing/racing-game/skybox.ts
+++ src/client/src/app/racing/racing-game/skybox.ts
@@ -1,21 +1,32 @@
 import * as THREE from 'three';
 
 export class Skybox extends THREE.Mesh {
-
+/*
     private static textureDayPromise: Promise<THREE.ShaderMaterial> = new Promise((resolve: Function, reject: (error: any) => void) => {
         const loader = new THREE.CubeTextureLoader();
         loader.setPath('/assets/racing/skybox/');
-        const day = loader.load(Skybox.findTextures('Day'), (texture: THREE.CubeTexture) => {
-            resolve(texture);
+        const day = loader.load(Skybox.findTextures('Day'), (material: THREE.CubeTexture) => {
+            resolve(material);
         }, () => { }, reject);
-    }).then((texture: THREE.CubeTexture) => {
-        const mat = Skybox.setShaders(texture);
-        return mat;
+    }).then((material: THREE.CubeTexture) => {
+        Skybox.textureDay = Skybox.setShaders(material);
+        return Skybox.textureDay;
     });
 
-    private static textureNight: THREE.ShaderMaterial = null;
-    private static textureDay: THREE.ShaderMaterial = null;
-    public daySkybox = new THREE.Mesh;
+    private static textureNightPromise: Promise<THREE.ShaderMaterial> = new Promise((resolve: Function, reject: (error: any) => void) => {
+        const loader = new THREE.CubeTextureLoader();
+        loader.setPath('/assets/racing/skybox/');
+        const night = loader.load(Skybox.findTextures('Night'), (material: THREE.CubeTexture) => {
+            resolve(material);
+        }, () => { }, reject);
+    }).then((material: THREE.CubeTexture) => {
+        Skybox.textureDay = Skybox.setShaders(material);
+        return Skybox.textureDay;
+    });
+*/
+    private static textureNight: THREE.ShaderMaterial = Skybox.createShaderMaterial('Night');
+    private static textureDay: THREE.ShaderMaterial = Skybox.createShaderMaterial('Day');
+
     private mode: string;
     public cube: THREE.Mesh;
 
@@ -25,13 +36,6 @@ export class Skybox extends THREE.Mesh {
         this.cube = new THREE.Mesh(new THREE.CubeGeometry(10000, 10000, 10000, 1, 1, 1), new THREE.ShaderMaterial());
         this.add(this.cube);
         this.swapMode(mode);
-    }
-
-    private static createTextureNight(): THREE.CubeTexture {
-        const loader = new THREE.CubeTextureLoader();
-        loader.setPath('./textures/');
-        const night = loader.load(Skybox.findTextures('Night'));
-        return night;
     }
 
     private static findTextures(mode: string): string[] {
@@ -44,14 +48,14 @@ export class Skybox extends THREE.Mesh {
                 'Day-Front.jpg', 'Day-Back.jpg'];
         }
         else if (mode === 'Night') {
-            images = ['Day-Left.jpg', 'Day-Right.jpg',
-                '.Day-Front.jpg', 'Day-Ceilling.jpg', '.Day-Bottom.jpg',
-                'Day-Back.jpg'
-            ];
+            images = ['Day-Right.jpg', 'Day-Left.jpg',
+                'Day-Ceilling.jpg', 'Day-Bottom.jpg',
+                'Day-Front.jpg', 'Day-Back.jpg'];
         }
 
         return images;
     }
+
 
     private static setShaders(texture: THREE.CubeTexture): THREE.ShaderMaterial {
         const SHADER = THREE.ShaderLib['cube'];
@@ -66,26 +70,42 @@ export class Skybox extends THREE.Mesh {
         return MATERIAL;
     }
 
+    private static createShaderMaterial(mode: string): THREE.ShaderMaterial {
+
+        let texture: THREE.CubeTexture;
+
+        const loader = new THREE.CubeTextureLoader();
+        loader.setPath('/assets/racing/skybox/');
+
+        if (mode === 'Day') {
+            texture = loader.load(Skybox.findTextures('Day'));
+        }
+        else {
+            texture = loader.load(Skybox.findTextures('Night'));
+        }
+
+        return Skybox.setShaders(texture);
+    }
+
     public swapMode(mode: string): void {
         if (mode === 'Day') {
             this.mode = 'Day';
-            if (Skybox.textureDay === null) {
-                Skybox.textureDayPromise.then((texture: THREE.ShaderMaterial) => {
-                    if (this.mode === 'Day') {
-                        Skybox.textureDay = texture;
-                        (this.cube.material as THREE.ShaderMaterial).setValues(Skybox.textureDay);
+            (this.cube.material as THREE.ShaderMaterial).setValues(Skybox.textureDay);
+        }/*
+        else if (mode === 'Night') {
+            this.mode = 'Night';
+            if (Skybox.textureNight === null) {
+                Skybox.textureNightPromise.then((material: THREE.ShaderMaterial) => {
+                    if (this.mode === 'Night') {
+                        (this.cube.material as THREE.ShaderMaterial).setValues(Skybox.textureNight);
                     }
                 });
             }
             else {
-                (this.cube.material as THREE.ShaderMaterial).setValues(Skybox.textureDay);
+                (this.cube.material as THREE.Material).update();
             }
-        }
-        else if (mode === 'Night') {
-            this.mode = 'Night';
-            (this.cube.material as THREE.MeshBasicMaterial).setValues(Skybox.textureDay);
-        }
-        (this.cube.material as THREE.Material).update();
+        }*/
     }
+
 
 }
