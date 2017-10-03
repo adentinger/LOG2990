03a6a31018ea9346935f9ac932a17fe45689e5dc
Adam <adam.martin-cote@noviflow.com>
2017-10-03 18:41:57 -0400

    Finalized the definition selection; linting

--- src/client/src/app/crossword/definition-field/definition-field.component.ts
+++ src/client/src/app/crossword/definition-field/definition-field.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Definition } from './class/definition';
 import { DefinitionsService } from './definitions.service';
-import { ClickOutsideModule } from 'ng-click-outside';
-
 
 @Component({
     selector: 'app-definition-field',
     templateUrl: './definition-field.component.html',
     styleUrls: ['./definition-field.component.css'],
     providers: [DefinitionsService]
-    
 })
 export class DefinitionFieldComponent implements OnInit {
 
@@ -21,17 +18,4 @@ export class DefinitionFieldComponent implements OnInit {
     public ngOnInit(): void {
         this.definitions = this.service.getDefinitions();
     }
-
-    public onSelect(e: Event): void {
-        //this.selectedDefinition = definition;
-        console.log('hi', e);
-    }
-
-    // public outsideSelect(): void {
-    //     this.selectedDefinition = null;
-    // }
-
-
-
-
 }
