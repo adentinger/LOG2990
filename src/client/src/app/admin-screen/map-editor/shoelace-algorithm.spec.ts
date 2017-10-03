9d10e4899821d8e38defc69e3c8116d0f9d8b4d5
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-03 09:03:14 -0400

    Fixed unit test of ShoelaceAlgorithm.

--- src/client/src/app/admin-screen/map-editor/shoelace-algorithm.spec.ts
+++ src/client/src/app/admin-screen/map-editor/shoelace-algorithm.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { ShoelaceAlgorithm } from './shoelace-algorithm';
 import { MockMaps } from './mock-maps';
+import { Point } from './point';
 
 describe('ShoelaceAlgorithm', () => {
 
@@ -24,9 +25,14 @@ describe('ShoelaceAlgorithm', () => {
     }));
 
     it('should compute the algebraic area of a polygon', () => {
+        const CHANGE_Y_AXIS = (point: Point) => { point.y = -point.y; };
+
         const CLOCKWISE_POLYGON = mockMaps.clockwise().path.points;
+        CLOCKWISE_POLYGON.forEach(CHANGE_Y_AXIS);
         expect(algorithm.algebraicAreaOf(CLOCKWISE_POLYGON) < 0).toBe(true);
+
         const COUNTER_CLOCKWISE_POLYGON = mockMaps.counterClockwise().path.points;
+        COUNTER_CLOCKWISE_POLYGON.forEach(CHANGE_Y_AXIS);
         expect(algorithm.algebraicAreaOf(COUNTER_CLOCKWISE_POLYGON) > 0).toBe(true);
     });
 
