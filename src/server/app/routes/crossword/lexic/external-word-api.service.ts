70dfc92681be41dbf24821ea0d7b8872ab95e1dd
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-10-02 20:26:05 -0400

    Implemented ExternalWordApiService.getFrequency

--- src/server/app/routes/crossword/lexic/external-word-api.service.ts
+++ src/server/app/routes/crossword/lexic/external-word-api.service.ts
@@ -1,7 +1,5 @@
 import * as http from 'http';
-import * as readline from 'readline';
 
-// http://api.wordnik.com:80/v4/word.json/the/frequency?useCanonical=false&startYear=1800&endYear=2017&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5
 export class ExternalWordApiService {
     private static readonly API_KEY = '509a8efe219607991700e030dbd01768e4a6b86cfa513bcc9';
     private static readonly REQUEST_BASE: http.RequestOptions = {
@@ -12,34 +10,52 @@ export class ExternalWordApiService {
         agent: false
     };
 
-    public getDefinitions(word: string): Promise<string[]> {
+    private requestWord(requestOptions: http.RequestOptions): Promise<string> {
         return new Promise((resolve, reject) => {
             const REQUEST_OPTIONS: http.RequestOptions = {
                 ...ExternalWordApiService.REQUEST_BASE,
-                path: `/v4/word.json/${word}/definitions` +
-                '?limit=200&includeRelated=true&useCanonical=false&includeTags=false' +
-                `&api_key=${ExternalWordApiService.API_KEY}`
+                ...requestOptions
             };
             const req = http.request(REQUEST_OPTIONS);
             req.on('response', (res: http.IncomingMessage) => {
                 let resp = '';
                 res.on('data', (chunck: string) => resp += chunck);
-                res.on('end', () => {
-                    try {
-                        const DEFINITIONS = JSON.parse(resp);
-                        resolve((DEFINITIONS as Array<any>)
-                            .sort((a, b) => +a.sequence - +b.sequence)
-                            .map((element) => <string>element.text));
-                    } catch (error) {
-                        reject(error);
-                    }
-                }).on('error', reject);
+                res.on('end', () => resolve(resp));
+                res.on('error', reject);
             }).on('error', reject);
             req.end(); // Ends the request setup and sends it.
         });
     }
 
+    public getDefinitions(word: string): Promise<string[]> {
+        return this.requestWord({
+            path: `/v4/word.json/${word}/definitions` +
+            '?limit=200&includeRelated=false&useCanonical=false&includeTags=false' +
+            `&api_key=${ExternalWordApiService.API_KEY}`
+        }).then((resp) => {
+            const DEFINITIONS = JSON.parse(resp);
+            if (Array.isArray(DEFINITIONS) && DEFINITIONS[0] && 'text' in DEFINITIONS[0]) {
+                return ((DEFINITIONS as Array<any>)
+                    .sort((a, b) => +a.sequence - +b.sequence)
+                    .map((element) => <string>element.text));
+            } else {
+                throw DEFINITIONS;
+            }
+        });
+    }
+
     public getFrequency(word: string): Promise<number> {
-        throw new Error('not implemented');
+        return this.requestWord({
+            path: `/v4/word.json/${word}/frequency` +
+            '?useCanonical=false&startYear=1800' +
+            `&api_key=${ExternalWordApiService.API_KEY}`
+        }).then((resp) => {
+            const FREQUENCY = JSON.parse(resp);
+            if ('totalCount' in FREQUENCY && Number.isNaN(FREQUENCY['totalCount'])) {
+                return +FREQUENCY.totalCount;
+            } else {
+                throw FREQUENCY;
+            }
+        });
     }
 }
