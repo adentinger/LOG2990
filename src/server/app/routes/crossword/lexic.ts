81c753240b4098acfba43c6b0151b2925d568f74
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-10-05 18:26:54 -0400

    Refactoring of Lexic and LexicMiddleWare classes

--- src/server/app/routes/crossword/lexic.ts
+++ src/server/app/routes/crossword/lexic.ts
@@ -8,6 +8,7 @@ import { provideDatabase } from '../../app-db';
 import { PrefixLogWith, isJson } from 'common/utils';
 import { RegexBuilder } from './lexic/regex-builder';
 import { ExternalWordApiService } from './lexic/external-word-api.service';
+import { CharConstraint } from './lexic/char-constraint';
 
 interface WordDocument {
     _id: string;
@@ -18,61 +19,85 @@ interface WordDocument {
 export class Lexic {
     public static readonly LEXIC_WORDS_COLLECTION = 'crossword-lexic-words';
     public static readonly COMMONALITY_FREQUENCY_THREASHOLD = 1000;
+    private wordCollection: Collection<WordDocument> = null;
+
     constructor(private databaseProvider: Promise<Db>,
         private regexBuilder: RegexBuilder,
-        private externalWordApiService: ExternalWordApiService) { }
+        private externalWordApiService: ExternalWordApiService) {
+        databaseProvider.then((db: Db) => {
+            db.collection(Lexic.LEXIC_WORDS_COLLECTION, (error: MongoError, wordCollection: Collection<WordDocument>) => {
+                this.wordCollection = wordCollection;
+            });
+        });
+    }
+
+    /**
+     * Check if all given word entries have a frequency. If one doesn't, it fetches the word' frequency
+     * from an external API and updates it on the database.
+     * @param words List of word entries from the database to check if they have a frequency or not.
+     */
+    private ensureFrequencies(words: WordDocument[]): Promise<WordDocument[]> {
+        if (this.wordCollection === null) {
+            return Promise.reject(new Error('MongoDB\'s word Collection unavailable'));
+        }
+        return Promise.all(words.map((word) => {
+            if (!Number.isFinite(word.frequency)) {
+                return this.externalWordApiService.getFrequency(word.value)
+                    .then((frequency: number) => {
+                        word.frequency = frequency;
+                        return this.wordCollection.updateOne({ _id: word._id }, { $set: { frequency: frequency } })
+                            .then((result) => {
+                                if (result.modifiedCount > 0) {
+                                    return word;
+                                }
+                                throw new Error(word.value + ' not updated');
+                            });
+                    }).catch(() => {
+                        return word;
+                    });
+            }
+            return Promise.resolve(word);
+        }));
+    }
+
+    private fetchWords(constraint: WordConstraint, resolve: (value: string[]) => void, reject: (reason?: any) => void) {
+        const REGEX = this.regexBuilder.buildFromConstraint(constraint);
+        if (REGEX === null) {
+            reject(new Error('Invalid WordConstraint'));
+            return;
+        }
+        const COMMONALITY_FILTER = constraint.isCommon ?
+            (word: WordDocument) => Number.isFinite(word.frequency) && word.frequency >= Lexic.COMMONALITY_FREQUENCY_THREASHOLD :
+            (word: WordDocument) => Number.isFinite(word.frequency) && word.frequency < Lexic.COMMONALITY_FREQUENCY_THREASHOLD;
+        const SEARCH_OPTION = { value: { $regex: REGEX } };
+        this.wordCollection.find(SEARCH_OPTION, { value: true, frequency: true })
+            .toArray()
+            .then(this.ensureFrequencies.bind(this))
+            .then((wordDocuments) => {
+                const words = wordDocuments.filter(COMMONALITY_FILTER).map((value) => value.value);
+                if (words.length === 0) {
+                    throw new Error('Requested words not found');
+                }
+                return words;
+            })
+            .then(resolve, reject);
+    }
 
     public getWords(constraint: WordConstraint): Promise<string[]> {
         return new Promise((resolve, reject) => {
-            const REGEX = this.regexBuilder.buildFromConstraint(constraint);
-            if (REGEX === null) {
-                reject(new Error('Invalid WordConstraint'));
-                return;
-            }
-            console.log(REGEX);
-            const COMMONALITY_FILTER = constraint.isCommon ?
-                (word: WordDocument) => Number.isFinite(word.frequency) && word.frequency >= Lexic.COMMONALITY_FREQUENCY_THREASHOLD :
-                (word: WordDocument) => Number.isFinite(word.frequency) && word.frequency < Lexic.COMMONALITY_FREQUENCY_THREASHOLD;
             this.databaseProvider.then((db: Db) => {
-                db.collection(Lexic.LEXIC_WORDS_COLLECTION, (error: MongoError, wordCollection: Collection<WordDocument>) => {
-                    if (error) {
-                        reject(error);
-                        return;
-                    }
-                    const SEARCH_OPTION = {
-                        value: { $regex: REGEX }
-                    };
-                    wordCollection.find(SEARCH_OPTION, { value: true, frequency: true })
-                        .toArray()
-                        .then((words) => {
-                            return Promise.all(words.map((word) => {
-                                if (!Number.isFinite(word.frequency)) {
-                                    return this.externalWordApiService.getFrequency(word.value)
-                                        .then((frequency: number) => {
-                                            word.frequency = frequency;
-                                            return wordCollection.updateOne({ _id: word._id }, { $set: { frequency: frequency } })
-                                                .then((result) => {
-                                                    if (result.modifiedCount > 0) {
-                                                        return word;
-                                                    }
-                                                    throw new Error(word.value + ' not updated');
-                                                });
-                                        }).catch(() => {
-                                            return word;
-                                        });
-                                }
-                                return Promise.resolve(word);
-                            }));
-                        })
-                        .then((wordDocuments) => {
-                            const words = wordDocuments.filter(COMMONALITY_FILTER).map((value) => value.value);
-                            if (words.length === 0) {
-                                throw new Error('Requested words not found');
-                            }
-                            return words;
-                        })
-                        .then(resolve, reject);
-                });
+                if (this.wordCollection !== null) {
+                    this.fetchWords(constraint, resolve, reject);
+                } else {
+                    db.collection(Lexic.LEXIC_WORDS_COLLECTION, (error: MongoError, wordCollection: Collection<WordDocument>) => {
+                        if (error) {
+                            reject(error);
+                            return;
+                        }
+                        this.wordCollection = wordCollection;
+                        this.fetchWords(constraint, resolve, reject);
+                    });
+                }
             });
         });
     }
@@ -86,27 +111,46 @@ export class Lexic {
 export class LexicMiddleWare {
     private static readonly LEXIC = new Lexic(provideDatabase(), new RegexBuilder(), new ExternalWordApiService());
 
+    private static parseQuery(query: any): WordConstraint {
+        let minLength: number = query.minLength,
+            maxLength: number = query.maxLength,
+            isCommon = false,
+            charConstraints: CharConstraint[] = [];
+        if ('minLength' in query) {
+            minLength = Number(query.minLength);
+        }
+        if ('isCommon' in query) {
+            isCommon = typeof query.isCommon === 'string' ? query.isCommon === 'true' : Boolean(query.isCommon);
+        }
+        if ('charConstraints' in query) {
+            if (typeof query.charConstraints === 'string' && isJson(query.charConstraints)) {
+                charConstraints = JSON.parse(query.charConstraints);
+            } else if (Array.isArray(query.charConstraints)) {
+                charConstraints = query.charConstraints;
+            }
+        }
+        if ('maxLength' in query) {
+            maxLength = Number(query.maxLength);
+            return <WordConstraint>{ minLength, maxLength, isCommon, charConstraints };
+        } else {
+            return <WordConstraint>{ minLength, isCommon, charConstraints };
+        }
+    }
+
     @Route('get', '/words')
     @PrefixLogWith('[lexic/words]')
     public words(req: express.Request, res: express.Response, next: express.NextFunction): void {
-        if (req.query && 'charConstraints' in req.query &&
-            typeof req.query['charConstraints'] === 'string' &&
-            isJson(req.query['charConstraints'])) {
-            req.query.charConstraints = JSON.parse(req.query.charConstraints);
-        }
-        req.query.isCommon = typeof req.query.isCommon === 'string' ? req.query.isCommon === 'true' : Boolean(req.query.isCommon);
-        req.query.minLength = Number(req.query.minLength);
-        if (req.query.maxLength && Number.isInteger(req.query.maxLength)) {
-            req.query.maxLength = Number(req.query.maxLength);
-        }
-        if (isWordConstraint(req.query)) {
-            const CONSTRAINT: WordConstraint = parseWordConstraint(req.query);
+        let initialQuery;
+        initialQuery = LexicMiddleWare.parseQuery(req.query);
+        if (isWordConstraint(initialQuery)) {
+            const CONSTRAINT: WordConstraint = parseWordConstraint(initialQuery);
             LexicMiddleWare.LEXIC.getWords(CONSTRAINT).then((words) => {
                 res.json(words);
             }).catch((reason: any) => {
                 reason = reason instanceof Error ? reason.message : reason;
                 console.warn(reason);
-                res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+                res.status((/not.*found/i).test(reason) ? HttpStatus.NOT_FOUND :
+                    ((/Invalid Word\s*Constraint/i).test(reason)) ? HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR);
                 res.send(reason);
             });
         } else {
