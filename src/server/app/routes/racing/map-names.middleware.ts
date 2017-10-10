d397a1377a31d3dd7f1f638dd5eed56317fcbf14
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-10 16:52:25 -0400

    Implemented MapNamesMiddleWare.

--- src/server/app/routes/racing/map-names.middleware.ts
+++ src/server/app/routes/racing/map-names.middleware.ts
@@ -2,15 +2,29 @@ import * as express from 'express';
 
 import { MiddleWare, Route } from '../middle-ware';
 import { HttpStatus } from '../../http-response-status';
+import { provideDatabase } from '../../app-db';
+import { MapDbService } from './maps-db-service';
 
 @MiddleWare('/racing/map-names')
 export class MapNamesMiddleWare {
+
+    private static readonly MAP_DB_SERVICE: MapDbService =
+        new MapDbService(provideDatabase());
 
     @Route('get', '/:count')
     public getMapNames(req: express.Request,
                        res: express.Response,
                        next: express.NextFunction): void {
-        res.sendStatus(HttpStatus.NOT_IMPLEMENTED);
+        const COUNT: number = req.params.count;
+        MapNamesMiddleWare.MAP_DB_SERVICE.getMapNames(COUNT)
+            .then((names: string[]) => {
+                res.status(HttpStatus.OK);
+                res.json(names);
+                res.send();
+            })
+            .catch((reason: any) => {
+                res.sendStatus(reason);
+            });
     }
 
 }
