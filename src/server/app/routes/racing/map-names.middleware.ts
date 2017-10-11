579ea1b7a3a84385054983a6e728ee41be6be1f9
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-10 21:49:57 -0400

    Changed a file name.

--- src/server/app/routes/racing/map-names.middleware.ts
+++ src/server/app/routes/racing/map-names.middleware.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import { MiddleWare, Route } from '../middle-ware';
 import { HttpStatus, getStatusOrDefault } from '../../http-response-status';
 import { provideDatabase } from '../../app-db';
-import { MapDbService } from './maps-db-service';
+import { MapDbService } from './map-db-service';
 
 @MiddleWare('/racing/map-names')
 export class MapNamesMiddleWare {
--- src/server/app/routes/racing/maps.middleware.ts
+++ src/server/app/routes/racing/maps.middleware.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import { MiddleWare, Route } from '../middle-ware';
 import { HttpStatus, getStatusOrDefault } from '../../http-response-status';
 import { provideDatabase } from '../../app-db';
-import { MapDbService } from './maps-db-service';
+import { MapDbService } from './map-db-service';
 import { SerializedMap } from '../../common/racing/serialized-map';
 
 @MiddleWare('/racing/maps')
