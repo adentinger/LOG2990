e87778f360e03d1de1c674492457cf404862315f
pascallacasse <pascal.lacasse@hotmail.fr>
2017-10-11 12:33:54 -0400

    added thumbnail component

---
+++ src/client/src/app/racing/initial-view/map-thumbnail/map-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-thumbnail',
+  templateUrl: './map-thumbnail.component.html',
+  styleUrls: ['./map-thumbnail.component.css']
+})
+export class MapThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
