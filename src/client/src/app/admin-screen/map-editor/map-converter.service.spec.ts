1ba7a7a032727cec527efffd4cdb1bc76f568984
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-11 20:16:50 -0400

    Made empty implementation of MapConverterService.

--- src/client/src/app/admin-screen/map-editor/map-converter.service.spec.ts
+++ src/client/src/app/admin-screen/map-editor/map-converter.service.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { MapConverterService } from './map-converter.service';
+import { RacingUnitConversionService } from './racing-unit-conversion.service';
 
 describe('MapConverterService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MapConverterService]
-    });
-  });
 
-  it('should be created', inject([MapConverterService], (service: MapConverterService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                MapConverterService,
+                RacingUnitConversionService
+            ]
+        });
+    });
+
+    it('should be created', inject([MapConverterService], (service: MapConverterService) => {
+        expect(service).toBeTruthy();
+    }));
+
 });
--- src/client/src/app/admin-screen/map-editor/map-converter.service.ts
+++ src/client/src/app/admin-screen/map-editor/map-converter.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { RacingUnitConversionService } from './racing-unit-conversion.service';
+import { Map } from './map';
+import { SerializedMap } from '../../common/racing/serialized-map';
 
 @Injectable()
 export class MapConverterService {
 
-  constructor() { }
+    constructor(private converter: RacingUnitConversionService) { }
+
+    public serialize(map: Map): SerializedMap {
+        return null;
+    }
+
+    public deserialize(serializedMap: SerializedMap): Map {
+        return null;
+    }
 
 }
