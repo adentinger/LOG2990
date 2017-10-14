38307de82031caee1986e8ecc610068535c8fe89
Hon-Leung Éric Chao <hochaa@l4712-08.info.polymtl.ca>
2017-10-13 21:07:25 -0400

    creation of wordGenerater class

--- src/server/app/routes/crossword/grid-generator.middleware.ts
+++ src/server/app/routes/crossword/grid-generator.middleware.ts
@@ -7,8 +7,24 @@ import { Difficulty } from './grid-generator/grid';
 @MiddleWare('/crossword/grid-generator')
 export class GridGeneratorMiddleware {
     @Route('get', '/easy')
-    public getEasy(req: express.Request, res: express.Response):void {
+    public getEasyGrid(req: express.Request, res: express.Response):void {
         let gridGenerator = new GridGenerator(Difficulty.easy);
+        res.status(HttpStatus.OK);
+        res.json(gridGenerator.gridGeneration());
+        res.send();
+    }
+    
+    @Route('get', '/normal')
+    public getNormalGrid(req: express.Request, res: express.Response):void {
+        let gridGenerator = new GridGenerator(Difficulty.normal);
+        res.status(HttpStatus.OK);
+        res.json(gridGenerator.gridGeneration());
+        res.send();
+    }
+    
+    @Route('get', '/hard')
+    public getHardGrid(req: express.Request, res: express.Response):void {
+        let gridGenerator = new GridGenerator(Difficulty.hard);
         res.status(HttpStatus.OK);
         res.json(gridGenerator.gridGeneration());
         res.send();
--- src/server/app/routes/crossword/grid-generator/lexique.ts
+++ src/server/app/routes/crossword/grid-generator/lexique.ts
@@ -3,7 +3,8 @@ import { Word } from './word';
 import { GridGenerator } from './grid-generator';
 import * as http from 'http';
 
-export function getWordOfDesiredLength(lengthMin: number, lengthMax: number, gridGenerator: GridGenerator) {
+export class GenerateWord {
+    export function getWordOfDesiredLength(lengthMin: number, lengthMax: number, gridGenerator: GridGenerator) {
     const desiredWords: string[] = [];
     for (let i = 0 ; i < lexicon.length; i++) {
         if ((lexicon[i].length >= lengthMin) && (lexicon[i].length <= lengthMax)) {
@@ -22,109 +23,112 @@ export function getWordOfDesiredLength(lengthMin: number, lengthMax: number, gri
     return new Word(desiredWordVerified);
 
 
+	}
+
+	export function getRandomIndex(min: number, max: number) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return (Math.floor(Math.random() * (max - min)) + min);
+	}
+
+	export function formatGrid(crossword: GridGenerator) {
+		for (let index = 0; index < crossword.gridDisplay.length; index++) {
+			for (let j = 0; j < crossword.gridDisplay[index].length; j++) {
+				if (!crossword.gridDisplay[index][j]) {
+					crossword.gridDisplay[index][j] = ' ';
+				}
+			}
+		}
+	}
+
+	export function noAccent(word: string) {
+		const accent = [
+			/[\300-\306]/g, /[\340-\346]/g, // A, a
+			/[\310-\313]/g, /[\350-\353]/g, // E, e
+			/[\314-\317]/g, /[\354-\357]/g, // I, i
+			/[\322-\330]/g, /[\362-\370]/g, // O, o
+			/[\331-\334]/g, /[\371-\374]/g, // U, u
+			/[\321]/g, /[\361]/g, // N, n
+			/[\307]/g, /[\347]/g, // C, c
+		];
+		const noAccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+
+		for (let i = 0; i < accent.length; i++) {
+			word = word.replace(accent[i], noAccent[i]);
+		}
+
+		return word;
+	}
+
+	export function noApostropheAndDash(word: string) {
+		word = word.replace(/['-]/g, '');
+		return word;
+	}
+
+	export function wordFormatting(word: string) {
+		word = noAccent(word);
+		word = noApostropheAndDash(word);
+		return word;
+	}
+
+	export function reverseString(str: string) {
+		const splitString = str.split('');
+		const reverseArray = splitString.reverse();
+		return reverseArray.join('');
+	}
+
+	export function alreadyChoosen(wordToCheck: string, gridForVertical: Word[], gridForAcross: Word[]) {
+		let alreadyChoosen = false;
+		const allWords: string[] = [];
+
+			for (let i = 0; i < gridForAcross.length; i++) {
+				allWords.push(gridForAcross[i].value);
+			}
+
+			for (let i = 0; i < gridForVertical.length; i++) {
+				allWords.push(gridForVertical[i].value);
+			}
+
+			for (let i = 0; i < allWords.length; i++) {
+				if (allWords[i] === wordToCheck) {
+					alreadyChoosen = true;
+				}
+			}
+
+		return alreadyChoosen;
+	}
+
+	export function getWords(minLength: number,
+		maxLength?: number, isCommon?: boolean,
+		charConstraints?: {char: string, position: number}[]): Promise<string[]> {
+		return new Promise((resolve, reject) => {
+			let url = 'http://localhost:3000/crossword/lexic/words?minLength=' + minLength;
+			if (Number.isInteger(maxLength)) {
+				url += '&maxLength' + maxLength;
+			}
+			if (isCommon !== undefined) {
+				url += '&isCommon=' + Boolean(isCommon);
+			}
+			if (Array.isArray(charConstraints)) {
+				url += '&charConstraints=' + JSON.stringify(charConstraints);
+			}
+			console.log('URL:', url);
+			http.get(url, (response: http.IncomingMessage) => {
+				let data = '';
+				response.on('data', (chunk) => data += chunk);
+				response.on('end', () => {
+					console.log(JSON.parse(data));
+					resolve(JSON.parse(data));
+				});
+				response.on('error', reject);
+			});
+		});
+	}
+
+	/*getWords(3, 5, false).then((words: string[]) => {
+		const randomWord = words[Math.round(Math.random() * words.length)];
+		console.log(randomWord);
+	});*/
 }
 
-export function getRandomIndex(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (Math.floor(Math.random() * (max - min)) + min);
-}
 
-export function formatGrid(crossword: GridGenerator) {
-    for (let index = 0; index < crossword.gridDisplay.length; index++) {
-        for (let j = 0; j < crossword.gridDisplay[index].length; j++) {
-            if (!crossword.gridDisplay[index][j]) {
-                crossword.gridDisplay[index][j] = ' ';
-            }
-        }
-    }
-}
-
-export function noAccent(word: string) {
-    const accent = [
-        /[\300-\306]/g, /[\340-\346]/g, // A, a
-        /[\310-\313]/g, /[\350-\353]/g, // E, e
-        /[\314-\317]/g, /[\354-\357]/g, // I, i
-        /[\322-\330]/g, /[\362-\370]/g, // O, o
-        /[\331-\334]/g, /[\371-\374]/g, // U, u
-        /[\321]/g, /[\361]/g, // N, n
-        /[\307]/g, /[\347]/g, // C, c
-    ];
-    const noAccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
-
-    for (let i = 0; i < accent.length; i++) {
-        word = word.replace(accent[i], noAccent[i]);
-    }
-
-    return word;
-}
-
-export function noApostropheAndDash(word: string) {
-    word = word.replace(/['-]/g, '');
-    return word;
-}
-
-export function wordFormatting(word: string) {
-    word = noAccent(word);
-    word = noApostropheAndDash(word);
-    return word;
-}
-
-export function reverseString(str: string) {
-    const splitString = str.split('');
-    const reverseArray = splitString.reverse();
-    return reverseArray.join('');
-}
-
-export function alreadyChoosen(wordToCheck: string, gridForVertical: Word[], gridForAcross: Word[]) {
-    let alreadyChoosen = false;
-    const allWords: string[] = [];
-
-        for (let i = 0; i < gridForAcross.length; i++) {
-            allWords.push(gridForAcross[i].value);
-        }
-
-        for (let i = 0; i < gridForVertical.length; i++) {
-            allWords.push(gridForVertical[i].value);
-        }
-
-        for (let i = 0; i < allWords.length; i++) {
-            if (allWords[i] === wordToCheck) {
-                alreadyChoosen = true;
-            }
-        }
-
-    return alreadyChoosen;
-}
-
-export function getWords(minLength: number,
-    maxLength?: number, isCommon?: boolean,
-    charConstraints?: {char: string, position: number}[]): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        let url = 'http://localhost:3000/crossword/lexic/words?minLength=' + minLength;
-        if (Number.isInteger(maxLength)) {
-            url += '&maxLength' + maxLength;
-        }
-        if (isCommon !== undefined) {
-            url += '&isCommon=' + Boolean(isCommon);
-        }
-        if (Array.isArray(charConstraints)) {
-            url += '&charConstraints=' + JSON.stringify(charConstraints);
-        }
-        console.log('URL:', url);
-        http.get(url, (response: http.IncomingMessage) => {
-            let data = '';
-            response.on('data', (chunk) => data += chunk);
-            response.on('end', () => {
-                console.log(JSON.parse(data));
-                resolve(JSON.parse(data));
-            });
-            response.on('error', reject);
-        });
-    });
-}
-
-/*getWords(3, 5, false).then((words: string[]) => {
-    const randomWord = words[Math.round(Math.random() * words.length)];
-    console.log(randomWord);
-});*/
