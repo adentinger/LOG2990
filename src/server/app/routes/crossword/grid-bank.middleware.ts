dfde8971e01208cda14970ab8ca5e27378960ab3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-14 12:46:33 -0400

    Implemented new class GridBankMiddleWare.

---
+++ src/server/app/routes/crossword/grid-bank.middleware.ts
@@ -0,0 +1,48 @@
+import * as express from 'express';
+
+import { MiddleWare, Route } from '../middle-ware';
+import { GridBanks } from './grid-bank/grid-banks';
+import { HttpStatus, getStatusOrDefault } from '../../http-response-status';
+import { Grid } from '../../common/grid';
+
+@MiddleWare('/crossword/grid-bank')
+export class GridBankMiddleWare {
+
+    @Route('post', '/fillup')
+    public postFillup(req: express.Request, res: express.Response): void {
+        GridBanks.getInstance().fillup()
+            .then(() => res.sendStatus(HttpStatus.OK))
+            .catch((reason: any) => {
+                res.sendStatus(getStatusOrDefault(reason));
+            });
+    }
+
+    @Route('get', '/easy')
+    public getEasy(req: express.Request, res: express.Response): void {
+        this.getGrid(GridBanks.getInstance().getEasyGrid, res);
+    }
+
+    @Route('get', '/normal')
+    public getNormal(req: express.Request, res: express.Response): void {
+        this.getGrid(GridBanks.getInstance().getNormalGrid, res);
+    }
+
+    @Route('get', '/hard')
+    public getHard(req: express.Request, res: express.Response): void {
+        this.getGrid(GridBanks.getInstance().getHardGrid, res);
+    }
+
+    private getGrid(gridGetter: () => Promise<Grid>,
+                    res: express.Response): void {
+        gridGetter()
+            .then((grid) => {
+                res.status(HttpStatus.OK);
+                res.json(grid);
+                res.send();
+            })
+            .catch((reason: any) => {
+                res.sendStatus(getStatusOrDefault(reason));
+            });
+    }
+
+}
