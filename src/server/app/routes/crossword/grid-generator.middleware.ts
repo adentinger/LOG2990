8bb5d6ebf5fb8182d9c427e773d94f885c713ddb
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-15 18:37:22 -0400

    Fixed linting errors of GridGeneratorMiddleWare.

--- src/server/app/routes/crossword/grid-generator.middleware.ts
+++ src/server/app/routes/crossword/grid-generator.middleware.ts
@@ -7,26 +7,26 @@ import { Difficulty } from './grid-generator/grid';
 @MiddleWare('/crossword/grid-generator')
 export class GridGeneratorMiddleware {
     @Route('get', '/easy')
-    public getEasyGrid(req: express.Request, res: express.Response):void {
-        let gridGenerator = new GridGenerator(Difficulty.easy);
+    public getEasyGrid(req: express.Request, res: express.Response): void {
+        const GRID_GENERATOR = new GridGenerator(Difficulty.easy);
         res.status(HttpStatus.OK);
-        res.json(gridGenerator.gridGeneration());
+        res.json(GRID_GENERATOR.gridGeneration());
         res.send();
     }
-    
+
     @Route('get', '/normal')
-    public getNormalGrid(req: express.Request, res: express.Response):void {
-        let gridGenerator = new GridGenerator(Difficulty.normal);
+    public getNormalGrid(req: express.Request, res: express.Response): void {
+        const GRID_GENERATOR = new GridGenerator(Difficulty.normal);
         res.status(HttpStatus.OK);
-        res.json(gridGenerator.gridGeneration());
+        res.json(GRID_GENERATOR.gridGeneration());
         res.send();
     }
-    
+
     @Route('get', '/hard')
-    public getHardGrid(req: express.Request, res: express.Response):void {
-        let gridGenerator = new GridGenerator(Difficulty.hard);
+    public getHardGrid(req: express.Request, res: express.Response): void {
+        const GRID_GENERATOR = new GridGenerator(Difficulty.hard);
         res.status(HttpStatus.OK);
-        res.json(gridGenerator.gridGeneration());
+        res.json(GRID_GENERATOR.gridGeneration());
         res.send();
     }
 }
