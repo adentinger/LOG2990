f97350e7948ddaedc0c6b4e0636d41a2bb8f4183
Adam <adam.martin-cote@noviflow.com>
2017-10-15 18:43:00 -0400

    Added definitions mock on server; new games fetches their definitions from it upon creation

--- src/server/app/routes/crossword/games/crossword-game.ts
+++ src/server/app/routes/crossword/games/crossword-game.ts
@@ -1,7 +1,14 @@
 import { CrosswordGameConfigs } from '../../../common/communication/game-configs';
 import { GridWord } from '../../../common/crossword/grid-word';
+import { DEFINITIONS_MOCK } from '../mocks/definitions-mock';
+import { Definition } from '../../../common/crossword/definition';
+
 export class CrosswordGame {
-    public grid: GridWord[] = [];
+    public horizontalGrid: GridWord[] = [];
+    public verticalGrid: GridWord[] = [];
+
+    public horizontalDefinitions: Definition[] = [];
+    public verticalDefinitions: Definition[] = [];
     public definitions: string;
     private gameMode: string;
 
@@ -10,12 +17,28 @@ export class CrosswordGame {
     constructor(configs: CrosswordGameConfigs) {
         this.gameMode = configs.gameMode;
         // other attributes
+
+        this.horizontalDefinitions = DEFINITIONS_MOCK;
+        this.verticalDefinitions = DEFINITIONS_MOCK;
+        console.log('new state of the game: ' + JSON.stringify(this.getGameInfo()));
     }
 
     public getGameInfo(): Object {
         return {
-            'player 1 id': this.player1Id,
-            'player 2 id': this.player2Id
+            'player1Id': this.player1Id,
+            'player2Id': this.player2Id,
+            'numberOfGridWords': this.getNumberOfWordsInGrid(),
+            'numberOfDefinitions': this.getNumberOfDefinitions(),
         };
+    }
+
+    private getNumberOfDefinitions(): number {
+        return this.horizontalDefinitions.length
+            + this.verticalDefinitions.length;
+    }
+
+    private getNumberOfWordsInGrid(): number {
+        return this.horizontalGrid.length
+            + this.verticalGrid.length;
     }
 }
