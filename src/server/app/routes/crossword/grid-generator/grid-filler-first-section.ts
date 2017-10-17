a7891b2179b85b4f765429ea76e23dacbf34f4d6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 19:10:59 -0400

    Implemented GridFillerFirstSection constructor.

--- src/server/app/routes/crossword/grid-generator/grid-filler-first-section.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-first-section.ts
@@ -1,5 +1,22 @@
 import { GridFiller } from './grid-filler';
+import { Difficulty } from './grid';
+import { GridFillerWordPlacement as WordPlacement } from './grid-filler-word-placement';
+import { WordPosition } from './word-position';
 
 export class GridFillerFirstSection extends GridFiller {
+
+    constructor(difficulty: Difficulty) {
+        super(difficulty);
+        this.acrossWords = [
+            new WordPlacement(new WordPosition(0, 0), 6, 9),
+            new WordPlacement(new WordPosition(1, 0), 3, 3),
+            new WordPlacement(new WordPosition(2, 0), 3, 3)
+        ];
+        this.verticalWords = [
+            new WordPlacement(new WordPosition(0, 0), 5, 8),
+            new WordPlacement(new WordPosition(0, 1), 3, 3),
+            new WordPlacement(new WordPosition(0, 2), 3, 3)
+        ];
+    }
 
 }
