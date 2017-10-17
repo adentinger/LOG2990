19d176d3a022a77789b4a8a307f81991a3d09b08
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 19:26:02 -0400

    Implemented GridFillerFourthSection constructor.

--- src/server/app/routes/crossword/grid-generator/grid-filler-fourth-section.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-fourth-section.ts
@@ -1,5 +1,15 @@
 import { GridFiller } from './grid-filler';
+import { Difficulty } from './grid';
+import { GridFillerWordPlacement as WordPlacement } from './grid-filler-word-placement';
+import { WordPosition } from './word-position';
 
 export class GridFillerFourthSection extends GridFiller {
+
+    constructor(difficulty: Difficulty) {
+        super(difficulty);
+        this.verticalWords = [
+            new WordPlacement(new WordPosition(1, 9), 4, 5)
+        ];
+    }
 
 }
