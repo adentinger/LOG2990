8f2f834282b41efdb426a2841297530add30b4e6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 18:35:38 -0400

    Implemented new method 'fillUsing' in Grid.

--- src/server/app/routes/crossword/grid-generator/grid.ts
+++ src/server/app/routes/crossword/grid-generator/grid.ts
@@ -1,4 +1,5 @@
 import { Word } from './word';
+import { GridFiller } from './grid-filler';
 
 export enum Difficulty {easy, normal, hard}
 
@@ -14,4 +15,9 @@ export class Grid {
     public isCurrentlyValid(): boolean {
         return false;
     }
+
+    public async fillUsing(filler: GridFiller): Promise<void> {
+        await(filler.fill(this));
+    }
+
 }
