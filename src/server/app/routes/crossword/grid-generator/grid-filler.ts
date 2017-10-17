a7115e35d37c95e3683c24d969df54478a019384
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 17:31:02 -0400

    Added attributes to GridFiller and added a param to fill().

--- src/server/app/routes/crossword/grid-generator/grid-filler.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler.ts
@@ -1,10 +1,16 @@
+import { WordPlacement } from './word-placement';
+import { Grid } from './grid';
+
 export const WORD_SEARCH_MAX_ATTEMPT = 200;
 export enum column { first, second, third }
 export enum row { first, second, third }
 
 export abstract class GridFiller {
 
-    public async fill(): Promise<void> {
+    protected acrossWords: WordPlacement[];
+    protected verticalWords: WordPlacement[];
+
+    public async fill(grid: Grid): Promise<void> {
         await null;
     }
 
