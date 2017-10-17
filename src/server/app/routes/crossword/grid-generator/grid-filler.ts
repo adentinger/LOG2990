f9d3c33edfa69496df399fcae1079001d76abe0a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 16:33:16 -0400

    Fixed linting errors.

--- src/server/app/routes/crossword/grid-generator/grid-filler.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler.ts
@@ -1,9 +1,3 @@
-import { Word } from './word';
-import { WordCaller } from './word-caller';
-import { GridGenerator } from './grid-generator';
-import { lexicon } from './englishWords';
-
-
 export const WORD_SEARCH_MAX_ATTEMPT = 200;
 export enum column { first, second, third }
 export enum row { first, second, third }
