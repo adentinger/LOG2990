e6784efe38688414a00873b00724b5b61fe1b9fa
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 18:22:08 -0400

    Implemented test forf GridFillerThirdSection.

---
+++ src/server/app/routes/crossword/grid-generator/grid-filler-third-section.spec.ts
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import { GridFillerThirdSection } from './grid-filler-third-section';
+import { Grid, Difficulty } from './grid';
+
+describe('GridFillerThirdSection', () => {
+
+    let filler: GridFillerThirdSection;
+
+    beforeEach(() => {
+        filler = new GridFillerThirdSection();
+    });
+
+    it('should be created', () => {
+        expect(filler).to.be.ok;
+    });
+
+    it('should fill grids', () => {
+        const GRID = new Grid(Difficulty.easy);
+        filler.fill(GRID);
+        expect(GRID.isCurrentlyValid).to.be.true;
+        filler.acrossPlacement.forEach((placement) => {
+            const MATCH_INDEX = GRID.across.findIndex((acrossWord) => {
+                const WORD_LENGTH_MATCHES =
+                    acrossWord.value.length >= placement.minLength &&
+                    acrossWord.value.length <= placement.maxLength;
+                const PLACEMENT_MATCHES = placement.position.equals(acrossWord.position);
+                return WORD_LENGTH_MATCHES && PLACEMENT_MATCHES;
+            });
+            expect(MATCH_INDEX).to.be.gte(0, 'Placement requirement not met');
+        });
+    });
+
+});
