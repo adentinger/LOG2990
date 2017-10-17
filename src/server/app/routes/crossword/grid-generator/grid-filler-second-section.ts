bcd272abdfda57c12a54f97c6f387bc035c4ed92
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 19:17:33 -0400

    Implemented constructor for GridFillerSecondSection.

--- src/server/app/routes/crossword/grid-generator/grid-filler-second-section.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-second-section.ts
@@ -1,5 +1,23 @@
 import { GridFiller } from './grid-filler';
+import { Difficulty } from './grid';
+import { GridFillerWordPlacement as WordPlacement } from './grid-filler-word-placement';
+import { WordPosition } from './word-position';
 
 export class GridFillerSecondSection extends GridFiller {
+
+    constructor(difficulty: Difficulty) {
+        super(difficulty);
+        this.acrossWords = [
+            new WordPlacement(new WordPosition(3, 3), 3, 5),
+            new WordPlacement(new WordPosition(4, 3), 3, 3),
+            new WordPlacement(new WordPosition(5, 3), 3, 5),
+            new WordPlacement(new WordPosition(6, 3), 3, 3)
+        ];
+        this.verticalWords = [
+            new WordPlacement(new WordPosition(3, 3), 4, 4),
+            new WordPlacement(new WordPosition(3, 4), 4, 4),
+            new WordPlacement(new WordPosition(3, 5), 4, 4)
+        ];
+    }
 
 }
