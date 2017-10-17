c7268421ae00f43b0012d72d351e2f3b1d583e6d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 19:23:18 -0400

    Implemented GridFillerThirdSection constructor.

--- src/server/app/routes/crossword/grid-generator/grid-filler-third-section.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-third-section.ts
@@ -1,5 +1,22 @@
 import { GridFiller } from './grid-filler';
+import { Difficulty } from './grid';
+import { GridFillerWordPlacement as WordPlacement } from './grid-filler-word-placement';
+import { WordPosition } from './word-position';
 
 export class GridFillerThirdSection extends GridFiller {
+
+    constructor(difficulty: Difficulty) {
+        super(difficulty);
+        this.acrossWords = [
+            new WordPlacement(new WordPosition(7, 6), 3, 3),
+            new WordPlacement(new WordPosition(8, 6), 3, 3),
+            new WordPlacement(new WordPosition(9, 2), 8, 8)
+        ];
+        this.verticalWords = [
+            new WordPlacement(new WordPosition(7, 6), 3, 3),
+            new WordPlacement(new WordPosition(7, 7), 3, 3),
+            new WordPlacement(new WordPosition(7, 8), 3, 3)
+        ];
+    }
 
 }
