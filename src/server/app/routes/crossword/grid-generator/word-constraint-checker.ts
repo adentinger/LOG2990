aeca0ed59d0987d242292acd081fe77fdd5d8857
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 20:28:54 -0400

    Created two methods in WordConstraintChecker.

--- src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
+++ src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
@@ -1,3 +1,7 @@
+import { CharConstraint } from '../../../common/index';
+import { Grid } from './grid';
+import { WordPosition } from './word-position';
+
 export class WordConstraintChecker {
 
     private static INSTANCE = new WordConstraintChecker();
@@ -6,6 +10,14 @@ export class WordConstraintChecker {
 
     public static getInstance(): WordConstraintChecker {
         return WordConstraintChecker.INSTANCE;
+    }
+
+    public getAcrossWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
+        return null;
+    }
+
+    public getVerticalWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
+        return null;
     }
 
 }
