40608433ac529179dddaa34f3dc16cd31cf5a8c6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-18 16:15:02 -0400

    Added 'equals' method in Word.

--- src/server/app/routes/crossword/grid-generator/word.ts
+++ src/server/app/routes/crossword/grid-generator/word.ts
@@ -1,10 +1,18 @@
 import { WordPosition } from './word-position';
 
 export class Word {
+
     public value: string;
     public position: WordPosition;
+
     constructor(value: string, position: WordPosition) {
         this.value = value;
         this.position = position;
     }
+
+    public equals(that: Word): boolean {
+        return this.value === that.value &&
+               this.position.equals(that.position);
+    }
+
 }
