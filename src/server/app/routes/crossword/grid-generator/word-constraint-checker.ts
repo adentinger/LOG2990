ee882726732515bf3311acff8af47593bd14b603
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 21:33:40 -0400

    Implemented getAcrossWordConstraint.

--- src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
+++ src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
@@ -1,6 +1,7 @@
 import { CharConstraint } from '../../../common/index';
 import { Grid } from './grid';
 import { WordPosition } from './word-position';
+import { Word } from './word';
 
 export class WordConstraintChecker {
 
@@ -13,11 +14,52 @@ export class WordConstraintChecker {
     }
 
     public getAcrossWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
-        return null;
+        const CONSTRAINTS: CharConstraint[] = [];
+
+        let characterPosition = 0;
+        let shouldTryToFindCharAtNextColumn = true;
+        const CURRENT_POSITION = new WordPosition(position.row, position.column);
+
+        while (shouldTryToFindCharAtNextColumn) {
+
+            const WORD_THAT_CONTAINS_POSITION =
+                this.findVerticalWordThatContainsPosition(grid, CURRENT_POSITION);
+
+            const CHAR_FOUND = (WORD_THAT_CONTAINS_POSITION !== undefined);
+
+            if (CHAR_FOUND) {
+                const CHARACTER = WORD_THAT_CONTAINS_POSITION.value
+                                  .charAt(CURRENT_POSITION.row - WORD_THAT_CONTAINS_POSITION.position.row);
+                const CHAR_CONSTRAINT: CharConstraint = {
+                    char: CHARACTER,
+                    position: characterPosition
+                };
+                CONSTRAINTS.push(CHAR_CONSTRAINT);
+            }
+
+            shouldTryToFindCharAtNextColumn = CHAR_FOUND;
+            ++CURRENT_POSITION.column;
+            ++characterPosition;
+        }
+
+        return CONSTRAINTS;
     }
 
     public getVerticalWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
         return null;
+    }
+
+    private findVerticalWordThatContainsPosition(grid: Grid, position: WordPosition): Word {
+        return grid.vertical.find((word) => {
+            const MIN_ROW = word.position.row;
+            const MAX_ROW = MIN_ROW + word.value.length - 1;
+            const COLUMN = word.position.column;
+            const WORD_CONTAINS_POSITION =
+                COLUMN === position.column &&
+                position.row >= MIN_ROW &&
+                position.row <= MAX_ROW;
+            return WORD_CONTAINS_POSITION;
+        });
     }
 
 }
