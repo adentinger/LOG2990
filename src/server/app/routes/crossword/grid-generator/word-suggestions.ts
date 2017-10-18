88e9c7bb06339eb55db615c1123dcb62567c1f01
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-18 15:32:50 -0400

    Created methods for WordSuggestions.

--- src/server/app/routes/crossword/grid-generator/word-suggestions.ts
+++ src/server/app/routes/crossword/grid-generator/word-suggestions.ts
@@ -1,3 +1,18 @@
 export class WordSuggestions {
 
+    private suggestions: string[];
+
+    constructor(suggestions: string[]) {
+        this.suggestions = suggestions;
+    }
+
+    public get length(): number {
+        return this.suggestions.length;
+    }
+
+
+    public get randomSuggestion(): string {
+        return null;
+    }
+
 }
