e7ca75beff2a1fbb678e58b85266b9bc5122b37f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 20:17:53 -0400

    Fixed UnhandledPromiseRejectionWarning in GridGenerator.

--- src/server/app/routes/crossword/grid-generator.middleware.ts
+++ src/server/app/routes/crossword/grid-generator.middleware.ts
@@ -11,25 +11,25 @@ export class GridGeneratorMiddleWare {
 
     @Route('get', '/easy')
     public async getEasyGrid(req: express.Request, res: express.Response): Promise<void> {
-        res.status(HttpStatus.OK);
         res.json(await GridGenerator.getInstance()
                        .gridGeneration(new DifficultyEasy));
+        res.status(HttpStatus.OK);
         res.send();
     }
 
     @Route('get', '/normal')
     public async getNormalGrid(req: express.Request, res: express.Response): Promise<void> {
-        res.status(HttpStatus.OK);
         res.json(await GridGenerator.getInstance()
                        .gridGeneration(new DifficultyNormal()));
+        res.status(HttpStatus.OK);
         res.send();
     }
 
     @Route('get', '/hard')
     public async getHardGrid(req: express.Request, res: express.Response): Promise<void> {
-        res.status(HttpStatus.OK);
         res.json(await GridGenerator.getInstance()
                        .gridGeneration(new DifficultyHard()));
+        res.status(HttpStatus.OK);
         res.send();
     }
 
