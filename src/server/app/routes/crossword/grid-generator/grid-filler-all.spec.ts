0ffe05b2612f37e48da873310e9f8915bb920a72
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-18 17:46:30 -0400

    Refactored GridFiller specs.

---
+++ src/server/app/routes/crossword/grid-generator/grid-filler-all.spec.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { GridFillerFirstSection } from './grid-filler-first-section';
+import { Grid } from './grid';
+import { DifficultyEasy } from './difficulty-easy';
+import { NormalWordSuggestionsGetter } from './normal-word-suggestions-getter';
+import { GridFiller } from './grid-filler';
+import { Constructor } from '../../../common';
+import { GridFillerSecondSection } from './grid-filler-second-section';
+import { GridFillerThirdSection } from './grid-filler-third-section';
+import { GridFillerFourthSection } from './grid-filler-fourth-section';
+
+function testGridFiller<T extends GridFiller>(testName: string,
+                        constructor: Constructor<T>) {
+    describe(testName, () => {
+
+        let filler: T;
+
+        beforeEach(() => {
+            filler = new constructor(
+                new NormalWordSuggestionsGetter(new DifficultyEasy()));
+        });
+
+        it('should be created', () => {
+            expect(filler).to.be.ok;
+        });
+
+        it('should fill grids', () => {
+            const GRID = new Grid();
+            filler.fill(GRID);
+            filler.acrossPlacement.forEach((placement) => {
+                const MATCH_INDEX = GRID.across.findIndex((acrossWord) => {
+                    const WORD_LENGTH_MATCHES =
+                        acrossWord.value.length >= placement.minLength &&
+                        acrossWord.value.length <= placement.maxLength;
+                    const PLACEMENT_MATCHES = placement.position.equals(acrossWord.position);
+                    return WORD_LENGTH_MATCHES && PLACEMENT_MATCHES;
+                });
+                expect(MATCH_INDEX).to.be.gte(0, 'Placement requirement not met');
+            });
+        });
+
+    });
+}
+
+testGridFiller('GridFillerFirstSection', GridFillerFirstSection);
+testGridFiller('GridFillerSecondSection', GridFillerSecondSection);
+testGridFiller('GridFillerThirdSection', GridFillerThirdSection);
+testGridFiller('GridFillerFourthSection', GridFillerFourthSection);
