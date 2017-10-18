29fa6993d4e90892f427e8d49c4be99c99a31abd
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-17 21:47:42 -0400

    Major refactoring of WordConstraintChecker.

--- src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
+++ src/server/app/routes/crossword/grid-generator/word-constraint-checker.ts
@@ -14,22 +14,45 @@ export class WordConstraintChecker {
     }
 
     public getAcrossWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
+        return this.getWordConstraint(
+            grid.vertical,
+            position,
+            (wordPosition) => wordPosition.column,
+            (wordPosition) => wordPosition.row,
+            (wordPosition) => { ++wordPosition.column; });
+    }
+
+    public getVerticalWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
+        return null;
+    }
+
+    private getWordConstraint(words: Word[],
+                              position: WordPosition,
+                              iteratedAxisGetter: (position: WordPosition) => number,
+                              constantAxisGetter: (position: WordPosition) => number,
+                              incrementIteratedAxis: (position: WordPosition) => void): CharConstraint[] {
         const CONSTRAINTS: CharConstraint[] = [];
 
         let characterPosition = 0;
-        let shouldTryToFindCharAtNextColumn = true;
-        const CURRENT_POSITION = new WordPosition(position.row, position.column);
+        let shouldTryToFindNextChar = true;
+        const CURRENT_POSITION = new WordPosition(constantAxisGetter(position),
+                                                  iteratedAxisGetter(position));
 
-        while (shouldTryToFindCharAtNextColumn) {
+        while (shouldTryToFindNextChar) {
 
             const WORD_THAT_CONTAINS_POSITION =
-                this.findVerticalWordThatContainsPosition(grid, CURRENT_POSITION);
+                this.findWordThatContainsPosition(
+                    words,
+                    CURRENT_POSITION,
+                    constantAxisGetter,
+                    iteratedAxisGetter);
 
             const CHAR_FOUND = (WORD_THAT_CONTAINS_POSITION !== undefined);
 
             if (CHAR_FOUND) {
                 const CHARACTER = WORD_THAT_CONTAINS_POSITION.value
-                                  .charAt(CURRENT_POSITION.row - WORD_THAT_CONTAINS_POSITION.position.row);
+                                    .charAt(constantAxisGetter(CURRENT_POSITION) -
+                                            constantAxisGetter(WORD_THAT_CONTAINS_POSITION.position));
                 const CHAR_CONSTRAINT: CharConstraint = {
                     char: CHARACTER,
                     position: characterPosition
@@ -37,27 +60,26 @@ export class WordConstraintChecker {
                 CONSTRAINTS.push(CHAR_CONSTRAINT);
             }
 
-            shouldTryToFindCharAtNextColumn = CHAR_FOUND;
-            ++CURRENT_POSITION.column;
+            shouldTryToFindNextChar = CHAR_FOUND;
+            incrementIteratedAxis(CURRENT_POSITION);
             ++characterPosition;
         }
 
         return CONSTRAINTS;
     }
 
-    public getVerticalWordConstraint(grid: Grid, position: WordPosition): CharConstraint[] {
-        return null;
-    }
-
-    private findVerticalWordThatContainsPosition(grid: Grid, position: WordPosition): Word {
-        return grid.vertical.find((word) => {
-            const MIN_ROW = word.position.row;
-            const MAX_ROW = MIN_ROW + word.value.length - 1;
-            const COLUMN = word.position.column;
+    private findWordThatContainsPosition(words: Word[],
+                                         position: WordPosition,
+                                         constantAxisGetter: (position: WordPosition) => number,
+                                         iteratedAxisGetter: (position: WordPosition) => number): Word {
+        return words.find((word) => {
+            const MIN_CONSTANT_AXIS = constantAxisGetter(word.position);
+            const MAX_CONSTANT_AXIS = MIN_CONSTANT_AXIS + word.value.length - 1;
+            const ITERATED_AXIS = iteratedAxisGetter(word.position);
             const WORD_CONTAINS_POSITION =
-                COLUMN === position.column &&
-                position.row >= MIN_ROW &&
-                position.row <= MAX_ROW;
+                ITERATED_AXIS === iteratedAxisGetter(position) &&
+                constantAxisGetter(position) >= MIN_CONSTANT_AXIS &&
+                constantAxisGetter(position) <= MAX_CONSTANT_AXIS;
             return WORD_CONTAINS_POSITION;
         });
     }
