68a33a50fb11871bf181ed8b21973652bcf52443
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-18 16:11:50 -0400

    Made the test pass.

--- src/server/app/routes/crossword/grid-generator/word-caller.ts
+++ src/server/app/routes/crossword/grid-generator/word-caller.ts
@@ -11,46 +11,6 @@ export class WordCaller {
         return WordCaller.INSTANCE;
     }
 
-    public getRandomWordFrom(words: string[]): string {
-        const MIN = 0;
-        const MAX = words.length;
-        let randomIndex = words.length;
-        while (randomIndex >= words.length) {
-            randomIndex = (Math.floor(Math.random() * (MAX - MIN)) + MIN);
-        }
-        return words[randomIndex];
-    }
-
-    private noAccent(word: string) {
-        const accent = [
-            /[\300-\306]/g, /[\340-\346]/g, // A, a
-            /[\310-\313]/g, /[\350-\353]/g, // E, e
-            /[\314-\317]/g, /[\354-\357]/g, // I, i
-            /[\322-\330]/g, /[\362-\370]/g, // O, o
-            /[\331-\334]/g, /[\371-\374]/g, // U, u
-            /[\321]/g, /[\361]/g, // N, n
-            /[\307]/g, /[\347]/g, // C, c
-        ];
-        const noAccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
-
-        for (let i = 0; i < accent.length; i++) {
-            word = word.replace(accent[i], noAccent[i]);
-        }
-
-        return word;
-    }
-
-    private noApostropheAndDash(word: string) {
-        word = word.replace(/['-]/g, '');
-        return word;
-    }
-
-    public wordFormatting(word: string) {
-        word = this.noAccent(word);
-        word = this.noApostropheAndDash(word);
-        return word;
-    }
-
     public alreadyChoosen(wordToCheck: string, gridForVertical: Word[], gridForAcross: Word[]) {
         let alreadyChoosen = false;
         const allWords: string[] = [];
--- src/server/app/routes/crossword/grid-generator/word-suggestions.ts
+++ src/server/app/routes/crossword/grid-generator/word-suggestions.ts
@@ -19,11 +19,44 @@ export class WordSuggestions {
             while (randomIndex >= this.suggestions.length) {
                 randomIndex = (Math.floor(Math.random() * (MAX - MIN)) + MIN);
             }
-            return this.suggestions[randomIndex];
+            let word = this.suggestions[randomIndex];
+            word = this.formatWord(word);
+            return word;
         }
         else {
             throw new Error('Cannot get random suggestion: no suggestions');
         }
+    }
+
+    private formatWord(word: string): string {
+        word = this.noAccent(word);
+        word = this.noApostropheAndDash(word);
+        word = word.toLowerCase();
+        return word;
+    }
+
+    private noAccent(word: string) {
+        const accent = [
+            /[\300-\306\340-\346]/g, // A, a
+            /[\310-\313\350-\353]/g, // E, e
+            /[\314-\317\354-\357]/g, // I, i
+            /[\322-\330\362-\370]/g, // O, o
+            /[\331-\334\371-\374]/g, // U, u
+            /[\321\361]/g, // N, n
+            /[\307\347]/g, // C, c
+        ];
+        const noAccent = ['a', 'e', 'i', 'o', 'u', 'n', 'c'];
+
+        for (let i = 0; i < accent.length; i++) {
+            word = word.replace(accent[i], noAccent[i]);
+        }
+
+        return word;
+    }
+
+    private noApostropheAndDash(word: string) {
+        word = word.replace(/['-]/g, '');
+        return word;
     }
 
 }
