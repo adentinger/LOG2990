405e40f2f17dd74181d00fefe78da4cec1304d46
pascallacasse <pascal.lacasse@hotmail.fr>
2017-10-19 13:56:33 -0400

    fixed bug in game manager

--- src/server/app/routes/crossword/games/game-manager.ts
+++ src/server/app/routes/crossword/games/game-manager.ts
@@ -72,7 +72,7 @@ export class GameManager {
         const gameToJoin = event.value.gameId;
         const playerSocketId = event.socketid;
 
-        console.debug('Player from socket ' + playerSocketId + ' requesting to join game: ' + gameToJoin);
+        //console.debug('Player from socket ' + playerSocketId + ' requesting to join game: ' + gameToJoin);
         this.addPlayerToGame(event.socketid, gameToJoin);
 
         // send all definitions
