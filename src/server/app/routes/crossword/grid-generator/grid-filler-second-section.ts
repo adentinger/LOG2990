dff630cc9a59cff499c1553625b178019db8cc12
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-10-19 18:20:28 -0400

    Made the change that improved the second section

--- src/server/app/routes/crossword/grid-generator/grid-filler-second-section.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-second-section.ts
@@ -10,12 +10,12 @@ export class GridFillerSecondSection extends GridFiller {
         this.acrossWords = [
             new WordPlacement(new WordPosition(3, 3), 3, 5),
             new WordPlacement(new WordPosition(4, 3), 3, 3),
-            new WordPlacement(new WordPosition(5, 3), 3, 5),
+            new WordPlacement(new WordPosition(5, 5), 3, 3),
             new WordPlacement(new WordPosition(6, 3), 3, 3)
         ];
         this.verticalWords = [
             new WordPlacement(new WordPosition(3, 3), 4, 5),
-            new WordPlacement(new WordPosition(3, 4), 4, 5),
+            new WordPlacement(new WordPosition(2, 3), 3, 3),
             new WordPlacement(new WordPosition(3, 5), 4, 4)
         ];
     }
