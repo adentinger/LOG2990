e48cf892fa7d59260714bad51cf324f8a70b12c8
pascallacasse <pascal.lacasse@hotmail.fr>
2017-10-19 12:03:11 -0400

    changed definition mock in server

--- src/server/app/routes/crossword/mocks/definitions-mock.ts
+++ src/server/app/routes/crossword/mocks/definitions-mock.ts
@@ -1,32 +1,78 @@
 import { Definition } from '../../../common/crossword/definition';
+import { Direction } from '../../../common/crossword/crossword-enums';
 
-export const DEFINITIONS_MOCK: Definition[] = [
+export const DEFINITIONS_MOCK_H: Definition[] = [
     {
         text:
-        `has feathers but cannot fly`
+        `has feathers but cannot fly`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `can fly but has no feathers`
+        `can fly but has no feathers`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `can neither fly nor grow feathers`
+        `can neither fly nor grow feathers`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `can grow feathers but wont because of unconventinal political beliefs`
+        `can grow feathers but wont because of unconventinal political beliefs`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `can fly but only once`
+        `can fly but only once`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `Has feathers, a beak, can fly but is not an animal`
+        `Has feathers, a beak, can fly but is not an animal`,
+        direction: Direction.horizontal
     },
     {
         text:
-        `Is a far cousin of my niece but doesn't know it`
+        `Is a far cousin of my niece but doesn't know it`,
+        direction: Direction.horizontal
+    }
+];
+
+export const DEFINITIONS_MOCK_V: Definition[] = [
+    {
+        text:
+        `has feathers but cannot fly`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `can fly but has no feathers`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `can neither fly nor grow feathers`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `can grow feathers but wont because of unconventinal political beliefs`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `can fly but only once`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `Has feathers, a beak, can fly but is not an animal`,
+        direction: Direction.vertical
+    },
+    {
+        text:
+        `Is a far cousin of my niece but doesn't know it`,
+        direction: Direction.vertical
     }
 ];
