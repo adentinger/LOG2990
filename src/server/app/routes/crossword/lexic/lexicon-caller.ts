b3380ee282b3033368b0b3d0df7214a22aee3a73
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-19 00:09:13 -0400

    Added doWordsExist to LexiconCaller.

--- src/server/app/routes/crossword/lexic/lexicon-caller.ts
+++ src/server/app/routes/crossword/lexic/lexicon-caller.ts
@@ -1,4 +1,5 @@
 import * as http from 'http';
+import { CharConstraint, HttpStatus } from '../../../common';
 
 // This class gets words from the Lexical microservice.
 
@@ -13,8 +14,9 @@ export class LexiconCaller {
     }
 
     public getWords(minLength: number,
-                    maxLength: number = 100, isCommon: boolean = true,
-                    charConstraints: {char: string, position: number}[] = []): Promise<string[]> {
+                    maxLength: number = 100,
+                    isCommon: boolean = true,
+                    charConstraints: CharConstraint[] = []): Promise<string[]> {
         return new Promise((resolve, reject) => {
             const URL = 'http://localhost:3000/crossword/lexic/words?' +
                         'minLength='  + minLength +
@@ -25,11 +27,37 @@ export class LexiconCaller {
                 let data = '';
                 response.on('data', (chunk) => data += chunk);
                 response.on('end', () => {
-                    resolve(JSON.parse(data));
+                    try {
+                        resolve(JSON.parse(data));
+                    }
+                    catch (e) {
+                        reject();
+                    }
                 });
                 response.on('error', reject);
             });
         });
     }
 
+    public doWordsExist(minLength: number,
+                        maxLength: number = 100,
+                        isCommon: boolean = true,
+                        charConstraints: CharConstraint[] = []): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const URL = 'http://localhost:3000/crossword/lexic/words/check?' +
+                        'minLength='  + minLength +
+                        '&maxLength=' + maxLength +
+                        '&isCommon='  + isCommon  +
+                        '&charConstraints=' + JSON.stringify(charConstraints);
+            http.get(URL, (response: http.IncomingMessage) => {
+                response.on('error', reject);
+                const STATUS_CODE: HttpStatus = response.statusCode;
+                switch (STATUS_CODE) {
+                    case HttpStatus.OK: resolve(true); break;
+                    case HttpStatus.NOT_FOUND: resolve(false); break;
+                    default: reject('Unexected http status code: ' + STATUS_CODE); break;
+                }
+            });
+        });
+    }
 }
