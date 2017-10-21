72beb74f9144eb704265b1778b6c470f4a848bcb
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-20 21:14:02 -0400

    Added method makeDocumentFrom to GridBank, and added a GridState enum.

--- src/server/app/routes/crossword/grid-bank/grid-bank.ts
+++ src/server/app/routes/crossword/grid-bank/grid-bank.ts
@@ -6,11 +6,17 @@ import { NormalWordSuggestionsGetter } from '../grid-generator/normal-word-sugge
 import { Difficulty } from '../../../../../common/src/crossword/difficulty';
 import { provideDatabase } from '../../../app-db';
 
+enum GridState {
+    GENERATING = 0,
+    READY,
+    CANCELLED
+}
+
 export abstract class GridBank {
 
-    public static readonly COLLECTION_NAME = 'grid-banks';
-    private static bank: Collection = GridBank.initializeBank();
     public static readonly NUMBER_OF_GRIDS = 5;
+    private static readonly COLLECTION_NAME = 'grid-banks';
+    private static bank: Collection = GridBank.initializeBank();
 
     private static initializeBank(): Collection {
         provideDatabase().then((db: Db) => {
@@ -46,6 +52,11 @@ export abstract class GridBank {
 
     private addGridToBank(): void {
         // TODO
+    }
+
+    private makeDocumentFrom(grid: Grid, state: GridState) {
+        const STATEFUL_GRID = {...grid, state: state};
+        return STATEFUL_GRID;
     }
 
 }
