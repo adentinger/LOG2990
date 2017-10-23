2112b9ad708a2f1a4f6459cdb14d33e9ac81423d
Eric Chao <hon-leung-eric.chao@polymtl.ca>
2017-10-23 15:03:20 -0400

    Implementation of puthole, puddle, speedBooster classes outline

---
+++ src/client/src/app/racing/racing-game/obstacles/pothole.ts
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+export class Pothole extends THREE.Mesh {
+
+    private currentPothole: THREE.Mesh;
+
+    constructor () {
+        super();
+    }
+
+    private static createPothole(): THREE.Mesh {
+        return null;
+    }
+
+    private static putTexture() {
+        return null;
+    }
+}
---
+++ src/client/src/app/racing/racing-game/obstacles/puddle.ts
@@ -0,0 +1,18 @@
+import * as THREE from 'three';
+
+export class Puddle extends THREE.Mesh {
+
+    private currentPuddle: THREE.Mesh;
+
+    constructor () {
+        super();
+    }
+
+    private static createPuddle(): THREE.Mesh {
+        return null;
+    }
+
+    private static putTexture() {
+        return null;
+    }
+}
