9722ab4372712f1323a9f28b75060cd2429d938b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-25 21:56:15 -0400

    Replace model with a new one, and used JSONLoader.

--- src/client/src/app/racing/racing-game/three-objects/car.ts
+++ src/client/src/app/racing/racing-game/three-objects/car.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 
 import { ObjMtlLoader } from '../three-utils/obj-mtl-loader';
-
+import { Logger } from '../../../../../../common/src/index';
 
 export enum CarColor {
     RED = 0,
@@ -12,17 +12,35 @@ export enum CarColor {
 
 export class Car extends THREE.Group {
 
-    private static readonly OBJ_MTL_LOADER: ObjMtlLoader
-        = new ObjMtlLoader('/assets/racing/car_model/');
+    private static readonly JSON_LOADER: THREE.JSONLoader = new THREE.JSONLoader();
+    private static readonly BASE_PATH = 'assets/racing/car_model/';
+    private static readonly FILE_EXTENSION = '.json';
 
-    constructor(carNumber: CarColor) {
+    private logger = Logger.getLogger('Car');
+
+    constructor(carColor: CarColor) {
         super();
-        Car.OBJ_MTL_LOADER.load(
-            'Low-Poly-Racing-Car.obj',
-            'Low-Poly-Racing-Car.mtl'
-        ).then((car) => {
-            this.children = car.children;
-        }).catch(() => {});
+        const loader = new THREE.JSONLoader();
+        this.addCarParts(carColor);
+    }
+
+    private async addCarParts(color: CarColor): Promise<void> {
+
+    }
+
+    private loadCarPart(name: string): Promise<THREE.Mesh> {
+        return new Promise((resolve, reject) => {
+            Car.JSON_LOADER.load(
+                Car.BASE_PATH + name + Car.FILE_EXTENSION,
+                (geometry, materials) => {
+                    const CAR_PART = new THREE.Mesh(geometry, materials[0]);
+                    console.log(CAR_PART);
+                    resolve(CAR_PART);
+                },
+                () => {},
+                (reason) => this.logger.warn(reason)
+            );
+        });
     }
 
 }
