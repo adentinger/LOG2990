fb2ca38e53e4c11d7d554d26d724dcec6e439f23
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-25 23:11:23 -0400

    Created util function applyMixins.

---
+++ src/client/src/app/util/mixin-apply.ts
@@ -0,0 +1,9 @@
+// From https://www.typescriptlang.org/docs/handbook/mixins.html
+
+export function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+            derivedCtor.prototype[name] = baseCtor.prototype[name];
+        });
+    });
+}
