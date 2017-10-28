75317fcdb47f64d47627cdd05b8b57ad6ce93e51
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-27 22:36:01 -0400

    Created class DayModeManager.

---
+++ src/client/src/app/racing/racing-game/day-mode-manager.ts
@@ -0,0 +1,18 @@
+export enum DayMode {
+    DAY = 0,
+    NIGHT
+}
+
+export class DayModeManager {
+
+    private mode: DayMode;
+
+    public setMode(mode: DayMode): void {
+
+    }
+
+    public updateScene(scene: THREE.Scene): void {
+
+    }
+
+}
