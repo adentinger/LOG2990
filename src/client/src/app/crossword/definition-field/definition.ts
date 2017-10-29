6509e35b1180028057bbdf548931070b7cb2e547
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-29 18:22:34 -0400

    Created class Definition.

---
+++ src/client/src/app/crossword/definition-field/definition.ts
@@ -0,0 +1,8 @@
+import { Direction } from '../../../../../common/src/crossword/crossword-enums';
+
+export class Definition {
+
+    constructor(public index: number = -1,
+                public direction: Direction = null) {}
+
+}
