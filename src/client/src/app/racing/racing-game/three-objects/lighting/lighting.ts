2d58f583b6dda12471ca2fb4222c16433dc7ff22
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-29 13:05:02 -0400

    Added cool class comment to Lighting.

--- src/client/src/app/racing/racing-game/three-objects/lighting/lighting.ts
+++ src/client/src/app/racing/racing-game/three-objects/lighting/lighting.ts
@@ -3,6 +3,11 @@ import * as THREE from 'three';
 import { DayModeNotifiable } from '../../day-mode/day-mode-notifiable';
 import { DayMode } from '../../day-mode/day-mode';
 
+/**
+ * Class which manages the scene's lighting.
+ * The scene's lighting is a three-point lighting:
+ * https://en.wikipedia.org/wiki/Three-point_lighting
+ */
 export class Lighting extends THREE.Object3D implements DayModeNotifiable {
 
     public dayModeChanged(newMode: DayMode): void {
