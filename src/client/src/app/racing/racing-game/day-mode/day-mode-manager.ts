c40a4f89fbcd7c23c9083d8a7eb252964856c52d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-28 21:35:14 -0400

    Implemented updateScene.

--- src/client/src/app/racing/racing-game/day-mode/day-mode-manager.ts
+++ src/client/src/app/racing/racing-game/day-mode/day-mode-manager.ts
@@ -1,3 +1,5 @@
+import { isDayModeNotifiable, DayModeNotifiable } from './day-mode-notifiable';
+
 export enum DayMode {
     DAY = 0,
     NIGHT
@@ -8,6 +10,13 @@ export class DayModeManager {
     public mode: DayMode = DayMode.DAY;
 
     public updateScene(scene: THREE.Scene): void {
+
+        scene.children.forEach((child) => {
+            if (isDayModeNotifiable(child)) {
+                const DAY_MODE_NOTIFIABLE_OBJECT: DayModeNotifiable = child;
+                DAY_MODE_NOTIFIABLE_OBJECT.dayModeChanged(this.mode);
+            }
+        });
 
     }
 
