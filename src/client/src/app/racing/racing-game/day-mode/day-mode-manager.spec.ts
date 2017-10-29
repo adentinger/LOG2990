37c488c3f99ea53dbc218a90f900758764c08918
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-28 20:45:16 -0400

    Moved DayModeManager and spec file to new directory.

--- src/client/src/app/racing/racing-game/day-mode/day-mode-manager.spec.ts
+++ src/client/src/app/racing/racing-game/day-mode/day-mode-manager.spec.ts
@@ -1,6 +1,25 @@
+import * as THREE from 'three';
 import { inject, TestBed } from '@angular/core/testing';
 
 import { DayModeManager, DayMode } from './day-mode-manager';
+
+interface ThreeData {
+    renderer: THREE.Renderer;
+    scene: THREE.Scene;
+    camera: THREE.Camera;
+}
+
+class ThreeDataFactory {
+    public make(): ThreeData {
+        return {
+            renderer: new THREE.WebGLRenderer(),
+            scene: new THREE.Scene(),
+            camera: new THREE.PerspectiveCamera()
+        };
+    }
+}
+
+const THREE_DATA_FACTORY = new ThreeDataFactory();
 
 describe('DayModeManager', () => {
 
@@ -13,14 +32,11 @@ describe('DayModeManager', () => {
     });
 
     let dayModeManager: DayModeManager;
-    let threeData: {
-        renderer: THREE.Renderer;
-        scene: THREE.Scene;
-        camera: THREE.Camera;
-    };
+    let threeData: ThreeData;
 
     beforeEach(inject([DayModeManager], (testedInstance) => {
         dayModeManager = testedInstance;
+        threeData = THREE_DATA_FACTORY.make();
     }));
 
     it('should be created', () => {
@@ -29,7 +45,7 @@ describe('DayModeManager', () => {
     });
 
     it('should update a scene\'s DayModeNotifiable elements', () => {
-        
+
     });
 
 });
