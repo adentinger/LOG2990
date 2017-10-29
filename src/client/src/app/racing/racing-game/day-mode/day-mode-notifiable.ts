193b5cf6a2395f896765b95275f6b789308b4aef
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-28 21:14:28 -0400

    Created function isDayModeNotifiable.

--- src/client/src/app/racing/racing-game/day-mode/day-mode-notifiable.ts
+++ src/client/src/app/racing/racing-game/day-mode/day-mode-notifiable.ts
@@ -5,3 +5,7 @@ export interface DayModeNotifiable extends THREE.Object3D {
     dayModeChanged(newMode: DayMode): void;
 
 }
+
+export function isDayModeNotifiable(obj: any): obj is DayModeNotifiable {
+    return false;
+}
