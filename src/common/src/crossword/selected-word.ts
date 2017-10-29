dd2367168d4191bd01a82cc6aa97f2401642991c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-29 15:33:11 -0400

    Whitespace improvements to SelectedWord.

--- src/common/src/crossword/selected-word.ts
+++ src/common/src/crossword/selected-word.ts
@@ -1,10 +1,13 @@
 import { Direction } from './crossword-enums';
 
 export class SelectedWord {
+
     public direction: Direction;
     public index: number;
+
     constructor() {
         this.direction = 0;
         this.index = -1;
     }
+
 }
