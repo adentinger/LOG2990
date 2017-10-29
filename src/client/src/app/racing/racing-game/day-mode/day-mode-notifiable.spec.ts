5d33e6fcd39bf18a1d77232976af11b04b8a2b78
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-28 21:15:23 -0400

    Implemented tests for isDayModeNotifiable.

---
+++ src/client/src/app/racing/racing-game/day-mode/day-mode-notifiable.spec.ts
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+import { inject, TestBed } from '@angular/core/testing';
+
+import { DayModeNotifiable, isDayModeNotifiable } from './day-mode-notifiable';
+import { DayMode } from './day-mode-manager';
+
+class DayModeNotifiableImplementer extends THREE.Object3D implements DayModeNotifiable {
+
+    public callback: () => void;
+
+    public dayModeChanged(newMode: DayMode): void {
+        this.callback();
+    }
+
+}
+
+describe('DayModeNotifiable', () => {
+
+    describe('isDayModeNotifiable', () => {
+
+        it('should tell that a isDayModeNotifiable object is one', () => {
+            const OBJECT = new DayModeNotifiableImplementer();
+            expect(isDayModeNotifiable(OBJECT)).toBe(true);
+        });
+
+        it('should tell that a isDayModeNotifiable object is one', () => {
+            const OBJECT = new THREE.Object3D();
+            expect(isDayModeNotifiable(OBJECT)).toBe(false);
+        });
+
+    });
+
+});
