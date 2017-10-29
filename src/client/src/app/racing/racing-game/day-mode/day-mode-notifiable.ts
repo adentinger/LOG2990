ec71238f7aa8e4ce057d01e9beddc24bf203faad
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-28 21:27:15 -0400

    Implemented isDayModeNotifiable.

--- src/client/src/app/racing/racing-game/day-mode/day-mode-notifiable.ts
+++ src/client/src/app/racing/racing-game/day-mode/day-mode-notifiable.ts
@@ -7,5 +7,15 @@ export interface DayModeNotifiable extends THREE.Object3D {
 }
 
 export function isDayModeNotifiable(obj: any): obj is DayModeNotifiable {
-    return false;
+    const FUNCTIONS_TO_CHECK = [
+        { name: 'dayModeChanged', numParams: 1}
+    ];
+    let isDayModeNotifiable = (obj != null);
+    for (let i = 0; i < FUNCTIONS_TO_CHECK.length && isDayModeNotifiable; ++i) {
+        const FUNCTION_TO_CHECK = FUNCTIONS_TO_CHECK[i];
+        isDayModeNotifiable =
+            FUNCTION_TO_CHECK.name in obj &&
+            obj[FUNCTION_TO_CHECK.name].length === FUNCTION_TO_CHECK.numParams;
+    }
+    return isDayModeNotifiable;
 }
