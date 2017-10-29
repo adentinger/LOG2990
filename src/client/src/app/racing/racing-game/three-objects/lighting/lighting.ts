660fa765f5eae7d47c4751ca4fdcbc7e5f3f3482
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-29 13:11:45 -0400

    Made class Lighting create three directional lights.

--- src/client/src/app/racing/racing-game/three-objects/lighting/lighting.ts
+++ src/client/src/app/racing/racing-game/three-objects/lighting/lighting.ts
@@ -10,6 +10,18 @@ import { DayMode } from '../../day-mode/day-mode';
  */
 export class Lighting extends THREE.Object3D implements DayModeNotifiable {
 
+    private readonly KEY_LIGHT: THREE.DirectionalLight;
+    private readonly FILL_LIGHT: THREE.DirectionalLight;
+    private readonly BACK_LIGHT: THREE.DirectionalLight;
+
+    constructor() {
+        super();
+        this.KEY_LIGHT  = new THREE.DirectionalLight(0x000000, 0);
+        this.FILL_LIGHT = new THREE.DirectionalLight(0x000000, 0);
+        this.BACK_LIGHT = new THREE.DirectionalLight(0x000000, 0);
+        this.add(this.KEY_LIGHT, this.FILL_LIGHT, this.BACK_LIGHT);
+    }
+
     public dayModeChanged(newMode: DayMode): void {
 
     }
