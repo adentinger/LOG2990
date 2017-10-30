8f4bfdb39c661317056daf0685d15a890ae6e036
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-30 18:26:40 -0400

    Created empty implementation of Grid.

--- src/client/src/app/crossword/board/grid.ts
+++ src/client/src/app/crossword/board/grid.ts
@@ -1,3 +1,18 @@
+import { GridWord } from '../../../../../common/src/crossword/grid-word';
+import { ViewableGrid } from './viewable-grid';
+
 export class Grid {
+
+    private words: GridWord[] = [];
+
+    public addWord(word: GridWord): void {
+    }
+
+    public empty(): void {
+    }
+
+    public computeViewableGrid(): ViewableGrid {
+        return null;
+    }
 
 }
