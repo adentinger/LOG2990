ac104e8e317f7d60d1c9ac392f5c03907720979a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-30 19:37:34 -0400

    Added tiles in Grid.

--- src/common/src/grid.ts
+++ src/common/src/grid.ts
@@ -2,6 +2,8 @@ import { Word } from './word';
 
 export class Grid {
     public static readonly DIMENSIONS = 10;
+    public static readonly BLACK_TILE = '0';
+    public static readonly EMPTY_TILE = ' ';
     public horizontalWords: Word[];
     public verticalWords: Word[];
 }
