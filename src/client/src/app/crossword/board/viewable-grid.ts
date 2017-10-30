8a339085e8411cd8ab894bf06e54cf9bc2122265
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-30 18:32:38 -0400

    Created empty implementation of ViewableGrid.

--- src/client/src/app/crossword/board/viewable-grid.ts
+++ src/client/src/app/crossword/board/viewable-grid.ts
@@ -1,3 +1,12 @@
+import { GridWord } from '../../../../../common/src/crossword/grid-word';
+
 export class ViewableGrid {
+
+    constructor(words: GridWord[] = []) {
+    }
+
+    public getCharAt(row: number, column: number): string {
+        return null;
+    }
 
 }
