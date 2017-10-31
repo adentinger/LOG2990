af3a137ff1aab409732732d17cc72ad6ae926d87
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-31 19:16:13 -0400

    Implemented TimerService and provided it in CrosswordComponent.

--- src/client/src/app/crossword/crossword.component.ts
+++ src/client/src/app/crossword/crossword.component.ts
@@ -3,12 +3,16 @@ import { SimpleTimer } from 'ng2-simple-timer';
 import { DefinitionsService } from './definition-field/definitions.service';
 import { BoardComponent } from './board/board.component';
 import { CrosswordGameService } from './crossword-game.service';
+import { TimerService } from './services/timer.service';
 
 @Component({
     selector: 'app-crossword',
     templateUrl: './crossword.component.html',
-    providers: [SimpleTimer,
-        DefinitionsService]
+    providers: [
+        SimpleTimer,
+        DefinitionsService,
+        TimerService
+    ]
 })
 export class CrosswordComponent implements OnInit {
 
