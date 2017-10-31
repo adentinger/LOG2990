192ef605cb362644e8092d656f3ee8643d2b778d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-31 18:08:15 -0400

    Created empty implemntaiton of GridWordParse.

--- src/common/src/crossword/packets/utils/grid-word-parse.ts
+++ src/common/src/crossword/packets/utils/grid-word-parse.ts
@@ -1,4 +1,17 @@
+import { GridWord } from '../../grid-word';
+
 export class GridWordParse {
+
+    public static parseFromBuffer(buffer: ArrayBuffer,
+                                  offset: number): GridWord {
+        return null;
+    }
+
+    public static serializeToBuffer(word: GridWord,
+                                    buffer: ArrayBuffer,
+                                    offset: number): void {
+        return null;
+    }
 
     private constructor() {}
 
