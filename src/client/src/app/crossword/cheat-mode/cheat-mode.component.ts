8d3f903b7828e958927249b8205af297be4ad4d8
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-31 16:06:28 -0400

    Refactored cheat mode related stuff into CheatModeComponent.

--- src/client/src/app/crossword/cheat-mode/cheat-mode.component.ts
+++ src/client/src/app/crossword/cheat-mode/cheat-mode.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import { CrosswordGameService } from '../crossword-game.service';
 
 @Component({
-  selector: 'app-cheat-mode',
-  templateUrl: './cheat-mode.component.html',
-  styleUrls: ['./cheat-mode.component.css']
+    selector: 'app-cheat-mode',
+    templateUrl: './cheat-mode.component.html',
+    styleUrls: ['./cheat-mode.component.css']
 })
 export class CheatModeComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('timerInput') private timerInput: ElementRef;
 
-  ngOnInit() {
-  }
+    constructor(private crosswordGameService: CrosswordGameService) { }
+
+    public ngOnInit() {
+    }
+
+    public onCheatModeToggle(): void {
+        this.crosswordGameService.setCheatModeOnOff();
+    }
+
+    public isCheatModeOn(): boolean {
+        return this.crosswordGameService.getCheatModeState();
+    }
+
+    public getCheatModeStateText(): string {
+        if (this.isCheatModeOn()) {
+            return 'Disable';
+        }
+        else {
+            return 'Enable';
+        }
+    }
+
+    public onShowWordsToggle(): void {
+        this.crosswordGameService.setShowWordsOnOff();
+    }
+
+    public isShowWordsOn(): boolean {
+        return this.crosswordGameService.getShowWordsState();
+    }
+
+    public getShowWordsStateText(): string {
+        if (this.isShowWordsOn()) {
+            return 'Hide words';
+        }
+        else {
+            return 'Show words';
+        }
+    }
+
+    public onTimerRunningToggle(): void {
+        this.crosswordGameService.setTimerOnOff();
+    }
+
+    public isTimerBeingSet(): boolean {
+        return this.crosswordGameService.getTimerState();
+    }
+
+    public getTimerStateText(): string {
+        if (this.isTimerBeingSet()) {
+            return 'Disable';
+        }
+        else {
+            return 'Set time';
+        }
+    }
+
+    public checkTimerInput(): void {
+        let input: string = this.timerInput.nativeElement.value;
+        input = input.replace(/[^0-9]/ig, '');
+        this.timerInput.nativeElement.value = input;
+    }
+
+    public changeTimerValue(): void {
+        this.checkTimerInput();
+        this.crosswordGameService.changeTimerValue(
+            this.timerInput.nativeElement.value
+        );
+    }
 
 }
--- src/client/src/app/crossword/crossword.component.ts
+++ src/client/src/app/crossword/crossword.component.ts
@@ -13,75 +13,14 @@ import { CrosswordGameService } from './crossword-game.service';
 export class CrosswordComponent implements OnInit {
 
     public gameIsBeingConfigured = true;
-    @ViewChild(BoardComponent) public gameBoard: BoardComponent;
-    @ViewChild('timerInput') private timerInput: ElementRef;
 
     constructor(private crosswordGameService: CrosswordGameService) { }
 
     public ngOnInit(): void {
     }
 
-    public onCheatModeToggle(): void {
-        this.crosswordGameService.setCheatModeOnOff();
-    }
-
-    public isCheatModeOn(): boolean {
-        return this.crosswordGameService.getCheatModeState();
-    }
-
-    public getCheatModeStateText(): string {
-        if (this.isCheatModeOn()) {
-            return 'Disable';
-        }
-        else {
-            return 'Enable';
-        }
-    }
-
-    public onShowWordsToggle(): void {
-        this.crosswordGameService.setShowWordsOnOff();
-    }
-
     public isShowWordsOn(): boolean {
         return this.crosswordGameService.getShowWordsState();
     }
 
-    public getShowWordsStateText(): string {
-        if (this.isShowWordsOn()) {
-            return 'Hide words';
-        }
-        else {
-            return 'Show words';
-        }
-    }
-
-    public onTimerRunningToggle(): void {
-        this.crosswordGameService.setTimerOnOff();
-    }
-
-    public isTimerBeingSet(): boolean {
-        return this.crosswordGameService.getTimerState();
-    }
-
-    public getTimerStateText(): string {
-        if (this.isTimerBeingSet()) {
-            return 'Disable';
-        }
-        else {
-            return 'Set time';
-        }
-    }
-
-    public checkTimerInput(): void {
-        let input: string = this.timerInput.nativeElement.value;
-        input = input.replace(/[^0-9]/ig, '');
-        this.timerInput.nativeElement.value = input;
-    }
-
-    public changeTimerValue(): void {
-        this.checkTimerInput();
-        this.crosswordGameService.changeTimerValue(
-            this.timerInput.nativeElement.value
-        );
-    }
 }
