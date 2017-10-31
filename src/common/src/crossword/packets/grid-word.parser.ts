f1e39a1eaaae6e4dc41f94d6dd102785136c1da7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-10-31 18:29:10 -0400

    Made use of GridWordParseUtil.

--- src/common/src/crossword/packets/grid-word.parser.ts
+++ src/common/src/crossword/packets/grid-word.parser.ts
@@ -1,7 +1,7 @@
 import { PacketParser } from '../../communication/packet-api';
 import { Parser, SIZE_UINT16, SIZE_UINT32 } from '../../communication/packet-api/packet-handler';
 import { GridWordPacket } from './grid-word.packet';
-import { GridWord } from '../grid-word';
+import { GridWordParseUtil } from './utils/grid-word-parse-util';
 
 @Parser(GridWordPacket)
 export class GridWordParser extends PacketParser<GridWordPacket> {
@@ -9,42 +9,12 @@ export class GridWordParser extends PacketParser<GridWordPacket> {
     public serialize(value: GridWordPacket): ArrayBuffer {
         const STRING_LENGTH = value.gridword.string.length;
         const BUFFER: ArrayBuffer = new ArrayBuffer(7 * SIZE_UINT32 + STRING_LENGTH * SIZE_UINT16);
-        const DATA = new DataView(BUFFER);
-
-        DATA.setInt32(0 * SIZE_UINT32, value.gridword.id);
-        DATA.setInt32(1 * SIZE_UINT32, value.gridword.y);
-        DATA.setInt32(2 * SIZE_UINT32, value.gridword.x);
-        // Be careful: this length is different from the actual string length
-        DATA.setInt32(3 * SIZE_UINT32, value.gridword.length);
-        DATA.setInt32(4 * SIZE_UINT32, value.gridword.direction);
-        DATA.setInt32(5 * SIZE_UINT32, value.gridword.owner);
-        DATA.setInt32(6 * SIZE_UINT32, STRING_LENGTH);
-
-        for (let i = 0; i < STRING_LENGTH; i++) {
-            DATA.setUint16(7 * SIZE_UINT32 + i * SIZE_UINT16, value.gridword.string.charCodeAt(i));
-        }
+        GridWordParseUtil.serializeToBuffer(value.gridword, BUFFER, 0);
         return BUFFER;
     }
 
     public parse(data: ArrayBuffer): GridWordPacket {
-        const VIEW = new DataView(data);
-        const id: number = VIEW.getInt32(0 * SIZE_UINT32);
-        const y: number = VIEW.getInt32(1 * SIZE_UINT32);
-        const x: number = VIEW.getInt32(2 * SIZE_UINT32);
-        const length: number = VIEW.getInt32(3 * SIZE_UINT32);
-        const direction: number = VIEW.getInt32(4 * SIZE_UINT32);
-        const owner: number = VIEW.getInt32(5 * SIZE_UINT32);
-        const stringLength: number = VIEW.getInt32(6 * SIZE_UINT32);
-
-        let buffer = '';
-
-        for (let i = 0; i < stringLength; i++) {
-
-            buffer += String.fromCharCode(VIEW.getUint16(7 * SIZE_UINT32 + i * SIZE_UINT16));
-        }
-
-        const newGridWord = new GridWord(id, y, x, length, direction, owner, buffer);
-        return new GridWordPacket(newGridWord);
+        return new GridWordPacket(GridWordParseUtil.parseFromBuffer(data, 0));
     }
 
 }
--- src/common/src/crossword/packets/word-try-result.parser.ts
+++ src/common/src/crossword/packets/word-try-result.parser.ts
@@ -1,12 +1,12 @@
 import { Parser, PacketParser, SIZE_UINT32, SIZE_UINT16 } from '../../index';
 import { WordTryResultPacket } from './word-try-result.packet';
 import { GridWord } from '../grid-word';
+import { GridWordParseUtil } from './utils/grid-word-parse-util';
 
 @Parser(WordTryResultPacket)
 export class WordTryResultParser extends PacketParser<WordTryResultPacket> {
 
     public serialize(value: WordTryResultPacket): ArrayBuffer {
-        console.log('HERE');
         let dataLength;
         if (value.found) {
             dataLength = 8 * SIZE_UINT32 + SIZE_UINT16 * value.word.string.length;
@@ -16,32 +16,10 @@ export class WordTryResultParser extends PacketParser<WordTryResultPacket> {
         }
         const BUFFER: ArrayBuffer = new ArrayBuffer(dataLength);
         const DATA = new DataView(BUFFER);
-        console.log('HERE', value);
 
         DATA.setUint8(0, value.found ? 1 : 0);
-        console.log('HERE', value.found);
         if (value.found) {
-            console.log('NOW HERE');
-            const STRING_LENGTH = value.word.string.length;
-            DATA.setInt32(1 * SIZE_UINT32, value.word.id);
-            DATA.setInt32(2 * SIZE_UINT32, value.word.y);
-            DATA.setInt32(3 * SIZE_UINT32, value.word.x);
-            console.log('HERE');
-            // Be careful: this length is different from the actual string length
-            DATA.setInt32(4 * SIZE_UINT32, value.word.length);
-            DATA.setInt32(5 * SIZE_UINT32, value.word.direction);
-            DATA.setInt32(6 * SIZE_UINT32, value.word.owner);
-            DATA.setInt32(7 * SIZE_UINT32, STRING_LENGTH);
-
-            console.log('HERE');
-            for (let i = 0; i < STRING_LENGTH; i++) {
-                DATA.setUint16(
-                    8 * SIZE_UINT32 + i * SIZE_UINT16,
-                    value.word.string.charCodeAt(i)
-                );
-                console.log('HERE');
-            }
-            console.log('THERE');
+            GridWordParseUtil.serializeToBuffer(value.word, BUFFER, SIZE_UINT32);
         }
         console.log(value);
         return BUFFER;
@@ -52,21 +30,7 @@ export class WordTryResultParser extends PacketParser<WordTryResultPacket> {
         const PACKET = new WordTryResultPacket();
         PACKET.found = VIEW.getUint8(0) !== 0 ? true : false;
         if (PACKET.found) {
-            PACKET.word = new GridWord();
-            PACKET.word.id = VIEW.getInt32(1 * SIZE_UINT32);
-            PACKET.word.y  = VIEW.getInt32(2 * SIZE_UINT32);
-            PACKET.word.x  = VIEW.getInt32(3 * SIZE_UINT32);
-            console.log('HERE');
-            // Be careful: this length is different from the actual string length
-            PACKET.word.length    = VIEW.getInt32(4 * SIZE_UINT32);
-            PACKET.word.direction = VIEW.getInt32(5 * SIZE_UINT32);
-            PACKET.word.owner     = VIEW.getInt32(6 * SIZE_UINT32);
-            const STRING_LENGTH   = VIEW.getInt32(7 * SIZE_UINT32);
-            let buffer = '';
-            for (let i = 0; i < STRING_LENGTH; i++) {
-                buffer += String.fromCharCode(VIEW.getUint16(i * SIZE_UINT16 + 8 * SIZE_UINT32));
-            }
-            PACKET.word.string = buffer;
+            PACKET.word = GridWordParseUtil.parseFromBuffer(data, SIZE_UINT32);
         }
         else {
             PACKET.word = null;
