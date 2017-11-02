0b3f76f7ecdb7a5f2125d853a8eab48f8bea5780
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-02 00:08:17 -0400

    Used one array for grid words rather than two maps. Did the same for definitions.

--- src/server/app/routes/crossword/games/crossword-game.ts
+++ src/server/app/routes/crossword/games/crossword-game.ts
@@ -8,7 +8,7 @@ import '../../../../../common/src/crossword/packets/crossword-timer.parser';
 import { PacketEvent, PacketHandler, registerHandlers } from '../../../../../common/src/index';
 import { Logger } from '../../../../../common/src/logger';
 import '../../../../../common/src/crossword/packets/word-try.parser';
-import { Direction, GameMode, Difficulty } from '../../../../../common/src/crossword/crossword-enums';
+import { GameMode, Difficulty } from '../../../../../common/src/crossword/crossword-enums';
 import { GridBanks } from '../grid-bank/grid-banks';
 import { Grid } from '../grid-generator/grid';
 
@@ -26,11 +26,9 @@ export class CrosswordGame {
 
     private packetManager: PacketManagerServer = PacketManagerServer.getInstance();
 
-    private horizontalGridWords: Map<number, GridWord> = new Map();
-    private verticalGridWords: Map<number, GridWord> = new Map();
+    private wordsInternal: GridWord[] = [];
 
-    private verticalDefinitions: Map<number, Definition> = new Map;
-    private horizontalDefinitions: Map<number, Definition> = new Map;
+    private definitionsInternal: Definition[] = [];
 
     private readonly playerIds: string[] = [];
 
@@ -59,25 +57,15 @@ export class CrosswordGame {
     }
 
     public get words(): GridWord[] {
-        const WORDS: GridWord[] = [];
-        this.horizontalGridWords.forEach((word) => {
-            WORDS.push(word);
-        });
-        this.verticalGridWords.forEach((word) => {
-            WORDS.push(word);
-        });
-        return WORDS;
+        return this.wordsInternal.slice();
     }
 
     public get definitionsWithIndex(): [Definition, number][] {
-        const DEFINITIONS: [Definition, number][] = [];
-        this.horizontalDefinitions.forEach((definition, index) => {
-            DEFINITIONS.push([definition, index]);
+        const DEFINITIONS_WITH_INDEX: [Definition, number][] = [];
+        this.definitionsInternal.forEach((definition, index) => {
+            DEFINITIONS_WITH_INDEX.push([definition, index]);
         });
-        this.verticalDefinitions.forEach((definition, index) => {
-            DEFINITIONS.push([definition, index]);
-        });
-        return DEFINITIONS;
+        return DEFINITIONS_WITH_INDEX;
     }
 
     public addPlayer(playerId: string): PlayerNumber {
@@ -115,10 +103,10 @@ export class CrosswordGame {
 
         /// MOCK : will get definitions from lexic by http requests
         for (let i = 0; i < DEFINITIONS_MOCK_V.length; i++) {
-            this.verticalDefinitions.set(i, DEFINITIONS_MOCK_V[i]);
+            this.definitionsInternal.push(DEFINITIONS_MOCK_V[i]);
         }
         for (let i = 0; i < DEFINITIONS_MOCK_H.length; i++) {
-            this.horizontalDefinitions.set(i, DEFINITIONS_MOCK_H[i]);
+            this.definitionsInternal.push(DEFINITIONS_MOCK_H[i]);
         }
     }
 
@@ -145,22 +133,20 @@ export class CrosswordGame {
     }
 
     public validateUserAnswer(wordTry: GridWord): boolean {
-        const index = wordTry.id;
-        const direction = wordTry.direction;
+        const ID = wordTry.id;
+        const DIRECTION = wordTry.direction;
+        const STRING = wordTry.string;
 
-        if (direction === Direction.horizontal &&
-            this.horizontalGridWords.get(index).string === wordTry.string) {
+        const FOUND = this.words.findIndex(
+            (word) => {
+                return word.id === ID &&
+                    word.direction === DIRECTION &&
+                    word.string === STRING;
+            }) >= 0;
+        if (FOUND) {
             this.countdown = COUNTDOWN_DEFAULT_VALUE;
-            return true;
         }
-        else if (direction === Direction.vertical &&
-            this.verticalGridWords.get(index).string === wordTry.string) {
-            this.countdown = COUNTDOWN_DEFAULT_VALUE;
-            return true;
-        }
-        else {
-            return false;
-        }
+        return FOUND;
     }
 
 }
