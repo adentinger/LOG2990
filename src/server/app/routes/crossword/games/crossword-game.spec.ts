1375fe36d148152c7b9f0c88ceb47972b5af2ec7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-01 22:32:21 -0400

    Implemented test for the addPlayerToGame method, since it's not public.

--- src/server/app/routes/crossword/games/crossword-game.spec.ts
+++ src/server/app/routes/crossword/games/crossword-game.spec.ts
@@ -22,9 +22,49 @@ describe('The Crossword Game', () => {
         done();
     });
 
-    it('should creates be able to provide basic informations', (done) => {
+    it('should be able to provide basic information', (done) => {
         expect(gameToTest.getGameInfo()).to.be.not.null;
         done();
+    });
+
+    describe('addPlayerToGame', () => {
+        it('should players to the game', () => {
+            const GAME = new CrosswordGame({
+                createJoin: CreateOrJoin.create,
+                difficulty: Difficulty.easy,
+                gameMode: GameMode.Classic,
+                playerNumber: 2
+            });
+            const PLAYER1 = 'asdf123';
+            const PLAYER2 = 'qwertyuiop';
+            GAME.addPlayerToGame(PLAYER1);
+            GAME.addPlayerToGame(PLAYER2);
+            expect(GAME.isPlayerInGame(PLAYER1)).to.be.true;
+            expect(GAME.isPlayerInGame(PLAYER2)).to.be.true;
+        });
+
+        it('should not add more players to the game than the max number of players', () => {
+            const GAME1PLAYER = new CrosswordGame({
+                createJoin: CreateOrJoin.create,
+                difficulty: Difficulty.easy,
+                gameMode: GameMode.Classic,
+                playerNumber: 1
+            });
+            const GAME2PLAYERS = new CrosswordGame({
+                createJoin: CreateOrJoin.create,
+                difficulty: Difficulty.easy,
+                gameMode: GameMode.Classic,
+                playerNumber: 2
+            });
+            const PLAYER1 = 'TAM ARAR';
+            const PLAYER2 = 'ERIC CHAO';
+            GAME1PLAYER.addPlayerToGame(PLAYER1);
+            expect(() => GAME1PLAYER.addPlayerToGame(PLAYER2)).to.throw;
+
+            GAME2PLAYERS.addPlayerToGame(PLAYER1);
+            GAME2PLAYERS.addPlayerToGame(PLAYER2);
+            expect(() => GAME2PLAYERS.addPlayerToGame('PASCAL LACASSE')).to.throw;
+        });
     });
 
     it('should tell whether a certain player is in the game', () => {
@@ -34,8 +74,8 @@ describe('The Crossword Game', () => {
             gameMode: GameMode.Classic,
             playerNumber: 2
         });
-        const PLAYER1 = 'asdf123';
-        const PLAYER2 = 'qwertyuiop';
+        const PLAYER1 = 'ADAM CÔTÉ';
+        const PLAYER2 = 'EMIR BELHADDAD';
         GAME.addPlayerToGame(PLAYER1);
         GAME.addPlayerToGame(PLAYER2);
         expect(GAME.isPlayerInGame(PLAYER1)).to.be.true;
