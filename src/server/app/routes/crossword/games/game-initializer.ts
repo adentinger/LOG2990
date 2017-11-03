c4d5bc21a3998d1cd2b2ca778c90d44151909592
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-03 17:03:10 -0400

    Made GameInitializer a singleton

--- src/server/app/routes/crossword/games/game-initializer.ts
+++ src/server/app/routes/crossword/games/game-initializer.ts
@@ -1,3 +1,11 @@
 export class GameInitializer {
 
+    private static readonly instance = new GameInitializer();
+
+    public static getInstance(): GameInitializer {
+        return GameInitializer.instance;
+    }
+
+    private constructor() {}
+
 }
