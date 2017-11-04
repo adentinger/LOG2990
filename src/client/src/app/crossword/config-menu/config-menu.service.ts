b98817481fc035eb9772c9143cd77d0ea40f7b2f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 16:06:38 -0400

    Added interface GameConfiguration to ConfigMenuService.

--- src/client/src/app/crossword/config-menu/config-menu.service.ts
+++ src/client/src/app/crossword/config-menu/config-menu.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { GameMode, CreateOrJoin, Difficulty } from '../../../../../common/src/crossword/crossword-enums';
+import { PlayerNumber, GameId } from '../../../../../common/src/communication/game-configs';
+
+export interface GameConfiguration {
+    gameMode: GameMode;
+    numberOfPlayers: PlayerNumber;
+    createOrJoin: CreateOrJoin;
+    gameToJoin: GameId;
+    difficulty: Difficulty;
+}
+
 @Injectable()
 export class ConfigMenuService {
 
