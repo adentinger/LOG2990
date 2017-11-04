7aa781e000135758778a05ea67f37f16a8318900
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 17:25:58 -0400

    Implemented spec of empty method.

--- src/client/src/app/crossword/config-menu/menu-automaton.service.spec.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.spec.ts
@@ -3,13 +3,36 @@ import { TestBed, inject } from '@angular/core/testing';
 import { MenuAutomatonService } from './menu-automaton.service';
 
 describe('MenuAutomatonService', () => {
+
     beforeEach(() => {
         TestBed.configureTestingModule({
         providers: [MenuAutomatonService]
         });
     });
 
-    it('should be created', inject([MenuAutomatonService], (service: MenuAutomatonService) => {
-        expect(service).toBeTruthy();
+    let menuAutomaton: MenuAutomatonService;
+
+    beforeEach(inject([MenuAutomatonService], (injectedMenuAutomaton) => {
+        menuAutomaton = injectedMenuAutomaton;
     }));
+
+    it('should be created', () => {
+        expect(menuAutomaton).toBeTruthy();
+    });
+
+    describe('chooseOption', () => {
+
+        it('should accept an existing option', () => {
+            const initialState = menuAutomaton.state;
+            menuAutomaton.chooseOption(menuAutomaton.state.options[0]);
+            expect(menuAutomaton.state).toBeTruthy();
+            expect(menuAutomaton.state).not.toEqual(initialState);
+        });
+
+        it('should not accept an option that does not exist', () => {
+            expect(() => menuAutomaton.chooseOption(null)).toThrow();
+        });
+
+    });
+
 });
