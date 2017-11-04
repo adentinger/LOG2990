f3e854f58e90689174158195b25a1c38a2560c2d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 16:10:56 -0400

    Fixed indentation of MenuAutomatonService and spec.

--- src/client/src/app/crossword/config-menu/menu-automaton.service.spec.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.spec.ts
@@ -3,13 +3,13 @@ import { TestBed, inject } from '@angular/core/testing';
 import { MenuAutomatonService } from './menu-automaton.service';
 
 describe('MenuAutomatonService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MenuAutomatonService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+        providers: [MenuAutomatonService]
+        });
     });
-  });
 
-  it('should be created', inject([MenuAutomatonService], (service: MenuAutomatonService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([MenuAutomatonService], (service: MenuAutomatonService) => {
+        expect(service).toBeTruthy();
+    }));
 });
--- src/client/src/app/crossword/config-menu/menu-automaton.service.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.ts
@@ -9,6 +9,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MenuAutomatonService {
 
-  constructor() { }
+    constructor() { }
 
 }
