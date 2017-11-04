a52044571181da92b8e24dce2991ab3cab14e213
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 16:34:15 -0400

    Added reference to FSA picture in MenuAutomatonService.

--- src/client/src/app/crossword/config-menu/menu-automaton.service.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.ts
@@ -5,6 +5,8 @@ import { Injectable } from '@angular/core';
  * @description Represents the Finite State Machine of the configuration menu.
  * For example, with choice '1' (2-player game) while in 'Player Number?' state,
  * we go to state 'Create or Join?'.
+ *
+ * See doc/architectures/ConfigurationMenu_FSA.jpg
  */
 @Injectable()
 export class MenuAutomatonService {
