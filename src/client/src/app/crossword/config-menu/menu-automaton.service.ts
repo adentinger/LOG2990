e885b72c054d61c82f020b3bf44b2690feca382a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 17:19:23 -0400

    Made MenuAutomatonService constructor

--- src/client/src/app/crossword/config-menu/menu-automaton.service.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.ts
@@ -17,12 +17,35 @@ export class MenuAutomatonService {
     private stateInternal: MenuState = null;
 
     constructor() {
+        // Create states
         const gameMode = new MenuState('Select game mode');
         const playerNumber = new MenuState('Select number of players');
         const difficulty = new MenuState('Select difficulty');
         const createOrJoin = new MenuState('Create or join game?');
         const chooseGame = new MenuState('Choose game');
         const confirm = new MenuState('Confirm choice?');
+
+        // Add state transitions
+        gameMode.addOption({name: 'Classic', nextState: playerNumber});
+        gameMode.addOption({name: 'Dynamic', nextState: playerNumber});
+
+        playerNumber.addOption({name: 'One player', nextState: difficulty});
+        playerNumber.addOption({name: 'Two players', nextState: createOrJoin});
+
+        difficulty.addOption({name: 'Easy', nextState: confirm});
+        difficulty.addOption({name: 'Normal', nextState: confirm});
+        difficulty.addOption({name: 'Hard', nextState: confirm});
+
+        createOrJoin.addOption({name: 'Create game', nextState: difficulty});
+        createOrJoin.addOption({name: 'Join game', nextState: chooseGame});
+
+        chooseGame.addOption({name: 'Done', nextState: confirm});
+
+        confirm.addOption({name: 'Start', nextState: MenuState.none});
+
+        // Setup things
+        this.stateInternal = gameMode;
+        this.states = [gameMode, playerNumber, difficulty, createOrJoin, chooseGame, confirm];
     }
 
     public get state(): MenuState {
