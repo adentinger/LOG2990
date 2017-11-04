8bd16ac373e268243fabf3b019eeb93c3bb440e4
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 16:10:23 -0400

    Added class comment for MenuAutomatonService.

--- src/client/src/app/crossword/config-menu/menu-automaton.service.ts
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * @class MenuAutomatonService
+ * @description Represents the Finite State Machine of the configuration menu.
+ * For example, with choice '1' (2-player game) while in 'Player Number?' state,
+ * we go to state 'Create or Join?'.
+ */
 @Injectable()
 export class MenuAutomatonService {
 
