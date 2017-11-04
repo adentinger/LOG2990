572330c7386ec5477f66e198e88d783c6bbf9e82
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-04 16:06:51 -0400

    Generated MenuAutomatonService.

---
+++ src/client/src/app/crossword/config-menu/menu-automaton.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { MenuAutomatonService } from './menu-automaton.service';
+
+describe('MenuAutomatonService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MenuAutomatonService]
+    });
+  });
+
+  it('should be created', inject([MenuAutomatonService], (service: MenuAutomatonService) => {
+    expect(service).toBeTruthy();
+  }));
+});
