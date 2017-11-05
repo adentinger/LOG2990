41927b1f58be0a2c4d8ccce01e39c08e1b6f619b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-05 17:18:16 -0500

    Implemented mock getGames method in GameService.

--- src/client/src/app/crossword/services/game.service.ts
+++ src/client/src/app/crossword/services/game.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { UserDisplayableGameData } from '../config-menu/available-games/user-displayable-game-data';
+
 @Injectable()
 export class GameService {
 
     constructor() { }
+
+    public getGames(): Promise<UserDisplayableGameData[]> {
+        return new Promise((resolve, reject) => resolve([]));
+    }
 
 }
