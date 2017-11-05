c1ed19c624b22a73a4dc22377a8f021cf1e79c59
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-05 18:11:24 -0500

    Implemented GamesMiddleWare.

--- src/server/app/routes/crossword/games/games.middleware.ts
+++ src/server/app/routes/crossword/games/games.middleware.ts
@@ -1,6 +1,15 @@
-import { MiddleWare } from '../../middle-ware';
+import * as express from 'express';
+
+import { MiddleWare, Route } from '../../middle-ware';
+import { GameManager } from './game-manager';
 
 @MiddleWare('/crossword/games')
 export class GamesMiddleWare {
+
+    @Route('get', '/')
+    public getGames(req: express.Request, res: express.Response, next: express.NextFunction): void {
+        const configurations = GameManager.getInstance().gamesConfigurations;
+        res.json(configurations);
+    }
 
 }
