7d76bc2fcc998080d52042715fc7cad43737b297
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-05 18:46:02 -0500

    Removed unused parameter in GamesMiddleWare.

--- src/server/app/routes/crossword/games/games.middleware.ts
+++ src/server/app/routes/crossword/games/games.middleware.ts
@@ -7,7 +7,7 @@ import { GameManager } from './game-manager';
 export class GamesMiddleWare {
 
     @Route('get', '/')
-    public getGames(req: express.Request, res: express.Response, next: express.NextFunction): void {
+    public getGames(req: express.Request, res: express.Response): void {
         const configurations = GameManager.getInstance().gamesConfigurations;
         res.json(configurations);
     }
