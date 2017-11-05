2c80e89c009e6143df44c038c22b6c64ed74fe1e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-05 15:25:21 -0500

    Provided MenuAutomatonService in ConfigMenuComponent, and subscribed to configEnd

--- src/client/src/app/crossword/config-menu/config-menu.component.ts
+++ src/client/src/app/crossword/config-menu/config-menu.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MenuAutomatonService } from './menu-automaton.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-config-menu',
     templateUrl: './config-menu.component.html',
-    styleUrls: ['./config-menu.component.css']
+    styleUrls: ['./config-menu.component.css'],
+    providers: [
+        MenuAutomatonService
+    ]
 })
-export class ConfigMenuComponent implements OnInit {
+export class ConfigMenuComponent implements OnInit, OnDestroy {
 
     public isConfiguringGame = true;
+    private subscriptions: Subscription[] = [];
 
-    constructor() { }
+    constructor(public menuAutomaton: MenuAutomatonService) {
+        const subscription = this.menuAutomaton.configEnd.subscribe(
+            () => this.isConfiguringGame = false
+        );
+        this.subscriptions.push(subscription);
+    }
 
     public ngOnInit(): void {
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach((subscription) => subscription.unsubscribe());
     }
 
     public log(...args): void {
