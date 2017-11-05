9093ef51919d459ae950c95eb4a74bf4ba44d948
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-05 17:03:41 -0500

    Added getters in UserDisplayableGameData.

--- src/client/src/app/crossword/config-menu/available-games/user-displayable-game-data.ts
+++ src/client/src/app/crossword/config-menu/available-games/user-displayable-game-data.ts
@@ -1,6 +1,11 @@
 import { CrosswordGameConfigs, GameId } from '../../../../../../common/src/communication/game-configs';
 import { GameMode, Difficulty } from '../../../../../../common/src/crossword/crossword-enums';
 
+/**
+ * @class UserDisplayableGameData
+ * @description Contains the data of a game as text so that it can be
+ * displayed to the user.
+ */
 export class UserDisplayableGameData {
 
     private idInternal: string;
@@ -9,6 +14,18 @@ export class UserDisplayableGameData {
 
     constructor(gameId: GameId, gameMode: GameMode, difficulty: Difficulty) {
 
+    }
+
+    public get id(): string {
+        return this.idInternal;
+    }
+
+    public get mode(): string {
+        return this.modeInternal;
+    }
+
+    public get difficulty(): string {
+        return this.difficultyInternal;
     }
 
 }
