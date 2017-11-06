0167788c6bbdfbac6cae850433d52f3f457be725
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-06 17:09:31 -0500

    Added class comment of UserChoiceService.

--- src/client/src/app/crossword/config-menu/user-choice.service.ts
+++ src/client/src/app/crossword/config-menu/user-choice.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * @class UserChoiceService
+ * @description Has the responsibility of containing the choices of the user.
+ */
 @Injectable()
 export class UserChoiceService {
 
-  constructor() { }
+    constructor() { }
 
 }
