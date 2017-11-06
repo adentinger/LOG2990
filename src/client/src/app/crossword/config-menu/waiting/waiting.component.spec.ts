16a5c94dec5de68529acefcd5e78b7bc0c826d7f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-06 08:12:02 -0500

    Fixes to indentation in WaitingComponent.

--- src/client/src/app/crossword/config-menu/waiting/waiting.component.spec.ts
+++ src/client/src/app/crossword/config-menu/waiting/waiting.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WaitingComponent } from './waiting.component';
 
 describe('WaitingComponent', () => {
-  let component: WaitingComponent;
-  let fixture: ComponentFixture<WaitingComponent>;
+    let component: WaitingComponent;
+    let fixture: ComponentFixture<WaitingComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WaitingComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ WaitingComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WaitingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WaitingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/client/src/app/crossword/config-menu/waiting/waiting.component.ts
+++ src/client/src/app/crossword/config-menu/waiting/waiting.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-waiting',
-  templateUrl: './waiting.component.html',
-  styleUrls: ['./waiting.component.css']
+    selector: 'app-waiting',
+    templateUrl: './waiting.component.html',
+    styleUrls: ['./waiting.component.css']
 })
 export class WaitingComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    public ngOnInit(): void {
+    }
 
 }
