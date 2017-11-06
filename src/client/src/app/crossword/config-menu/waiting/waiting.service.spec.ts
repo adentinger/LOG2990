2741785e63ddef8ce2ac119c09b2b2f0b87e9108
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-06 08:24:47 -0500

    Implemented spec for WaitingService.

--- src/client/src/app/crossword/config-menu/waiting/waiting.service.spec.ts
+++ src/client/src/app/crossword/config-menu/waiting/waiting.service.spec.ts
@@ -9,7 +9,29 @@ describe('WaitingService', () => {
         });
     });
 
-    it('should be created', inject([WaitingService], (service: WaitingService) => {
-        expect(service).toBeTruthy();
+    let waitingService: WaitingService;
+
+    beforeEach(inject([WaitingService], (waitingServiceInjected: WaitingService) => {
+        waitingService = waitingServiceInjected;
     }));
+
+    it('should be created', () => {
+        expect(waitingService).toBeTruthy();
+    });
+
+    it('should provide a subject on which we can send a new value', () => {
+        let hasBeenNotified = false;
+        waitingService.isWaiting.subscribe(() => hasBeenNotified = true);
+        waitingService.isWaiting.next(false);
+        expect(hasBeenNotified).toBe(true);
+    });
+
+    it('should keep the current value consistent with the emitted value', () => {
+        for (let i = 0; i < 10; ++i) {
+            const newValue = !waitingService.isWaitingValue;
+            waitingService.isWaiting.next(newValue);
+            expect(waitingService.isWaitingValue).toEqual(newValue);
+        }
+    });
+
 });
