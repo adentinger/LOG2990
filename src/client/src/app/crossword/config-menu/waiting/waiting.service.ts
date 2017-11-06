facef4bd8e199f9ab47b5e7f3b0bb00ea3b57905
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-06 08:19:33 -0500

    Empty implementation of WaitingService.

--- src/client/src/app/crossword/config-menu/waiting/waiting.service.ts
+++ src/client/src/app/crossword/config-menu/waiting/waiting.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
+/**
+ * @class WaitingService
+ * @description Has the responsibility of containing whether we are waiting,
+ * and notifying when we no longer wait.
+ */
 @Injectable()
 export class WaitingService {
 
+    private isWaitingInternal = new Subject<boolean>();
+    private isWaitingValueInternal: boolean;
+
     constructor() { }
+
+    public get isWaiting(): Subject<boolean> {
+        return null;
+    }
+
+    public get isWaitingValue(): boolean {
+        return this.isWaitingValue;
+    }
 
 }
