79f7f6ee7a3bb2ba5886435a091828e9a573bf52
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-07 19:14:07 -0500

    Implemented new route to get a game's words.

--- src/server/app/routes/crossword/games/games.middleware.ts
+++ src/server/app/routes/crossword/games/games.middleware.ts
@@ -3,6 +3,8 @@ import * as express from 'express';
 import { MiddleWare, Route } from '../../middle-ware';
 import { GameManager } from './game-manager';
 import { CrosswordGameConfigs } from '../../../../../common/src/communication/game-configs';
+import { GridWord } from '../../../../../common/src/crossword/grid-word';
+import { Direction, Owner } from '../../../../../common/src/crossword/crossword-enums';
 
 @MiddleWare('/crossword/games')
 export class GamesMiddleWare {
@@ -18,6 +20,19 @@ export class GamesMiddleWare {
         const configuration: CrosswordGameConfigs = req.body;
         const gameId = GameManager.getInstance().newGame(configuration);
         res.json(gameId);
+    }
+
+    @Route('get', '/:id/words')
+    public getWords(req: express.Request, res: express.Response): void {
+        const gameId = req.params.id;
+        console.log(gameId);
+        res.json([
+            new GridWord(1, 0, 0, 1, Direction.horizontal, Owner.none, 'a'),
+            new GridWord(2, 0, 2, 1, Direction.horizontal, Owner.none, 'b'),
+            new GridWord(1, 0, 3, 1, Direction.vertical,   Owner.none, 'c'),
+            new GridWord(3, 0, 4, 1, Direction.horizontal, Owner.none, 'd'),
+            new GridWord(2, 0, 5, 1, Direction.vertical,   Owner.none, 'e'),
+        ]);
     }
 
 }
