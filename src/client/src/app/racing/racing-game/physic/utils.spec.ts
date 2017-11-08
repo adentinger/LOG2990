8ba351878920144e19acdef88ac619c81ed5702e
Adam <adam.martin-cote@noviflow.com>
2017-11-08 17:49:55 -0500

    Physic testing
    simple root test

--- src/client/src/app/racing/racing-game/physic/utils.spec.ts
+++ src/client/src/app/racing/racing-game/physic/utils.spec.ts
@@ -2,9 +2,16 @@ import { TestBed, inject } from '@angular/core/testing';
 import { ConnectionBackend, Http, RequestOptions, BaseRequestOptions } from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
 import { PhysicUtils } from './utils';
+import { Collidable } from './collidable';
 
 import * as THREE from 'three';
 import { EventManager } from '../../../event-manager.service';
+import { PhysicMesh } from './object';
+import { Kilograms } from '../../types';
+import { SlipDirection, PuddleBox } from './examples/puddle-box';
+
+let eventManager: EventManager;
+let physicUtils: PhysicUtils;
 
 describe('Physic utils', () => {
     beforeEach(() => {
@@ -13,19 +20,15 @@ describe('Physic utils', () => {
                 EventManager
             ]
         });
-    });
-
-    let service: PhysicUtils;
-
-    beforeEach(() => {
-        service = new PhysicUtils(new EventManager());
+        eventManager = new EventManager();
+        physicUtils = new PhysicUtils(eventManager);
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(PhysicUtils).toBeTruthy();
     });
 
-    it('returns non-negative values for object dimensions', () => {
+    it('should returns non-negative values for object dimensions', () => {
         const dummyObject = new THREE.Object3D();
         const dimensionsMesured: THREE.Vector3 = PhysicUtils.getObjectDimensions(dummyObject);
         expect(dimensionsMesured.x >= 0);
@@ -33,7 +36,7 @@ describe('Physic utils', () => {
         expect(dimensionsMesured.z >= 0);
     });
 
-    it('return bigger dimension values for bigger objects. Yeah you read that right', () => {
+    it('should return bigger dimension values for bigger objects. Yeah you read that right', () => {
         const dummyObject1 = new THREE.Object3D();
         const dummyObject2 = new THREE.Object3D();
 
@@ -49,5 +52,12 @@ describe('Physic utils', () => {
         expect(dimensionsObject1.x < dimensionsObject2.x);
         expect(dimensionsObject1.y < dimensionsObject2.y);
         expect(dimensionsObject1.z < dimensionsObject2.z);
+    });
+
+    it('should properly set root', () => {
+        const dummyObject = new THREE.Object3D();
+        dummyObject.name = 'Franky';
+        physicUtils.setRoot(dummyObject);
+        expect(physicUtils['root'].name === 'Franky');
     });
 });
