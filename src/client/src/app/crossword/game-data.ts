1816689dbf98b6366b75a7877962d0a0fb9fe1aa
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-08 23:01:29 -0500

    Added attributes of GameData.

--- src/client/src/app/crossword/game-data.ts
+++ src/client/src/app/crossword/game-data.ts
@@ -1,3 +1,12 @@
+import { GameMode } from '../../../../common/src/crossword/crossword-enums';
+import { PlayerNumber, GameId } from '../../../../common/src/communication/game-configs';
+
 export class GameData {
+
+    constructor(public id: GameId,
+                public playerName: string,
+                public opponentName: string,
+                public mode: GameMode,
+                public numberOfPlayers: PlayerNumber) {}
 
 }
