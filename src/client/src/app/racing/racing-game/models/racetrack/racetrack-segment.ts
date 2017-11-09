756bf7ae54a0c8056c205e95dc54c4c6882afcba
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-11-08 20:58:36 -0500

    Removed code duplication

--- src/client/src/app/racing/racing-game/models/racetrack/racetrack-segment.ts
+++ src/client/src/app/racing/racing-game/models/racetrack/racetrack-segment.ts
@@ -1,32 +1,25 @@
 import * as THREE from 'three';
 import { Track } from '../../../track';
+import { loadTexture } from '../../../../util/textures';
 
 export class RacetrackSegment extends THREE.Mesh {
+    private static TEXTURE_LOADER = new THREE.TextureLoader();
+
     private static ASPHALT_URL = 'assets/racing/textures/ground_asphalt_old_07.png';
     private static ASPHALT_NORMALS_URL = 'assets/racing/normals/road_normal_map.jpg';
-    private static TEXTURE_LOADER = new THREE.TextureLoader();
-    private static ASPHALT_TEXTURE_PROMISE: Promise<THREE.Texture> = RacetrackSegment.loadTexture(RacetrackSegment.ASPHALT_URL);
-    private static ASPHALT_NORMALS_PROMISE: Promise<THREE.Texture> = RacetrackSegment.loadTexture(RacetrackSegment.ASPHALT_NORMALS_URL);
+
+    private static ASPHALT_TEXTURE_PROMISE: Promise<THREE.Texture> = loadTexture(RacetrackSegment.ASPHALT_URL);
+    private static ASPHALT_NORMALS_PROMISE: Promise<THREE.Texture> = loadTexture(RacetrackSegment.ASPHALT_NORMALS_URL);
 
     public readonly mass = 0;
 
     public readonly length;
-    public readonly waitToLoad: Promise<void> = RacetrackSegment.ASPHALT_TEXTURE_PROMISE.then(() => {});
-
-    private static loadTexture(url: string): Promise<THREE.Texture> {
-        return new Promise<THREE.Texture>((resolve, reject) => {
-            const texture = RacetrackSegment.TEXTURE_LOADER.load(url,
-                () => resolve(texture),
-                () => {},
-                reject
-            );
-        });
-    }
+    public readonly waitToLoad: Promise<void>;
 
     constructor(segmentLength: number) {
         super(new THREE.PlaneBufferGeometry(Track.SEGMENT_WIDTH, segmentLength));
         this.material = new THREE.MeshPhongMaterial({side: THREE.FrontSide, shininess: 100});
-        Promise.all([
+        this.waitToLoad = Promise.all([
             RacetrackSegment.ASPHALT_TEXTURE_PROMISE,
             RacetrackSegment.ASPHALT_NORMALS_PROMISE
         ]).then(([texture, normalMap]) => {
@@ -39,7 +32,7 @@ export class RacetrackSegment extends THREE.Mesh {
             (<THREE.MeshPhongMaterial>this.material).normalMap = normalMap;
             (<THREE.MeshPhongMaterial>this.material).bumpMap = normalMap;
             (<THREE.MeshPhongMaterial>this.material).bumpScale = 5;
-        });
+        }).then(() => {});
         this.rotation.x = 3 * Math.PI / 2;
         this.position.add(new THREE.Vector3(0, 0.02, 0)); // segment must be on top to support other textures
 
