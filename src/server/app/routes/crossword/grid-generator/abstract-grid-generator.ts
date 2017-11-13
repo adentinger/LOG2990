1db6936ba9a82936ed4da94946895b879d09b6cb
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-12 22:02:54 -0500

    Moved some implementation from abstract to specialized class.

--- src/server/app/routes/crossword/grid-generator/abstract-grid-generator.ts
+++ src/server/app/routes/crossword/grid-generator/abstract-grid-generator.ts
@@ -4,17 +4,12 @@ import { GridFillerSecondSection } from './grid-filler-second-section';
 import { GridFillerThirdSection } from './grid-filler-third-section';
 import { GridFillerFourthSection } from './grid-filler-fourth-section';
 import { AbstractWordSuggestionsGetter } from './abstract-word-suggestions-getter';
-import { Logger } from '../../../../../common/src/logger';
 import { GridWord } from '../../../../../common/src/crossword/grid-word';
 
-const logger = Logger.getLogger();
-
 export abstract class AbstractGridGenerator {
-    private static count = 0;
-
     protected constructor() { }
 
-    public async gridGenerationBase(wordsToInclude: GridWord[], suggestionsGetter: AbstractWordSuggestionsGetter): Promise<Grid> {
+    protected async gridGenerationBase(wordsToInclude: GridWord[], suggestionsGetter: AbstractWordSuggestionsGetter): Promise<Grid> {
         const GRID = new Grid();
         const FILLER_FIRST_SECTION = new GridFillerFirstSection(suggestionsGetter);
         const FILLER_SECOND_SECTION = new GridFillerSecondSection(suggestionsGetter);
@@ -24,8 +19,6 @@ export abstract class AbstractGridGenerator {
         await GRID.fillUsing(FILLER_SECOND_SECTION);
         await GRID.fillUsing(FILLER_THIRD_SECTION);
         await GRID.fillUsing(FILLER_FOURTH_SECTION);
-        logger.log('count: %d', ++AbstractGridGenerator.count);
-        logger.log(GRID.toString());
         return GRID;
     }
 }
--- src/server/app/routes/crossword/grid-generator/grid-generator.ts
+++ src/server/app/routes/crossword/grid-generator/grid-generator.ts
@@ -1,8 +1,15 @@
 import { AbstractGridGenerator } from './abstract-grid-generator';
+import { Grid } from './grid';
+import { NormalWordSuggestionsGetter } from './normal-word-suggestions-getter';
+import { Difficulty } from '../../../../../common/src/crossword/difficulty';
+import { Logger } from '../../../../../common/src/index';
 
 export class GridGenerator extends AbstractGridGenerator {
 
     private static readonly INSTANCE = new GridGenerator();
+    private static count = 0;
+
+    private logger = Logger.getLogger('GridGenerator');
 
     public static getInstance(): GridGenerator {
         return GridGenerator.INSTANCE;
@@ -10,6 +17,13 @@ export class GridGenerator extends AbstractGridGenerator {
 
     protected constructor() {
         super();
+    }
+
+    public async gridGeneration(difficulty: Difficulty): Promise<Grid> {
+        const grid = await super.gridGenerationBase([], new NormalWordSuggestionsGetter(difficulty));
+        this.logger.log('count: %d', ++GridGenerator.count);
+        this.logger.log(grid.toString());
+        return grid;
     }
 
 }
