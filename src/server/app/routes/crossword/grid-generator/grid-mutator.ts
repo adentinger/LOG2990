84478635bb42d8bf4339288fe596f0d3a2ed4248
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-12 22:11:34 -0500

    Added class comment fir GridMutator.

--- src/server/app/routes/crossword/grid-generator/grid-mutator.ts
+++ src/server/app/routes/crossword/grid-generator/grid-mutator.ts
@@ -1,5 +1,16 @@
 import { AbstractGridGenerator } from './abstract-grid-generator';
 
+/**
+ * @class GridMutator
+ * @description Has the responsibility of generating a single mutated grid.
+ * Unlike the GridGenerator, that we only need one instance of, multiple instances
+ * of the mutator can exist, since we might want to stop the generation of a
+ * mutated grid midway.
+ *
+ * You might say that this class is like a real-life factory that creates a single item.
+ * At any time you can ask the factory to throw away the item that's being produced,
+ * and then possibly ask to generate a new one instead.
+ */
 export class GridMutator extends AbstractGridGenerator {
 
     private static readonly INSTANCE = new GridMutator();
