1335a3708a1b1cd6ade1ec415034ee8605f42730
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-12 21:52:42 -0500

    Made GridGenerator singleton.

--- src/server/app/routes/crossword/grid-generator/grid-generator.ts
+++ src/server/app/routes/crossword/grid-generator/grid-generator.ts
@@ -2,4 +2,14 @@ import { AbstractGridGenerator } from './abstract-grid-generator';
 
 export class GridGenerator extends AbstractGridGenerator {
 
+    private static readonly _INSTANCE = new GridGenerator();
+
+    public static getInstance(): GridGenerator {
+        return GridGenerator._INSTANCE;
+    }
+
+    protected constructor() {
+        super();
+    }
+
 }
