c0f0be22c37249728668190ef50de0e6a5acc27c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-12 21:53:08 -0500

    Made GridMutator singleton.

--- src/server/app/routes/crossword/grid-generator/grid-mutator.ts
+++ src/server/app/routes/crossword/grid-generator/grid-mutator.ts
@@ -2,4 +2,14 @@ import { AbstractGridGenerator } from './abstract-grid-generator';
 
 export class GridMutator extends AbstractGridGenerator {
 
+    private static readonly _INSTANCE = new GridMutator();
+
+    public static getInstance(): GridMutator {
+        return GridMutator._INSTANCE;
+    }
+
+    protected constructor() {
+        super();
+    }
+
 }
