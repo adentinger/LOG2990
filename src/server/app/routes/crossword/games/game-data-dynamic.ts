8fd2096c8d207e8961b8cc45138e09260f10fe55
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-17 15:41:36 -0500

    Added a mutator in GameDataDynamic.

--- src/server/app/routes/crossword/games/game-data-dynamic.ts
+++ src/server/app/routes/crossword/games/game-data-dynamic.ts
@@ -1,16 +1,25 @@
 import { GameData } from './game-data';
 import { GridWord } from '../../../../../common/src/crossword/grid-word';
 import { Difficulty } from '../../../../../common/src/crossword/crossword-enums';
+import { GridMutator } from '../grid-generator/grid-mutator';
+import { toGridGeneratorDifficulty } from './temp-util';
 
 export class GameDataDynamic extends GameData {
 
+    private mutator: GridMutator;
+
     constructor(difficulty: Difficulty) {
         super(difficulty);
+        this.mutator = new GridMutator(toGridGeneratorDifficulty(difficulty));
     }
 
     public updateData(gridWords: GridWord[]): void {
         this.wordsInternal = gridWords;
         this.setDefinitions();
+    }
+
+    public get mutatedWords(): Promise<GridWord[]> {
+        return this.mutator.mutatedGrid.then(grid => grid.toGridWords());
     }
 
 }
