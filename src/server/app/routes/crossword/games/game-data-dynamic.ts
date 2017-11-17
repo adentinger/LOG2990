76318b6572c22ef39e348935ebbeace9426b7c3a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-17 13:58:07 -0500

    Created method updateData in GameDataDynamic.

--- src/server/app/routes/crossword/games/game-data-dynamic.ts
+++ src/server/app/routes/crossword/games/game-data-dynamic.ts
@@ -1,9 +1,14 @@
 import { GameData } from './game-data';
+import { GridWord } from '../../../../../common/src/crossword/grid-word';
 
 export class GameDataDynamic extends GameData {
 
     constructor() {
         super();
+    }
+
+    public updateData(gridWords: GridWord[]): void {
+        return null;
     }
 
 }
