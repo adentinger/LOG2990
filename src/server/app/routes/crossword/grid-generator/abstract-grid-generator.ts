0b60166c68ad9660256150cd9d98491b2231ca29
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-17 11:19:22 -0500

    Made generating a grid set an attribute in AbstractGridGenerator.

--- src/server/app/routes/crossword/grid-generator/abstract-grid-generator.ts
+++ src/server/app/routes/crossword/grid-generator/abstract-grid-generator.ts
@@ -13,32 +13,30 @@ export abstract class AbstractGridGenerator {
     protected constructor() { }
 
     protected gridGenerationBase(wordsToInclude: Word[], suggestionsGetter: AbstractWordSuggestionsGetter): Promise<Grid> {
-
-        this.latestGeneration = new Promise((resolve, reject) => {
-            const GRID = new Grid();
-
-            const FILLER_FIRST_SECTION = new GridFillerFirstSection(suggestionsGetter);
-            const FILLER_SECOND_SECTION = new GridFillerSecondSection(suggestionsGetter);
-            const FILLER_THIRD_SECTION = new GridFillerThirdSection(suggestionsGetter);
-            const FILLER_FOURTH_SECTION = new GridFillerFourthSection(suggestionsGetter);
-
-            GRID.fillUsing(FILLER_FIRST_SECTION)
-            .then(() => {
-                GRID.fillUsing(FILLER_SECOND_SECTION);
-            }).then(() => {
-                GRID.fillUsing(FILLER_THIRD_SECTION);
-            }).then(() => {
-                GRID.fillUsing(FILLER_FOURTH_SECTION);
-            });
-
-            return GRID;
-        });
-
+        this.latestGeneration = this.asyncGridGenerationBase(wordsToInclude, suggestionsGetter);
         return this.latestGeneration;
     }
 
+    private async asyncGridGenerationBase(wordsToInclude: Word[], suggestionsGetter: AbstractWordSuggestionsGetter): Promise<Grid> {
+        const GRID = new Grid();
+
+        const FILLER_FIRST_SECTION = new GridFillerFirstSection(suggestionsGetter);
+        const FILLER_SECOND_SECTION = new GridFillerSecondSection(suggestionsGetter);
+        const FILLER_THIRD_SECTION = new GridFillerThirdSection(suggestionsGetter);
+        const FILLER_FOURTH_SECTION = new GridFillerFourthSection(suggestionsGetter);
+
+        await GRID.fillUsing(FILLER_FIRST_SECTION);
+        await GRID.fillUsing(FILLER_SECOND_SECTION);
+        await GRID.fillUsing(FILLER_THIRD_SECTION);
+        await GRID.fillUsing(FILLER_FOURTH_SECTION);
+
+        return GRID;
+    }
+
     protected cancelLatestGeneration(): Promise<void> {
-        return this.latestGeneration.catch(() => { return; }).then(() => { return; });
+        return this.latestGeneration
+            .catch(() => { return; })
+            .then (() => { return; });
     }
 
 }
