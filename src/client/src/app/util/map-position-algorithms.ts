4b0d34b66e668cc2cdea68d33ae9511bdfe8b57d
Adam <adam.martin-cote@noviflow.com>
2017-11-17 10:44:29 -0500

    Game Progress
    projection algorithm structure set

--- src/client/src/app/util/map-position-algorithms.ts
+++ src/client/src/app/util/map-position-algorithms.ts
@@ -1,11 +1,27 @@
 import { Line } from '../../../../common/src/math/line';
+import * as THREE from 'three';
+import { Point } from '../../../../common/src/math/point';
 
 export class Projection {
-    public interpolation: number;
-    public segment: Line;
-    public d: number;
+    constructor(public readonly interpolation: number,
+        public readonly segment: Line,
+        public readonly distanceToSegment: number) { }
 }
 
 export class MapPositionAlgorithms {
 
+    public static getClosestProjection(position: THREE.Vector2, lines: Line[]): Projection {
+
+        return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
+    }
+
+    public static getProjectionOnLine(position: THREE.Vector2, lines: Line): Projection {
+
+        return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
+    }
+
+    public static getAllProjections(position: THREE.Vector2, lines: Line): Projection {
+
+        return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
+    }
 }
