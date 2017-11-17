ab33f0f197f27efdedf6dd8f06e6b07f8a54bab4
Adam <adam.martin-cote@noviflow.com>
2017-11-17 11:56:53 -0500

    Game Progress
    Completed all test case for projection algorigthms

--- src/client/src/app/util/map-position-algorithms.ts
+++ src/client/src/app/util/map-position-algorithms.ts
@@ -1,22 +1,21 @@
 import { Line } from '../../../../common/src/math/line';
-import * as THREE from 'three';
 import { Point } from '../../../../common/src/math/point';
 import { Projection } from './projection';
 
 export class MapPositionAlgorithms {
 
-    public static getClosestProjection(position: THREE.Vector2, lines: Line[]): Projection {
+    public static getProjectionOnLine(position: Point, lines: Line): Projection {
 
         return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
     }
 
-    public static getProjectionOnLine(position: THREE.Vector2, lines: Line): Projection {
+    public static getClosestProjection(position: Point, lines: Line[]): Projection {
 
         return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
     }
 
-    public static getAllProjections(position: THREE.Vector2, lines: Line): Projection {
+    public static getAllProjections(position: Point, lines: Line[]): Projection[] {
 
-        return new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0);
+        return [new Projection(0, new Line(new Point(0, 0), new Point(0, 0)), 0)];
     }
 }
