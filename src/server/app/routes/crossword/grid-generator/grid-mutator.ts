6156c4fc6fe79be937441a0449022d92c9a8a07f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-17 10:36:30 -0500

    I thought things through. Don't need the mutated grid bank. Games will just have a GridMutator.

--- src/server/app/routes/crossword/grid-generator/grid-mutator.ts
+++ src/server/app/routes/crossword/grid-generator/grid-mutator.ts
@@ -13,12 +13,6 @@ import { AbstractGridGenerator } from './abstract-grid-generator';
  */
 export class GridMutator extends AbstractGridGenerator {
 
-    private static readonly INSTANCE = new GridMutator();
-
-    public static getInstance(): GridMutator {
-        return GridMutator.INSTANCE;
-    }
-
     protected constructor() {
         super();
     }
