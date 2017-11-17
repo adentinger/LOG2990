b5f231ae6809ae7aee6eaa21b9ba36ab5fbbf62e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-17 15:49:39 -0500

    Overrid validateWord for GameDataDynamic.

--- src/server/app/routes/crossword/games/game-data-dynamic.ts
+++ src/server/app/routes/crossword/games/game-data-dynamic.ts
@@ -1,8 +1,9 @@
 import { GameData } from './game-data';
 import { GridWord } from '../../../../../common/src/crossword/grid-word';
-import { Difficulty } from '../../../../../common/src/crossword/crossword-enums';
+import { Difficulty, Owner } from '../../../../../common/src/crossword/crossword-enums';
 import { GridMutator } from '../grid-generator/grid-mutator';
 import { toGridGeneratorDifficulty } from './temp-util';
+import { Word } from '../grid-generator/word';
 
 export class GameDataDynamic extends GameData {
 
@@ -20,6 +21,21 @@ export class GameDataDynamic extends GameData {
 
     public get mutatedWords(): Promise<GridWord[]> {
         return this.mutator.mutatedGrid.then(grid => grid.toGridWords());
+    }
+
+    public validateWord(wordGuess: GridWord): boolean {
+        const validated = super.validateWord(wordGuess);
+
+        if (validated) {
+            this.mutator.cancelMutation();
+            this.mutator.mutateGrid(
+                this.wordsInternal
+                    .filter(gridWord => gridWord.owner !== Owner.none)
+                    .map(gridWord => Word.fromGridWord(gridWord))
+            );
+        }
+
+        return validated;
     }
 
 }
