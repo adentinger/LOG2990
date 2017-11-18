a14a6d23acb3db7c80fc97bac8a4a22d04cb451a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-18 16:41:29 -0500

    Implemented constructor for GridFillerContainer.

--- src/server/app/routes/crossword/grid-generator/grid-filler-container.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-container.ts
@@ -1,10 +1,22 @@
 import { GridFiller } from './grid-filler';
+import { GridFillerFirstSection } from './grid-filler-first-section';
+import { GridFillerSecondSection } from './grid-filler-second-section';
+import { GridFillerThirdSection } from './grid-filler-third-section';
+import { GridFillerFourthSection } from './grid-filler-fourth-section';
 import { AbstractWordSuggestionsGetter } from './abstract-word-suggestions-getter';
 
 export class GridFillerContainer extends GridFiller {
 
+    private readonly fillers: GridFiller[];
+
     constructor(suggestionsGetter: AbstractWordSuggestionsGetter) {
         super(suggestionsGetter);
+        this.fillers = [
+            new GridFillerFirstSection (suggestionsGetter),
+            new GridFillerSecondSection(suggestionsGetter),
+            new GridFillerThirdSection (suggestionsGetter),
+            new GridFillerFourthSection(suggestionsGetter)
+        ];
     }
 
 }
