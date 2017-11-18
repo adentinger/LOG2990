b2be57bf338246ad492c42ce80dfc60942cb425f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-18 16:47:02 -0500

    Implemented class GridFillerContainer.

--- src/server/app/routes/crossword/grid-generator/grid-filler-container.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-container.ts
@@ -4,6 +4,8 @@ import { GridFillerSecondSection } from './grid-filler-second-section';
 import { GridFillerThirdSection } from './grid-filler-third-section';
 import { GridFillerFourthSection } from './grid-filler-fourth-section';
 import { AbstractWordSuggestionsGetter } from './abstract-word-suggestions-getter';
+import { GridFillerWordPlacement as WordPlacement } from './grid-filler-word-placement';
+import { Grid } from './grid';
 
 export class GridFillerContainer extends GridFiller {
 
@@ -17,6 +19,35 @@ export class GridFillerContainer extends GridFiller {
             new GridFillerThirdSection (suggestionsGetter),
             new GridFillerFourthSection(suggestionsGetter)
         ];
+    }
+
+    public get acrossPlacement(): WordPlacement[] {
+        let placement: WordPlacement[] = [];
+        this.fillers.forEach(filler => {
+            placement = placement.concat(filler.acrossPlacement);
+        });
+        return placement;
+    }
+
+    public get verticalPlacement(): WordPlacement[] {
+        let placement: WordPlacement[] = [];
+        this.fillers.forEach(filler => {
+            placement = placement.concat(filler.verticalPlacement);
+        });
+        return placement;
+    }
+
+    public cancelFilling(): void {
+        this.fillers.forEach(filler => {
+            filler.cancelFilling();
+        });
+    }
+
+    public async fill(grid: Grid): Promise<void> {
+        for (let i = 0; i < this.fillers.length; ++i) {
+            const filler = this.fillers[i];
+            await filler.fill(grid);
+        }
     }
 
 }
