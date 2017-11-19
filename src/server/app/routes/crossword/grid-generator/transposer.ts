c6aefb134a5e6acab76743236bd037c2028bfe4a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-19 18:26:25 -0500

    Renamed GridFillerTransposer to Transposer.

---
+++ src/server/app/routes/crossword/grid-generator/transposer.ts
@@ -0,0 +1,14 @@
+import { GridFiller } from './grid-filler';
+
+/**
+ * @class GridFillerTransposer
+ * @description Has the responsibility of transposing objects,
+ * such as GridFillers and Grids.
+ */
+export class Transposer {
+
+    public transposeFiller(filler: GridFiller): void {
+        return null;
+    }
+
+}
