f7addcdc0b89c540f45b5c017909e74254698603
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-19 18:24:35 -0500

    Created class comment for GridFillerTransposer.

--- src/server/app/routes/crossword/grid-generator/grid-filler-transposer.ts
+++ src/server/app/routes/crossword/grid-generator/grid-filler-transposer.ts
@@ -1,5 +1,9 @@
 import { GridFiller } from './grid-filler';
 
+/**
+ * @class GridFillerTransposer
+ * @description Has the responsibility of transposing a grid filler.
+ */
 export class GridFillerTransposer {
 
     public transpose(filler: GridFiller): void {
