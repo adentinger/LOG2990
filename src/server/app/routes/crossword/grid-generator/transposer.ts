bcfc1bb452152c65fbe84e4baa4c97be3cedb91d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-19 19:01:31 -0500

    Implemented transposeGrid.

--- src/server/app/routes/crossword/grid-generator/transposer.ts
+++ src/server/app/routes/crossword/grid-generator/transposer.ts
@@ -1,5 +1,6 @@
 import { GridFiller } from './grid-filler';
 import { Grid } from './grid';
+import { Direction } from '../../../../../common/src/crossword/crossword-enums';
 
 /**
  * @class GridFillerTransposer
@@ -9,11 +10,27 @@ import { Grid } from './grid';
 export class Transposer {
 
     public transposeFiller(filler: GridFiller): void {
-        return null;
+        filler.acrossPlacement.forEach(placement => {
+            [placement.position.row, placement.position.column] =
+                [placement.position.column, placement.position.row];
+        });
+        filler.verticalPlacement.forEach(placement => {
+            [placement.position.row, placement.position.column] =
+                [placement.position.column, placement.position.row];
+        });
     }
 
     public transposeGrid(grid: Grid): void {
-        return null;
+        console.log(grid.toString());
+        grid.words.forEach(word => {
+            word.direction = (word.direction === Direction.horizontal)
+                           ? Direction.vertical
+                           : Direction.horizontal;
+            [word.position.row, word.position.column] =
+                [word.position.column, word.position.row];
+        });
+        console.log('---------------------');
+        console.log(grid.toString());
     }
 
 }
