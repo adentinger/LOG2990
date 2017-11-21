3fb58a9b2b748d4c31486fc78b9853bcd37a2c93
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-20 19:49:03 -0500

    Implemented startGame in GameStarterService.

--- src/client/src/app/crossword/services/game-starter.service.ts
+++ src/client/src/app/crossword/services/game-starter.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { WaitingService } from '../config-menu/waiting/waiting.service';
+import { UserChoiceService, CreateOrJoin } from '../config-menu/user-choice.service';
+import { GameService, GameState } from '../game.service';
+import { GameHttpService } from './game-http.service';
 
 @Injectable()
 export class GameStarterService {
 
-    constructor() { }
+    constructor(private waitingService: WaitingService,
+                private userChoiceService: UserChoiceService,
+                private gameService: GameService,
+                private gameHttpService: GameHttpService) { }
+
+    public startGame(): void {
+        this.waitingService.isWaiting.next(true);
+        const isJoiningGame = this.userChoiceService.createOrJoin === CreateOrJoin.join;
+        if (isJoiningGame) {
+            this.gameService.state = GameState.started;
+            this.gameService.joinGame(
+                this.userChoiceService.chosenGame,
+                this.userChoiceService.playerName
+            );
+        }
+        else {
+            this.gameHttpService.createGame(this.userChoiceService.toGameConfiguration())
+                .then((gameId) => {
+                    this.gameService.state = GameState.started;
+                    this.gameService.joinGame(
+                        gameId,
+                        this.userChoiceService.playerName
+                    );
+                });
+        }
+    }
 
 }
