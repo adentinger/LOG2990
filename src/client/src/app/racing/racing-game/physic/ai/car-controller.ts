d463761809d77b53ee7d64f6cf28ea93f3f9cb59
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-11-21 12:28:37 -0500

    Added CarController skeleton

--- src/client/src/app/racing/racing-game/physic/ai/car-controller.ts
+++ src/client/src/app/racing/racing-game/physic/ai/car-controller.ts
@@ -1,5 +1,14 @@
 import { Car } from '../../models/car/car';
+import { EventManager } from '../../../../event-manager.service';
+import { BEFORE_PHYSIC_UPDATE_EVENT } from '../engine';
 
 export abstract class CarController {
     public constructor(protected controlledCar: Car) { }
+
+    public start(): void { }
+    public stop(): void { }
+
+    @EventManager.Listener(BEFORE_PHYSIC_UPDATE_EVENT)
+    // tslint:disable-next-line:no-unused-variable
+    private updateControls(): void { }
 }
