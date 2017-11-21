5a5c2e6ce21cee761b676ff0ca4c3004b03a81e0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-20 19:47:18 -0500

    Provided GameStarterService in the CrosswordModule.

--- src/client/src/app/crossword/crossword.module.ts
+++ src/client/src/app/crossword/crossword.module.ts
@@ -23,6 +23,7 @@ import { ConfirmationComponent } from './config-menu/confirmation/confirmation.c
 import { GameService } from './game.service';
 import { UserChoiceService } from './config-menu/user-choice.service';
 import { GameHttpService } from './services/game-http.service';
+import { GameStarterService } from './services/game-starter.service';
 
 @NgModule({
     imports: [
@@ -53,7 +54,8 @@ import { GameHttpService } from './services/game-http.service';
         GameDetailsService,
         SelectionService,
         GameService,
-        UserChoiceService
+        UserChoiceService,
+        GameStarterService
     ],
     exports: [
         CrosswordComponent
