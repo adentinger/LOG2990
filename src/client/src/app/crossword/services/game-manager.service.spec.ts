e5bf503905db8c9918420ef43bae1030dce4b539
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-21 18:46:52 -0500

    Fixed remaining unit tests.

--- src/client/src/app/crossword/services/game-manager.service.spec.ts
+++ src/client/src/app/crossword/services/game-manager.service.spec.ts
@@ -1,12 +1,35 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GameManagerService } from './game-manager.service';
+import { GameService } from '../game.service';
+import { UserChoiceService } from '../config-menu/user-choice.service';
+import { GameHttpService } from './game-http.service';
+import { DefinitionsService } from '../definition-field/definitions.service';
+import { GridService } from '../board/grid.service';
+import { WaitingService } from '../config-menu/waiting/waiting.service';
+import { PacketManagerClient } from '../../packet-manager-client';
+import { packetManagerClient } from '../../packet-manager.service';
+import { SelectionService } from '../selection.service';
 
 describe('GameManagerService', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-        providers: [GameManagerService]
+            imports: [
+                HttpClientModule
+            ],
+            providers: [
+                { provide: PacketManagerClient, useValue: packetManagerClient },
+                GameManagerService,
+                GameHttpService,
+                GameService,
+                UserChoiceService,
+                DefinitionsService,
+                GridService,
+                WaitingService,
+                SelectionService
+            ]
         });
     });
 
