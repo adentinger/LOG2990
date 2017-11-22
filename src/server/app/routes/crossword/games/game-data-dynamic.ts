11852dc5b7f2e73b991e1f71f73859a2676961e7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-22 18:08:46 -0500

    Made us fetch the grid's definitions when it is done mutating.

--- src/server/app/routes/crossword/games/game-data-dynamic.ts
+++ src/server/app/routes/crossword/games/game-data-dynamic.ts
@@ -16,6 +16,7 @@ export class GameDataDynamic extends GameData {
 
     private mutator: GridMutator;
     private logger = Logger.getLogger('GameDataDynamic');
+    private mutationPromise: Promise<MutationData>;
 
     constructor(difficulty: Difficulty) {
         super(difficulty);
@@ -24,18 +25,19 @@ export class GameDataDynamic extends GameData {
     }
 
     public applyMutation(): Promise<void> {
-        return this.mutatedGrid.then(grid => {
-            this.grid = grid;
-            this.setDefinitions();
-            this.startMutatingGrid();
-        }).catch(() => this.logger.warn('Failed to apply mutation.'));
+        const promise = this.mutationPromise.then(mutationData => {
+            this.grid = mutationData.grid;
+            this.definitionsInternal = mutationData.definitions;
+        });
+        this.startMutatingGrid();
+        return promise;
     }
 
     protected get mutatedGrid(): Promise<Grid> {
         return new Promise((resolve, reject) => {
-            this.mutator.mutatedGrid.then(grid => {
-                if (grid !== null) {
-                    resolve(grid);
+            this.mutationPromise.then(mutationData => {
+                if (mutationData.grid !== null) {
+                    resolve(mutationData.grid);
                 }
                 else {
                     reject('[GameDataDynamic]: Grid generation cancelled');
@@ -60,10 +62,27 @@ export class GameDataDynamic extends GameData {
     }
 
     private startMutatingGrid(): void {
-        this.mutator.mutateGrid(
+        const promise = this.mutator.mutateGrid(
             this.grid.words
                 .filter(word => word.owner !== Player.NO_PLAYER)
-        ).catch(() => { return; });
+        );
+
+        this.mutationPromise = promise.then(grid => {
+            if (grid !== null) {
+                return this.fetchDefinitionsOf(grid).then(definitions => {
+                    return {
+                        grid: grid,
+                        definitions: definitions
+                    };
+                });
+            }
+            else {
+                return {
+                    grid: grid,
+                    definitions: []
+                };
+            }
+        });
     }
 
 }
