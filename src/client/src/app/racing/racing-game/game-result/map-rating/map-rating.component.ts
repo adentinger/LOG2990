3b9c189334e726c80d5c594e362c4f82f77552d8
Eric Chao <hon-leung-eric.chao@polymtl.ca>
2017-12-01 12:35:37 -0500

    implement five stars rating effect

--- src/client/src/app/racing/racing-game/game-result/map-rating/map-rating.component.ts
+++ src/client/src/app/racing/racing-game/game-result/map-rating/map-rating.component.ts
@@ -12,11 +12,32 @@ export class MapRatingComponent implements OnInit {
 
     @Input() public map: RenderableMap;
     public displayable;
+    private readonly filledStarImageSource = '/assets/racing/stars-rating/filled-star.png';
+    private readonly emptyStarImageSource = '/assets/racing/stars-rating/empty-star.png';
+    private firstStar = this.emptyStarImageSource;
+    private secondStar = this.emptyStarImageSource;
+    private thirdStar = this.emptyStarImageSource;
+    private fourthStar = this.emptyStarImageSource;
+    private fifthStar = this.emptyStarImageSource;
+    private stars: string[] = [];
 
-    constructor() { }
+    constructor() {  }
 
     public ngOnInit(): void {
         this.displayable = true;
+        for (let i = 0 ; i < 5 ; i++) {
+            this.stars.push(this.emptyStarImageSource);
+        }
+    }
+
+    public OnMouseOver(index: number): void {
+        for (let i = 0 ; i <= index; i++) {
+            this.stars[i] = this.filledStarImageSource;
+        }
+    }
+
+    public OnMouseOut(): void {
+        this.stars.fill(this.emptyStarImageSource);
     }
 }
 
