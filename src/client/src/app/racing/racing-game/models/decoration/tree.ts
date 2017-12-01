b65d504d43aa2133915cf062b4ab43ff65abbe25
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-01 01:03:35 -0500

    Made Tree use the Oak tree model.

--- src/client/src/app/racing/racing-game/models/decoration/tree.ts
+++ src/client/src/app/racing/racing-game/models/decoration/tree.ts
@@ -1,11 +1,34 @@
 import * as THREE from 'three';
+
 import { Decoration } from './decoration';
+import { Loader } from '../loader';
 
 export class Tree extends Decoration {
 
+    private static readonly BASE_PATH = 'assets/racing/decoration/oak_tree/';
+
+    private static readonly PART_NAMES: string[] = [
+        'oak_green1_leaves', 'oak_green2_leaves', 'oak_trunk'
+    ];
+
+    private static readonly LOADER = new Loader();
+
+    public static readonly TREE_PARTS = Tree.LOADER.loadAll(Tree.BASE_PATH, Tree.PART_NAMES);
+
     constructor() {
-        super(new THREE.BoxGeometry(2, 4, 2).translate(0, 2, 0),
-            new THREE.MeshPhongMaterial({ specular: 10, color: 0x40A454}));
+        super();
+        this.addTreeParts();
     }
+
+    private addTreeParts(): void {
+        Tree.TREE_PARTS.then(parts => {
+            const clonedParts = parts.map(part => {
+                return part.clone();
+            });
+            clonedParts.forEach(part => part.material = (part.material as THREE.Material).clone());
+            this.add(... clonedParts);
+        });
+    }
+
 }
 
