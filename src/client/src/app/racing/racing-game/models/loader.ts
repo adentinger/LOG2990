c358ebcab0266b0ebc55c1e57a0a75fe8179aeaa
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-01 00:03:08 -0500

    Renamed method in Loader, and made it public.

--- src/client/src/app/racing/racing-game/models/loader.ts
+++ src/client/src/app/racing/racing-game/models/loader.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as THREE from 'three';
 
-import { Logger } from 'common/src/index';
+import { Logger } from '../../../../../../common/src/index';
 
 export class Loader {
 
@@ -10,7 +10,7 @@ export class Loader {
 
     constructor() { }
 
-    private loadCarPart(url: string, name: string): Promise<THREE.Mesh> {
+    public load(url: string, name: string): Promise<THREE.Mesh> {
         return new Promise((resolve, reject) => {
             this.jsonLoader.load(
                 url,
