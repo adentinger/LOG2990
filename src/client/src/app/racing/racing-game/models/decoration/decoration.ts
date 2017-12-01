d9e11e83aac8fd7ca0bfe98e192b0f9974185f1c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-01 01:18:14 -0500

    Made the trees look smoother.

--- src/client/src/app/racing/racing-game/models/decoration/decoration.ts
+++ src/client/src/app/racing/racing-game/models/decoration/decoration.ts
@@ -4,6 +4,8 @@ import { Loader } from '../loader';
 
 export abstract class Decoration extends THREE.Mesh {
 
+    private static readonly SHININESS = 10;
+
     protected static readonly loader = new Loader();
 
     public readonly isDecoration: true;
@@ -12,7 +14,16 @@ export abstract class Decoration extends THREE.Mesh {
         const clonedParts = parts.map(part => {
             return part.clone();
         });
-        clonedParts.forEach(part => part.material = (part.material as THREE.Material).clone());
+        clonedParts.forEach(part => {
+            part.material = (part.material as THREE.Material).clone();
+            part.geometry.computeVertexNormals();
+            part.geometry['computeMorphNormals']();
+            (part.material as THREE.MeshPhongMaterial).blending = THREE.NoBlending;
+            (part.material as THREE.MeshPhongMaterial).shininess = Decoration.SHININESS;
+            (part.material as THREE.MeshPhongMaterial).emissiveIntensity = 0;
+            part.receiveShadow = true;
+            part.castShadow = true;
+        });
         this.add(... clonedParts);
     }
 
