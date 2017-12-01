01b7f06f7f293266d4a023480588effeb37538c0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-11-30 23:57:36 -0500

    Implemented new method "loadCarPart" in LoaderService.

--- src/client/src/app/racing/racing-game/models/loader.service.ts
+++ src/client/src/app/racing/racing-game/models/loader.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+
+import { Logger } from '../../../../../../common/src/index';
 
 @Injectable()
 export class LoaderService {
 
+    private readonly jsonLoader = new THREE.JSONLoader();
+    private readonly logger = Logger.getLogger('LoaderService');
+
     constructor() { }
+
+    private loadCarPart(url: string, name: string): Promise<THREE.Mesh> {
+        return new Promise((resolve, reject) => {
+            this.jsonLoader.load(
+                url,
+                (geometry, materials) => {
+                    const CAR_PART = new THREE.Mesh(geometry, materials[0]);
+                    CAR_PART.name = name;
+                    resolve(CAR_PART);
+                },
+                () => { },
+                (reason) => { this.logger.warn(reason); reject(reason); }
+            );
+        });
+    }
 
 }
