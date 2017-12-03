76166731bb28ef9ddcc71db324e7f72253f5c663
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-02 19:05:32 -0500

    Created new methods updateTime and updateRating in MapUpdater.

--- src/server/app/routes/racing/map-updater.ts
+++ src/server/app/routes/racing/map-updater.ts
@@ -4,4 +4,14 @@ export class MapUpdater {
 
     protected constructor() {}
 
+    public updateTime(mapName: string, time: number): Promise<void> {
+        console.log('Updated' + mapName + '\'s time:' + time);
+        return Promise.resolve();
+    }
+
+    public updateRating(mapName: string, rating: number): Promise<void> {
+        console.log('Updated' + mapName + '\'s rating:' + rating);
+        return Promise.resolve();
+    }
+
 }
