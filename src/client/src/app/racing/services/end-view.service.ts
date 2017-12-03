c348701e7d67de4781663821dbb6e6a8bfcd1c4b
Eric Chao <hon-leung-eric.chao@polymtl.ca>
2017-12-02 21:37:04 -0500

    attribut for get

--- src/client/src/app/racing/services/end-view.service.ts
+++ src/client/src/app/racing/services/end-view.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HttpResponse } from '@angular/common/http/src/response';
+
 
 @Injectable()
 export class EndViewService {
+
+  private static readonly ADDRESS = 'http://localhost:3000';
+  private static readonly AUTHENTICATION_PATH = '/admin/authentication/';
+  private http: HttpClient;
 
   public displayGameResult;
   public mapName;
@@ -9,5 +17,6 @@ export class EndViewService {
   constructor() {
     this.displayGameResult = null;
     this.mapName = '';
+    this.http = new HttpClient(EndViewService.ADDRESS);
   }
 }
