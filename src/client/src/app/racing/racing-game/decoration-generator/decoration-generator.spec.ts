92c885237c44cc674f4cf0907e8fa56ab16cdbb6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-04 12:38:21 -0500

    Added timeout to Jasmine unit tests of DecorationGenerator.

--- src/client/src/app/racing/racing-game/decoration-generator/decoration-generator.spec.ts
+++ src/client/src/app/racing/racing-game/decoration-generator/decoration-generator.spec.ts
@@ -12,8 +12,9 @@ import { PhysicUtils } from '../physic/utils';
 describe('Decoration generator', () => {
 
     let decorationGenerator: DecorationGenerator;
-    const mockMaps = new MockMaps();
     let map: RenderableMap;
+    const mockMaps = new MockMaps();
+    const PLACEMENT_SPEC_TIMEOUT = 120000; // ms
 
     beforeEach(() => {
             map = mockMaps.renderableMap();
@@ -21,25 +22,38 @@ describe('Decoration generator', () => {
         });
 
     it('Should be created', () => {
+        console.log('HOLA');
         expect(DecorationGenerator).toBeTruthy();
+        console.log('done 26');
     });
 
-    it('Should add decorations on map', () => {
-        decorationGenerator.placeDecorationsOnMap(map).then(() => {
-            expect(map.children.some(child => child instanceof Decoration)).toBeTruthy();
-        });
-    });
+    it('Should add decorations on map',
+        (done) => {
+            decorationGenerator.placeDecorationsOnMap(map).then(() => {
+                expect(map.children.some(child => child instanceof Decoration)).toBeTruthy();
+                console.log('done 33');
+                done();
+            });
+        },
+        PLACEMENT_SPEC_TIMEOUT
+    );
 
-    it('Should not place decorations on track', () => {
-        decorationGenerator.placeDecorationsOnMap(map);
-        const decorations = map.children.filter(child => child instanceof Decoration) as Decoration[];
-        let dimensions: THREE.Vector3;
-        expect(decorations.every(decoration =>
-            map.mapLines.every(line =>
-                MapPositionAlgorithms.getProjectionOnLine(new Point(decoration.position.x, decoration.position.z), line)
-                    .distanceToSegment > (Track.SEGMENT_WIDTH / 2) + ((dimensions = PhysicUtils.getObjectDimensions(decoration)) &&
-                    Math.max(dimensions.x, dimensions.z))
-            )
-        )).toBeTruthy();
-    });
+    it('Should not place decorations on track',
+        (done) => {
+            decorationGenerator.placeDecorationsOnMap(map).then(() => {
+                const decorations = map.children.filter(child => child instanceof Decoration) as Decoration[];
+                let dimensions: THREE.Vector3;
+                expect(decorations.every(decoration =>
+                    map.mapLines.every(line =>
+                        MapPositionAlgorithms.getProjectionOnLine(new Point(decoration.position.x, decoration.position.z), line)
+                            .distanceToSegment > (Track.SEGMENT_WIDTH / 2) + ((dimensions = PhysicUtils.getObjectDimensions(decoration)) &&
+                            Math.max(dimensions.x, dimensions.z))
+                    )
+                )).toBeTruthy();
+                console.log('done 52');
+                done();
+            });
+        },
+        PLACEMENT_SPEC_TIMEOUT
+    );
 });
