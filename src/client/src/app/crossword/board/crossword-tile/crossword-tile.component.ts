1875bd4af906f2e5b714d9149078c87e06bb9f8f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2017-12-04 12:11:02 -0500

    Fixed problem where tile would get black when we wrote words on the grid.

--- src/client/src/app/crossword/board/crossword-tile/crossword-tile.component.ts
+++ src/client/src/app/crossword/board/crossword-tile/crossword-tile.component.ts
@@ -16,21 +16,14 @@ export class CrosswordTileComponent {
     @Input() public filled: WhoIsSelecting;
 
     @Input()
-    public set tileChar(value: string) {
-        this.tileValue = value !== Grid.BLACK_TILE ? value : '';
-    }
-
-    public get tileChar(): string {
-        return this.tileValue;
-    }
-
-    public get isBlackTile(): boolean {
-        return this.tileValue === '';
-    }
+    public tileChar: string;
 
     @ViewChild('caseInput') public caseInput: ElementRef;
 
     public readonly WhoIsSelecting = WhoIsSelecting;
-    public tileValue: string;
+
+    public isBlackTile(): boolean {
+        return this.tileChar === Grid.BLACK_TILE;
+    }
 
 }
