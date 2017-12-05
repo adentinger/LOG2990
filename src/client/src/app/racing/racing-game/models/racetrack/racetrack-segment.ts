f9a389981029bbdc05cbe20548c45bbf658111e9
Emir Khaled Belhaddad <emir-khaled.belhaddad@polymtl.ca>
2017-12-04 20:22:22 -0500

    Added a cehcker pattern at the beginning of the track

--- src/client/src/app/racing/racing-game/models/racetrack/racetrack-segment.ts
+++ src/client/src/app/racing/racing-game/models/racetrack/racetrack-segment.ts
@@ -7,9 +7,11 @@ export class RacetrackSegment extends THREE.Mesh {
 
     private static readonly ASPHALT_URL = 'assets/racing/textures/ground_asphalt_old_07.png';
     private static readonly ASPHALT_NORMALS_URL = 'assets/racing/normals/road_normal_map.jpg';
+    private static readonly CHECKER_URL = 'assets/racing/textures/checker.png';
 
     private static readonly ASPHALT_TEXTURE_PROMISE: Promise<THREE.Texture> = loadTexture(RacetrackSegment.ASPHALT_URL);
     private static readonly ASPHALT_NORMALS_PROMISE: Promise<THREE.Texture> = loadTexture(RacetrackSegment.ASPHALT_NORMALS_URL);
+    private static readonly CHECKER_TEXTURE_PROMISE: Promise<THREE.Texture> = loadTexture(RacetrackSegment.CHECKER_URL);
 
     public readonly mass = 0;
 
@@ -20,7 +22,8 @@ export class RacetrackSegment extends THREE.Mesh {
         this.material = new THREE.MeshPhongMaterial({side: THREE.FrontSide, shininess: 1000});
         this.waitToLoad = Promise.all([
             RacetrackSegment.ASPHALT_TEXTURE_PROMISE,
-            RacetrackSegment.ASPHALT_NORMALS_PROMISE
+            RacetrackSegment.ASPHALT_NORMALS_PROMISE,
+            RacetrackSegment.CHECKER_TEXTURE_PROMISE
         ]).then((textures) =>
             textures.map((texture) => texture.clone())
                 .map((texture) => {
@@ -37,6 +40,19 @@ export class RacetrackSegment extends THREE.Mesh {
                 (<THREE.MeshPhongMaterial>this.material).normalMap = normalMap;
             }).then(() => { });
         this.position.setY(0.015); // segment must be on top to support other textures
+    }
+
+    public placeStartingSegment(): void {
+        const startSegment = new THREE.Mesh(
+            new THREE.PlaneGeometry(Track.SEGMENT_WIDTH, Track.SEGMENT_WIDTH).rotateX(RacetrackSegment.GEOMETRY_ROTATION),
+            new THREE.MeshPhongMaterial({side: THREE.FrontSide, shininess: 1000})
+        );
+        RacetrackSegment.CHECKER_TEXTURE_PROMISE.then((texture) => {
+            (<THREE.MeshPhongMaterial>startSegment.material).map = texture;
+        });
+        startSegment.translateZ(- this.length / 2 + Track.SEGMENT_WIDTH / 2);
+        startSegment.position.setY(0.01);
+        this.add(startSegment);
     }
 
 }
